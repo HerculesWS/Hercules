HerculesWS/Hercules

commit bfa65c105a93d3f3021e82b5c7f4cc1bddb26b74

Update to Auth Glitch fix: moved unit_free_pc back to where it was and updated it so unit_free_pc just tells the script to end(not hard delete). (bugreport:1214)
git-svn-id: https://rathena.svn.sourceforge.net/svnroot/rathena/trunk@12481 54d463be-8e91-2dee-dedb-b68131a5f0ec

src/map/unit.c
@@ -1838,12 +1838,11 @@ int unit_free(struct block_list *bl, int clrtype)
 			sd->regstr = NULL;
 			sd->regstr_num = 0;
 		}
+
+		//Tell the script to end, not delete it, it will free itself when necessary [Kevin]
 		if (sd->st) {
-			if (sd->st->stack)
-				script_free_stack (sd->st->stack);
-			aFree(sd->st);
-			sd->st = NULL;
-			sd->npc_id = 0;
+			sd->st->rid = 0;
+			sd->st->state = 2;
 		}
 	} else if( bl->type == BL_PET ) {
 		struct pet_data *pd = (struct pet_data*)bl;

src/map/chrif.c
@@ -400,9 +400,6 @@ int chrif_changemapserverack(int account_id, int login_id1, int login_id2, int c
 	} else
 		clif_changemapserver(node->sd, map_index, x, y, ntohl(ip), ntohs(port));
 
-	//Free session data from this map server [Kevin]
-	unit_free_pc(node->sd);
-
 	//Player has been saved already, remove him from memory. [Skotlex]
 	chrif_auth_delete(account_id, char_id, ST_MAPCHANGE);
 

src/map/pc.c
@@ -3595,6 +3595,9 @@ int pc_setpos(struct map_session_data* sd, unsigned short mapindex, int x, int y
 		chrif_save(sd,2);
 		chrif_changemapserver(sd, ip, (short)port);
 
+		//Free session data from this map server [Kevin]
+		unit_free_pc(node->sd);
+
 		return 0;
 	}
 

commit 05debeeda0c727bb299950e1abb8b27465943e07

Fixed a memleak that occurs when a player exits while waiting on a script select() or menu() (caused by r12481). The change made the attached script not deallocate to avoid a crash when changing mapservers, however deallocation must occur here since scripts waiting for user input are unable to free themselves.
git-svn-id: https://rathena.svn.sourceforge.net/svnroot/rathena/trunk@14213 54d463be-8e91-2dee-dedb-b68131a5f0ec

src/map/unit.c
@@ -2015,12 +2015,11 @@ int unit_free(struct block_list *bl, int clrtype)
 				sd->regstr = NULL;
 				sd->regstr_num = 0;
 			}
-			//Tell the script to end, not delete it, it will free itself when necessary [Kevin]
-			// TODO review this assumption, possible source of memory leaks [FlavioJS]
-			if( sd->st )
-			{
-				sd->st->rid = 0;
-				sd->st->state = END;
+			if( sd->st && sd->st->state != RUN )
+			{// free attached scripts that are waiting
+				script_free_state(sd->st);
+				sd->st = NULL;
+				sd->npc_id = 0;
 			}
 			break;
 		}