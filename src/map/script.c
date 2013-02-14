49831814d6a462104e5a4b61bdff405a3bb69fb1
shennetsind <ind@henn.et>
2013-02-14 17:22:50 -0200

    Updating equip[] for script.c functions
    Script commands relying on equipment slot may now access the 3 following types
    
    EQI_COSTUME_HEAD_LOW (11)
    EQI_COSTUME_HEAD_MID (12)
    EQI_COSTUME_HEAD_TOP (13)
    e.g.
    getequipid(13) returns the item id of the equipped costume head top (if any)
    
    Signed-off-by: shennetsind <ind@henn.et>

--- src/map/script.c
+++ src/map/script.c
@@ -7347,7 +7347,7 @@ BUILDIN_FUNC(strnpcinfo)
 
 
 // aegis->athena slot position conversion table
-static unsigned int equip[] = {EQP_HEAD_TOP,EQP_ARMOR,EQP_HAND_L,EQP_HAND_R,EQP_GARMENT,EQP_SHOES,EQP_ACC_L,EQP_ACC_R,EQP_HEAD_MID,EQP_HEAD_LOW};
+static unsigned int equip[] = {EQP_HEAD_TOP,EQP_ARMOR,EQP_HAND_L,EQP_HAND_R,EQP_GARMENT,EQP_SHOES,EQP_ACC_L,EQP_ACC_R,EQP_HEAD_MID,EQP_HEAD_LOW,EQP_COSTUME_HEAD_LOW,EQP_COSTUME_HEAD_MID,EQP_COSTUME_HEAD_TOP};
 
 /*==========================================
  * GetEquipID(Pos);     Pos: 1-10
