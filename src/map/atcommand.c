9b5ec24b69d4ae517f07ffa7a9b2730adbe998d4
shennetsind <ind@henn.et>
2013-03-27 15:55:51 -0300

    @accinfo update
    Added pincode support
    Changed group_id to group_level as to respect the level hierarchy on whether to display passwords or not.
    Updated GeoIP Database to March 2013
    
    Signed-off-by: shennetsind <ind@henn.et>

--- src/map/atcommand.c
+++ src/map/atcommand.c
@@ -8598,7 +8598,7 @@ ACMD_FUNC(accinfo) {
 	//remove const type
 	safestrncpy(query, message, NAME_LENGTH);
 
-	intif_request_accinfo( sd->fd, sd->bl.id, sd->group_id, query );
+	intif_request_accinfo( sd->fd, sd->bl.id, pc_get_group_level(sd), query );
 
 	return 0;
 }
