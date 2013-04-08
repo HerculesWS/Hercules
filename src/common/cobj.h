5e018af1749be4b64e0c0bdf77d412eaa82dae57
Jonathan Lima <greenboxal@gmail.com>
2013-04-08 02:45:35 -0300

    Added COBJ header. Disabled NPCs, don't need them by now.

---
+++ src/common/cobj.h
@@ -0,0 +1,21 @@
+// Copyright (c) Hercules Dev Team, licensed under GNU GPL.
+// See the LICENSE file
+
+#ifndef _COBJ_H_
+#define _COBJ_H_
+
+// Defines a pattern for creating "classes"
+
+#define COBJ(name) \
+	struct name
+
+#define COBJ_DEF_DETAIL \
+	void *__detail;
+
+#define COBJ_GET_DETAIL(obj, type) ((type)((obj)->__detail))
+#define COBJ_SET_DETAIL(obj, value) (obj)->__detail = (void*)(value)
+
+#define VTABLE_ENTRY(ret, name, args) \
+	ret (*name) args;
+
+#endif
