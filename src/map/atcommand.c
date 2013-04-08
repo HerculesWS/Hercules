43ce3dbf3da829635869d6a7a9c6688bc0b133e3
lemongrass3110 <lemongrass3110@54d463be-8e91-2dee-dedb-b68131a5f0ec>
2013-04-08 02:05:27 +0000

    Speeded up jobchange command. Partially fixes bugreport:7453
    git-svn-id: https://rathena.svn.sourceforge.net/svnroot/rathena/trunk@17235 54d463be-8e91-2dee-dedb-b68131a5f0ec

--- src/map/atcommand.c
+++ src/map/atcommand.c
@@ -1001,14 +1001,24 @@ ACMD_FUNC(jobchange)
 	nullpo_retr(-1, sd);
 
     if (!message || !*message || sscanf(message, "%d %d", &job, &upper) < 1) {
-		int i, found = 0;
+		int i;
+		bool found = false;
 
-        for (i = JOB_NOVICE; i < JOB_MAX; ++i) {
-            if (strncmpi(message, job_name(i), 16) == 0) {
-                job = i;
-				upper = 0;
-				found = 1;
-				break;
+		upper = 0;
+ 
+		// Normal Jobs
+		for( i = JOB_NOVICE; i < JOB_MAX_BASIC && !found; i++ ){
+			if (strncmpi(message, job_name(i), 16) == 0) {
+				job = i;
+				found = true;
+			}
+		}
+ 
+		// High Jobs, Babys and Third
+		for( i = JOB_NOVICE_HIGH; i < JOB_MAX && !found; i++ ){
+			if (strncmpi(message, job_name(i), 16) == 0) {
+				job = i;
+				found = true;
 			}
 		}
 
