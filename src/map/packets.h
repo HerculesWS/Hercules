e6baf9ffcf0b8ca25b8c235f7b3760daf88acc4c
Matheus Macabu <mkbu95@gmail.com>
2013-06-09 01:53:57 -0300

    Few adjustments to group search for 2012-04-1x and added new packets for 2013-06-05 (thanks to Shakto).
    Signed-off-by: Matheus Macabu <mkbu95@gmail.com>

--- src/map/packets.h
+++ src/map/packets.h
@@ -2164,6 +2164,7 @@ packet(0x020d,-1);
 	// Shuffle End
 #endif
 
+//2013-05-29Ragexe (Shakto)
 #if PACKETVER >= 20130529
 	packet(0x0890,7,clif->pActionRequest,2,6);
 	packet(0x0438,10,clif->pUseSkillToId,2,4,6);
@@ -2194,6 +2195,39 @@ packet(0x020d,-1);
 	packet(0x0877,26,clif->pFriendsListAdd,2);
 	packet(0x023B,5,clif->pHomMenu,2,4);
 	packet(0x0956,36,clif->pStoragePassword,0);
+#endif
+
+//2013-06-05Ragexe (Shakto)
+#if PACKETVER >= 20130605
+	packet(0x0369,7,clif->pActionRequest,2,6);
+	packet(0x083C,10,clif->pUseSkillToId,2,4,6);
+	packet(0x0437,5,clif->pWalkToXY,2);
+	packet(0x035F,6,clif->pTickSend,2);
+	packet(0x0202,5,clif->pChangeDir,2,4);
+	packet(0x07E4,6,clif->pTakeItem,2);
+	packet(0x0362,6,clif->pDropItem,2,4);
+	packet(0x07EC,8,clif->pMoveToKafra,2,4);
+	packet(0x0364,8,clif->pMoveFromKafra,2,4);
+	packet(0x0438,10,clif->pUseSkillToPos,2,4,6,8);
+	packet(0x0366,90,clif->pUseSkillToPosMoreInfo,2,4,6,8,10);
+	packet(0x096A,6,clif->pGetCharNameRequest,2);
+	packet(0x0368,6,clif->pSolveCharName,2);
+	packet(0x0838,12,clif->pSearchStoreInfoListItemClick,2,6,10);
+	packet(0x0835,2,clif->pSearchStoreInfoNextPage,0);
+	packet(0x0819,-1,clif->pSearchStoreInfo,2,4,5,9,13,14,15);
+	packet(0x0811,-1,clif->pReqTradeBuyingStore,2,4,8,12);
+	packet(0x0360,6,clif->pReqClickBuyingStore,2);
+	packet(0x0817,2,clif->pReqCloseBuyingStore,0);
+	packet(0x0815,-1,clif->pReqOpenBuyingStore,2,4,8,9,89);
+	packet(0x0365,41,clif->pPartyBookingRegisterReq,2,4);
+	// packet(0x0363,8); // CZ_JOIN_BATTLE_FIELD
+	packet(0x0281,-1,clif->pItemListWindowSelected,2,4,8);
+	packet(0x022D,19,clif->pWantToConnection,2,6,10,14,18);
+	packet(0x0802,26,clif->pPartyInvite2,2);
+	// packet(0x0436,4); // CZ_GANGSI_RANK
+	packet(0x023B,26,clif->pFriendsListAdd,2);
+	packet(0x0361,5,clif->pHomMenu,2,4);
+	packet(0x0883,36,clif->pStoragePassword,0);
 #endif
 
 #endif /* _PACKETS_H_ */
