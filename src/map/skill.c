f33c54e76be4f3fea6a21aab40fe13feb952239d
malufett <malufett.eat.my.binaries@gmail.com>
2013-06-16 22:38:02 -0700

    Fixed Bug#7382 -Map crash with AB_CLEARANCE

--- src/map/skill.c
+++ src/map/skill.c
@@ -7965,12 +7965,11 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 					break;
 				for(i = 0; i < SC_MAX; i++)
 				{
-					if( SC_COMMON_MAX > i ){
-						if ( !tsc->data[i] || !status_get_sc_type(i) )
-							continue;
+					if ( !tsc->data[i] )
+						continue;
+					if( SC_COMMON_MAX > i )
 						if ( status_get_sc_type(i)&SC_NO_CLEARANCE )
 							continue;
-					}
 					switch (i) {
 						case SC_ASSUMPTIO:
 							if( bl->type == BL_MOB )
