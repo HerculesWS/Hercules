dc5c72590f286125f1f1aad8321a80127b0ed9d8
Matias <matiassw@gmail.com>
2013-06-25 02:30:05 -0400

    - Fixes Chatrooms disappearing when using @disguise after creating a chatroom.

--- src/map/pc.c
+++ src/map/pc.c
@@ -17,6 +17,7 @@
 #include "atcommand.h" // get_atcommand_level()
 #include "battle.h" // battle_config
 #include "battleground.h"
+#include "chat.h"
 #include "chrif.h"
 #include "clif.h"
 #include "date.h" // is_day_of_*()
@@ -1665,6 +1666,13 @@ int pc_disguise(struct map_session_data *sd, int class_) {
 			//It seems the cart info is lost on undisguise.
 			clif->cartlist(sd);
 			clif->updatestatus(sd,SP_CARTINFO);
+		}
+		if (sd->chatID) {
+			struct chat_data* cd;
+			nullpo_retr(1, sd);
+			cd = (struct chat_data*)map_id2bl(sd->chatID);
+			if( cd != NULL || (struct block_list*)sd == cd->owner )
+				clif->dispchat(cd,0);
 		}
 	}
 	return 1;
