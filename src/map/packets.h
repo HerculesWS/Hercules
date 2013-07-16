f530cfe173bc5e9ac177adac0ae4aef33164838f
Haru <haru@dotalux.com>
2013-07-16 20:30:19 +0200

    Packets and packet keys update with the info provided by Shakto
    - Added packets for 2013-06-12Ragexe, 2013-06-18Ragexe,
      2013-06-26Ragexe, 2013-07-03Ragexe
    - Added packetKeys for 2013-06-26Ragexe, 2013-07-03Ragexe
    - Thanks to Shakto for always providing us with the data, OnNplay for
      the the update reminder.
    
    Signed-off-by: Haru <haru@dotalux.com>

--- src/map/packets.h
+++ src/map/packets.h
@@ -2272,6 +2272,102 @@ packet(0x020d,-1);
 	packet(0x0883,36,clif->pStoragePassword,0);
 #endif
 
+//2013-06-12Ragexe (Shakto)
+#if PACKETVER >= 20130612
+	packet(0x087E,5,clif->pChangeDir,2,4);
+	packet(0x0919,19,clif->pWantToConnection,2,6,10,14,18);
+	packet(0x0940,26,clif->pFriendsListAdd,2);
+	packet(0x093A,5,clif->pHomMenu,2,4);
+	packet(0x0964,36,clif->pStoragePassword,0);
+#endif
+
+//2013-06-18Ragexe (Shakto)
+#if PACKETVER >= 20130618
+	packet(0x0889,7,clif->pActionRequest,2,6);
+	packet(0x0951,10,clif->pUseSkillToId,2,4,6);
+	packet(0x088E,5,clif->pWalkToXY,2);
+	packet(0x0930,6,clif->pTickSend,2);
+	packet(0x08A6,5,clif->pChangeDir,2,4);
+	packet(0x0962,6,clif->pTakeItem,2);
+	packet(0x0917,6,clif->pDropItem,2,4);
+	packet(0x0885,8,clif->pMoveToKafra,2,4);
+	packet(0x0936,8,clif->pMoveFromKafra,2,4);
+	packet(0x096A,10,clif->pUseSkillToPos,2,4,6,8);
+	packet(0x094F,90,clif->pUseSkillToPosMoreInfo,2,4,6,8,10);
+	packet(0x0944,6,clif->pGetCharNameRequest,2);
+	packet(0x0945,6,clif->pSolveCharName,2);
+	packet(0x0890,12,clif->pSearchStoreInfoListItemClick,2,6,10);
+	packet(0x0363,2,clif->pSearchStoreInfoNextPage,0);
+	packet(0x0281,-1,clif->pSearchStoreInfo,2,4,5,9,13,14,15);
+	packet(0x0891,-1,clif->pReqTradeBuyingStore,2,4,8,12);
+	packet(0x0862,6,clif->pReqClickBuyingStore,2);
+	packet(0x085A,2,clif->pReqCloseBuyingStore,0);
+	packet(0x0932,-1,clif->pReqOpenBuyingStore,2,4,8,9,89);
+	packet(0x08A7,41,clif->pPartyBookingRegisterReq,2,4);
+	// packet(0x087A,8); // CZ_JOIN_BATTLE_FIELD
+	packet(0x0942,-1,clif->pItemListWindowSelected,2,4,8);
+	packet(0x095B,19,clif->pWantToConnection,2,6,10,14,18);
+	packet(0x0887,26,clif->pPartyInvite2,2);
+	// packet(0x0878,4); // CZ_GANGSI_RANK
+	packet(0x0953,26,clif->pFriendsListAdd,2);
+	packet(0x02C4,5,clif->pHomMenu,2,4);
+	packet(0x0864,36,clif->pStoragePassword,0);
+#endif
+
+//2013-06-26Ragexe (Shakto)
+#if PACKETVER >= 20130626
+	packet(0x0369,7,clif->pActionRequest,2,6);
+	packet(0x083C,10,clif->pUseSkillToId,2,4,6);
+	packet(0x0437,5,clif->pWalkToXY,2);
+	packet(0x035F,6,clif->pTickSend,2);
+	packet(0x094D,5,clif->pChangeDir,2,4);
+	packet(0x088B,6,clif->pTakeItem,2);
+	packet(0x0952,6,clif->pDropItem,2,4);
+	packet(0x0921,8,clif->pMoveToKafra,2,4);
+	packet(0x0817,8,clif->pMoveFromKafra,2,4);
+	packet(0x0438,10,clif->pUseSkillToPos,2,4,6,8);
+	packet(0x0366,90,clif->pUseSkillToPosMoreInfo,2,4,6,8,10);
+	packet(0x096A,6,clif->pGetCharNameRequest,2);
+	packet(0x0368,6,clif->pSolveCharName,2);
+	packet(0x0838,12,clif->pSearchStoreInfoListItemClick,2,6,10);
+	packet(0x0835,2,clif->pSearchStoreInfoNextPage,0);
+	packet(0x0819,-1,clif->pSearchStoreInfo,2,4,5,9,13,14,15);
+	packet(0x0811,-1,clif->pReqTradeBuyingStore,2,4,8,12);
+	packet(0x0360,6,clif->pReqClickBuyingStore,2);
+	packet(0x0365,2,clif->pReqCloseBuyingStore,0);
+	packet(0x0815,-1,clif->pReqOpenBuyingStore,2,4,8,9,89);
+	packet(0x0894,41,clif->pPartyBookingRegisterReq,2,4);
+	// packet(0x0860,8); // CZ_JOIN_BATTLE_FIELD
+	packet(0x08A5,-1,clif->pItemListWindowSelected,2,4,8);
+	packet(0x088C,19,clif->pWantToConnection,2,6,10,14,18);
+	packet(0x0895,26,clif->pPartyInvite2,2);
+	// packet(0x088F,4); // CZ_GANGSI_RANK
+	packet(0x08AB,26,clif->pFriendsListAdd,2);
+	packet(0x0960,5,clif->pHomMenu,2,4);
+	packet(0x0930,36,clif->pStoragePassword,0);
+#endif
+
+//2013-07-03Ragexe (Shakto)
+#if PACKETVER >= 20130703
+	packet(0x0930,5,clif->pChangeDir,2,4);
+	packet(0x07E4,6,clif->pTakeItem,2);
+	packet(0x0362,6,clif->pDropItem,2,4);
+	packet(0x07EC,8,clif->pMoveToKafra,2,4);
+	packet(0x0364,8,clif->pMoveFromKafra,2,4);
+	packet(0x0202,6,clif->pReqClickBuyingStore,2);
+	packet(0x0817,2,clif->pReqCloseBuyingStore,0);
+	packet(0x0815,-1,clif->pReqOpenBuyingStore,2,4,8,9,89);
+	packet(0x0365,41,clif->pPartyBookingRegisterReq,2,4);
+	// packet(0x0363,8); // CZ_JOIN_BATTLE_FIELD
+	packet(0x0281,-1,clif->pItemListWindowSelected,2,4,8);
+	packet(0x022D,19,clif->pWantToConnection,2,6,10,14,18);
+	packet(0x0802,26,clif->pPartyInvite2,2);
+	// packet(0x0436,4); // CZ_GANGSI_RANK
+	packet(0x0360,26,clif->pFriendsListAdd,2);
+	packet(0x094A,5,clif->pHomMenu,2,4);
+	packet(0x0873,36,clif->pStoragePassword,0);
+#endif
+
 /* PacketKeys: http://hercules.ws/board/topic/1105-hercules-wpe-free-june-14th-patch/ */
 #if PACKETVER >= 20110817
 	packetKeys(0x053D5CED,0x3DED6DED,0x6DED6DED); /* Thanks to Shakto */
@@ -2451,6 +2547,14 @@ packet(0x020d,-1);
 
 #if PACKETVER >= 20130618
 	packetKeys(0x434115DE,0x34A10FE9,0x6791428E); /* Thanks to Shakto */
+#endif
+
+#if PACKETVER >= 20130626
+	packetKeys(0x38F453EF,0x6A040FD8,0X65BD6668); /* Thanks to Shakto */
+#endif
+
+#if PACKETVER >= 20130703
+	packetKeys(0x4FF90E23,0x0F1432F2,0x4CFA1EDA); /* Thanks to Shakto */
 #endif
 
 
