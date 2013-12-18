2fbfedddcd5b7b67eb465e162a4de08ffcb1b298
shennetsind <ind@henn.et>
2013-12-18 16:47:55 -0200

    Follow up a23d072a66d2569ba13921522be3c82ae9aad576
    just updating the headers since they were modified.
    
    Signed-off-by: shennetsind <ind@henn.et>

--- src/common/nullpo.h
+++ src/common/nullpo.h
@@ -1,5 +1,6 @@
-// Copyright (c) Athena Dev Teams - Licensed under GNU GPL
-// For more information, see LICENCE in the main folder
+// Copyright (c) Hercules Dev Team, licensed under GNU GPL.
+// See the LICENSE file
+// Portions Copyright (c) Athena Dev Teams
 
 #ifndef COMMON_NULLPO_H
 #define COMMON_NULLPO_H
