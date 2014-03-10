HerculesWS/Hercules

commit 4f3156b78f807553b11f37a7d0c898f6e358495b

Ref redesign, part 3
- Updated several functions to properly use the data structures defined
  in parts 1 and 2.
- Special thanks to Ind for his help on this, as well as the previous
  two parts.
- Fixes some crashes related to variable references passed back and
  forth between nested callfunc/callsub scopes (related: 2669e9a,
  3259f89, 4f2de07)

Signed-off-by: Haru <haru@dotalux.com>

src/map/script.h
@@ -355,7 +355,7 @@ struct script_data {
 		char *str;
 		struct script_retinfo* ri;
 	} u;
-	struct DBMap** ref;
+	struct reg_db *ref;
 };
 
 // Moved defsp from script_state to script_stack since
@@ -563,9 +563,9 @@ struct script_interface {
 	const char* (*conv_str) (struct script_state *st,struct script_data *data);
 	TBL_PC *(*rid2sd) (struct script_state *st);
 	void (*detach_rid) (struct script_state* st);
-	struct script_data* (*push_val)(struct script_stack* stack, enum c_op type, int64 val, struct DBMap** ref);
+	struct script_data* (*push_val)(struct script_stack* stack, enum c_op type, int64 val, struct reg_db *ref);
 	struct script_data *(*get_val) (struct script_state* st, struct script_data* data);
-	void* (*get_val2) (struct script_state* st, int64 uid, struct DBMap** ref);
+	void* (*get_val2) (struct script_state* st, int64 uid, struct reg_db *ref);
 	struct script_data* (*push_str) (struct script_stack* stack, enum c_op type, char* str);
 	struct script_data* (*push_copy) (struct script_stack* stack, int pos);
 	void (*pop_stack) (struct script_state* st, int start, int end);
@@ -589,7 +589,7 @@ struct script_interface {
 	int (*add_str) (const char* p);
 	const char* (*get_str) (int id);
 	int (*search_str) (const char* p);
-	void (*setd_sub) (struct script_state *st, struct map_session_data *sd, const char *varname, int elem, void *value, struct DBMap **ref);
+	void (*setd_sub) (struct script_state *st, struct map_session_data *sd, const char *varname, int elem, void *value, struct reg_db *ref);
 	void (*attach_state) (struct script_state* st);
 	/* */
 	struct hQueue *(*queue) (int idx);
@@ -628,9 +628,9 @@ struct script_interface {
 	void (*read_constdb) (void);
 	const char* (*print_line) (StringBuf *buf, const char *p, const char *mark, int line);
 	void (*errorwarning_sub) (StringBuf *buf, const char *src, const char *file, int start_line, const char *error_msg, const char *error_pos);
-	int (*set_reg) (struct script_state *st, TBL_PC *sd, int64 num, const char *name, const void *value, struct DBMap **ref);
+	int (*set_reg) (struct script_state *st, TBL_PC *sd, int64 num, const char *name, const void *value, struct reg_db *ref);
 	void (*stack_expand) (struct script_stack *stack);
-	struct script_data* (*push_retinfo) (struct script_stack *stack, struct script_retinfo *ri, DBMap **ref);
+	struct script_data* (*push_retinfo) (struct script_stack *stack, struct script_retinfo *ri, struct reg_db *ref);
 	void (*op_3) (struct script_state *st, int op);
 	void (*op_2str) (struct script_state *st, int op, const char *s1, const char *s2);
 	void (*op_2num) (struct script_state *st, int op, int i1, int i2);
@@ -674,15 +674,15 @@ struct script_interface {
 	/**
 	 * Array Handling
 	 **/
-	struct DBMap *(*array_src) (struct script_state *st, struct map_session_data *sd, const char *name, struct DBMap **ref);
-	void (*array_update) (struct DBMap **src, int64 num, bool empty);
-	void (*array_delete) (struct DBMap *src, struct script_array *sa);
-	void (*array_remove_member) (struct DBMap *src, struct script_array *sa, unsigned int idx);
+	struct reg_db *(*array_src) (struct script_state *st, struct map_session_data *sd, const char *name, struct reg_db *ref);
+	void (*array_update) (struct reg_db *src, int64 num, bool empty);
+	void (*array_delete) (struct reg_db *src, struct script_array *sa);
+	void (*array_remove_member) (struct reg_db *src, struct script_array *sa, unsigned int idx);
 	void (*array_add_member) (struct script_array *sa, unsigned int idx);
-	unsigned int (*array_size) (struct script_state *st, struct map_session_data *sd, const char *name, struct DBMap** ref);
-	unsigned int (*array_highest_key) (struct script_state *st, struct map_session_data *sd, const char *name, struct DBMap** ref);
+	unsigned int (*array_size) (struct script_state *st, struct map_session_data *sd, const char *name, struct reg_db *ref);
+	unsigned int (*array_highest_key) (struct script_state *st, struct map_session_data *sd, const char *name, struct reg_db *ref);
 	int (*array_free_db) (DBKey key, DBData *data, va_list ap);
-	void (*array_ensure_zero) (struct script_state *st, struct map_session_data *sd, int64 uid, struct DBMap** ref);
+	void (*array_ensure_zero) (struct script_state *st, struct map_session_data *sd, int64 uid, struct reg_db *ref);
 	/* */
 	void (*reg_destroy_single) (struct map_session_data *sd, int64 reg, struct script_reg_state *data);
 	int (*reg_destroy) (DBKey key, DBData *data, va_list ap);

src/map/mapreg_sql.c
@@ -64,7 +64,7 @@ bool mapreg_setreg(int64 uid, int val) {
 			}
 		} else {
 			if( i )
-				script->array_update(&mapreg->regs.arrays, uid, false);
+				script->array_update(&mapreg->regs, uid, false);
 
 			m = ers_alloc(mapreg->ers, struct mapreg_save);
 
@@ -83,7 +83,7 @@ bool mapreg_setreg(int64 uid, int val) {
 		}
 	} else { // val == 0
 		if( i )
-			script->array_update(&mapreg->regs.arrays, uid, true);
+			script->array_update(&mapreg->regs, uid, true);
 		if( (m = i64db_get(mapreg->regs.vars, uid)) ) {
 			ers_free(mapreg->ers, m);
 		}
@@ -113,7 +113,7 @@ bool mapreg_setregstr(int64 uid, const char* str) {
 
 	if( str == NULL || *str == 0 ) {
 		if( i )
-			script->array_update(&mapreg->regs.arrays, uid, true);
+			script->array_update(&mapreg->regs, uid, true);
 		if(name[1] != '@') {
 			if( SQL_ERROR == SQL->Query(map->mysql_handle, "DELETE FROM `%s` WHERE `varname`='%s' AND `index`='%d'", mapreg->table, name, i) )
 				Sql_ShowDebug(map->mysql_handle);
@@ -135,7 +135,7 @@ bool mapreg_setregstr(int64 uid, const char* str) {
 			}
 		} else {
 			if( i )
-				script->array_update(&mapreg->regs.arrays, uid, false);
+				script->array_update(&mapreg->regs, uid, false);
 
 			m = ers_alloc(mapreg->ers, struct mapreg_save);
 

src/map/pc.c
@@ -8127,11 +8127,11 @@ void pc_setreg(struct map_session_data* sd, int64 reg, int val) {
 	if( val ) {
 		i64db_iput(sd->regs.vars, reg, val);
 		if( index )
-			script->array_update(&sd->regs.arrays, reg, false);
+			script->array_update(&sd->regs, reg, false);
 	} else {
 		i64db_remove(sd->regs.vars, reg);
 		if( index )
-			script->array_update(&sd->regs.arrays, reg, true);
+			script->array_update(&sd->regs, reg, true);
 	}
 }
 
@@ -8166,7 +8166,7 @@ void pc_setregstr(struct map_session_data* sd, int64 reg, const char* str) {
 			ers_free(pc->str_reg_ers, p);
 		} else {
 			if( index )
-				script->array_update(&sd->regs.arrays, reg, false);
+				script->array_update(&sd->regs, reg, false);
 		}
 	} else {
 		if( sd->regs.vars->remove(sd->regs.vars, DB->i642key(reg), &prev) ) {
@@ -8175,7 +8175,7 @@ void pc_setregstr(struct map_session_data* sd, int64 reg, const char* str) {
 				aFree(p->value);
 			ers_free(pc->str_reg_ers, p);
 			if( index )
-				script->array_update(&sd->regs.arrays, reg, true);
+				script->array_update(&sd->regs, reg, true);
 		}
 	}
 }
@@ -8265,12 +8265,12 @@ int pc_setregistry(struct map_session_data *sd, int64 reg, int val) {
 	if( (p = i64db_get(sd->regs.vars, reg) ) ) {
 		if( val ) {
 			if( !p->value && index ) /* its a entry that was deleted, so we reset array */
-				script->array_update(&sd->regs.arrays, reg, false);
+				script->array_update(&sd->regs, reg, false);
 			p->value = val;
 		} else {
 			p->value = 0;
 			if( index )
-				script->array_update(&sd->regs.arrays, reg, true);
+				script->array_update(&sd->regs, reg, true);
 		}
 		if( !pc->reg_load )
 			p->flag.update = 1;/* either way, it will require either delete or replace */
@@ -8278,7 +8278,7 @@ int pc_setregistry(struct map_session_data *sd, int64 reg, int val) {
 		DBData prev;
 		
 		if( index )
-			script->array_update(&sd->regs.arrays, reg, false);
+			script->array_update(&sd->regs, reg, false);
 		
 		p = ers_alloc(pc->num_reg_ers, struct script_reg_num);
 		
@@ -8318,12 +8318,12 @@ int pc_setregistry_str(struct map_session_data *sd, int64 reg, const char *val) 
 			if( p->value )
 				aFree(p->value);
 			else if ( index ) /* a entry that was deleted, so we reset */
-				script->array_update(&sd->regs.arrays, reg, false);
+				script->array_update(&sd->regs, reg, false);
 			p->value = aStrdup(val);
 		} else {
 			p->value = NULL;
 			if( index )
-				script->array_update(&sd->regs.arrays, reg, true);
+				script->array_update(&sd->regs, reg, true);
 		}
 		if( !pc->reg_load )
 			p->flag.update = 1;/* either way, it will require either delete or replace */
@@ -8331,7 +8331,7 @@ int pc_setregistry_str(struct map_session_data *sd, int64 reg, const char *val) 
 		DBData prev;
 
 		if( index )
-			script->array_update(&sd->regs.arrays, reg, false);
+			script->array_update(&sd->regs, reg, false);
 
 		p = ers_alloc(pc->str_reg_ers, struct script_reg_str);
 		

commit 1a4a16ee5c798a6ef9c583fb3f6fc63a7c0ae35e

Fixed a crash caused by NPC variable references in callfunc
- Fixes bugreport:8074, thanks to ahmadshidqi
  http://hercules.ws/board/tracker/issue-8074-help-crash/
- Also fixed some DBMap allocation mistakes introduced in 4f3156b.
- Added testcases to the self-test script.
- Made possible thanks to Ind.

Signed-off-by: Haru <haru@dotalux.com>

src/map/script.c
@@ -3130,8 +3130,7 @@ struct script_data* push_copy(struct script_stack* stack, int pos) {
 
 /// Removes the values in indexes [start,end[ from the stack.
 /// Adjusts all stack pointers.
-void pop_stack(struct script_state* st, int start, int end)
-{
+void pop_stack(struct script_state* st, int start, int end) {
 	struct script_stack* stack = st->stack;
 	struct script_data* data;
 	int i;
@@ -3153,6 +3152,10 @@ void pop_stack(struct script_state* st, int start, int end)
 		{
 			struct script_retinfo* ri = data->u.ri;
 			if( ri->scope.vars ) {
+				// Note: This is necessary evern if we're also doing it in run_func
+				// (in the RETFUNC block) because not all functions return.  If a
+				// function (or a sub) has an 'end' or a 'close', it'll reach this
+				// block with its scope vars still to be freed.
 				script->free_vars(ri->scope.vars);
 				ri->scope.vars = NULL;
 			}
@@ -3224,6 +3227,8 @@ struct script_state* script_alloc_state(struct script_code* rootscript, int pos,
 
 	st = ers_alloc(script->st_ers, struct script_state);
 	st->stack = ers_alloc(script->stack_ers, struct script_stack);
+	st->pending_refs = NULL;
+	st->pending_ref_count = 0;
 	st->stack->sp = 0;
 	st->stack->sp_max = 64;
 	CREATE(st->stack->stack_data, struct script_data, st->stack->sp_max);
@@ -3280,12 +3285,31 @@ void script_free_state(struct script_state* st) {
 			}
 		}
 		st->pos = -1;
+		if (st->pending_ref_count > 0) {
+			while (st->pending_ref_count > 0)
+				aFree(st->pending_refs[--st->pending_ref_count]);
+			aFree(st->pending_refs);
+			st->pending_refs = NULL;
+		}
 		idb_remove(script->st_db, st->id);
 		ers_free(script->st_ers, st);
 		if( --script->active_scripts == 0 ) {
 			script->next_id = 0;
 		}
 	}
+}
+
+/**
+ * Adds a pending reference entry to the current script.
+ *
+ * @see struct script_state::pending_refs
+ *
+ * @param st[in]  Script state.
+ * @param ref[in] Reference to be added.
+ */
+void script_add_pending_ref(struct script_state *st, struct reg_db *ref) {
+	RECREATE(st->pending_refs, struct reg_db*, ++st->pending_ref_count);
+	st->pending_refs[st->pending_ref_count-1] = ref;
 }
 
 //
@@ -4883,7 +4907,10 @@ BUILDIN(callfunc)
 	st->stack->defsp = st->stack->sp;
 	st->state = GOTO;
 	st->stack->scope.vars = i64db_alloc(DB_OPT_RELEASE_DATA);
-	st->stack->scope.arrays = i64db_alloc(DB_OPT_BASE);
+	st->stack->scope.arrays = idb_alloc(DB_OPT_BASE);
+
+	if( !st->script->local.vars )
+		st->script->local.vars = i64db_alloc(DB_OPT_RELEASE_DATA);
 
 	return true;
 }
@@ -4934,7 +4961,7 @@ BUILDIN(callsub)
 	st->stack->defsp = st->stack->sp;
 	st->state = GOTO;
 	st->stack->scope.vars = i64db_alloc(DB_OPT_RELEASE_DATA);
-	st->stack->scope.arrays = i64db_alloc(DB_OPT_BASE);
+	st->stack->scope.arrays = idb_alloc(DB_OPT_BASE);
 
 	return true;
 }
@@ -4977,30 +5004,30 @@ BUILDIN(getarg)
 ///
 /// return;
 /// return <value>;
-BUILDIN(return)
-{
+BUILDIN(return) {
 	if( script_hasdata(st,2) )
 	{// return value
 		struct script_data* data;
 		script_pushcopy(st, 2);
 		data = script_getdatatop(st, -1);
-		if( data_isreference(data) )
-		{
+		if( data_isreference(data) ) {
 			const char* name = reference_getname(data);
-			if( name[0] == '.' && name[1] == '@' )
-			{// scope variable
+			if( name[0] == '.' && name[1] == '@' ) {
+				// scope variable
 				if( !data->ref || data->ref->vars == st->stack->scope.vars )
 					script->get_val(st, data);// current scope, convert to value
 				if( data->ref && data->ref->vars == st->stack->stack_data[st->stack->defsp-1].u.ri->scope.vars )
 					data->ref = NULL; // Reference to the parent scope, remove reference pointer
-			}
-			else if( name[0] == '.' && !data->ref )
-			{// script variable, link to current script
+			} else if( name[0] == '.' && !data->ref ) {
+				// script variable without a reference set, link to current script
 				data->ref = (struct reg_db *)aCalloc(sizeof(struct reg_db), 1);
+				script->add_pending_ref(st, data->ref);
 				data->ref->vars = st->script->local.vars;
 				if( !st->script->local.arrays )
 					st->script->local.arrays = idb_alloc(DB_OPT_BASE);
 				data->ref->arrays = st->script->local.arrays;
+			} else if ( name[0] == '.' /* && data->ref != NULL */ ) {
+				data->ref = NULL; // Reference to the parent scope's script, remove reference pointer.
 			}
 		}
 	}
@@ -19231,6 +19258,7 @@ void script_defaults(void) {
 	script->free_vars = script_free_vars;
 	script->alloc_state = script_alloc_state;
 	script->free_state = script_free_state;
+	script->add_pending_ref = script_add_pending_ref;
 	script->run_autobonus = script_run_autobonus;
 	script->cleararray_pc = script_cleararray_pc;
 	script->setarray_pc = script_setarray_pc;

src/map/script.h
@@ -401,6 +401,8 @@ struct hQueueIterator {
 
 struct script_state {
 	struct script_stack* stack;
+	struct reg_db **pending_refs; ///< References to .vars returned by sub-functions, pending deletion.
+	int pending_ref_count;        ///< Amount of pending_refs currently stored.
 	int start,end;
 	int pos;
 	enum e_script_state state;
@@ -589,6 +591,7 @@ struct script_interface {
 	void (*free_vars) (struct DBMap *var_storage);
 	struct script_state* (*alloc_state) (struct script_code* rootscript, int pos, int rid, int oid);
 	void (*free_state) (struct script_state* st);
+	void (*add_pending_ref) (struct script_state *st, struct reg_db *ref);
 	void (*run_autobonus) (const char *autobonus,int id, int pos);
 	void (*cleararray_pc) (struct map_session_data* sd, const char* varname, void* value);
 	void (*setarray_pc) (struct map_session_data* sd, const char* varname, uint32 idx, void* value, int* refcache);