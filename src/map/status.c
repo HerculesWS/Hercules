73ab0c441de0f74c2e621a594df7e269ce89bfc5
shennetsind <ind@henn.et>
2014-03-10 17:03:08 -0300

    Fixed Bug 8046
    Fixed issue with permanent statuses being removed during logout for memory-freeing operations, special thanks to streusel and all those who contributed to the bug report.
    http://hercules.ws/board/tracker/issue-8046-cart-disappearance/
    
    Signed-off-by: shennetsind <ind@henn.et>

--- src/map/status.c
+++ src/map/status.c
@@ -9605,7 +9605,7 @@ int status_change_end_(struct block_list* bl, enum sc_type type, int tid, const 
 	if (sce->timer != tid && tid != INVALID_TIMER)
 		return 0;
 
-	if( sd && sce->timer == INVALID_TIMER )
+	if( sd && sce->timer == INVALID_TIMER && !sd->state.loggingout )
 		chrif->del_scdata_single(sd->status.account_id,sd->status.char_id,type);
 	
 	if (tid == INVALID_TIMER) {
