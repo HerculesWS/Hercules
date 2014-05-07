db42e06f31fe98a28dce8cc05c480b897ab8cb99
panikon <panikon@zoho.com>
2014-05-07 20:05:37 -0300

    #Fixed issue 8175 *http://hercules.ws/board/tracker/issue-8175-missing-delequip/ *Documentation wrote by @emistry #Added return values for *delequip

--- src/map/script.c
+++ src/map/script.c
@@ -7969,6 +7969,9 @@ BUILDIN(delequip)
 	if(i >= 0) {
 		pc->unequipitem(sd,i,3); //recalculate bonus
 		pc->delitem(sd,i,1,0,2,LOG_TYPE_SCRIPT);
+		script_pushint(st,1);
+	} else {
+		script_pushint(st,0);
 	}
 
 	return true;
