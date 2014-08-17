de54bc8484d7a08219078df68fe8319504278155
Darkelfen <elfengirox00@gmail.com>
2014-08-17 13:54:15 -0400

    Update status.c
    [Ranger] Unlimit not giving + 50*skill level % damage bonus

--- src/map/status.c
+++ src/map/status.c
@@ -9021,6 +9021,9 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 			case SC_DARKCROW:
 				val2 = 30 * val1;
 				break;
+			case SC_UNLIMIT:
+				val2 = 50 * val1;
+				break;
 			case SC_MONSTER_TRANSFORM:
 				if( !mob->db_checkid(val1) )
 					val1 = 1002; // default poring
