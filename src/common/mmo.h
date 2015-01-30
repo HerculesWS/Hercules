7bd70dbe0ed2d004e7863b1575db5c6fbf126f5c
Alvin Angeles <alag@netop.com>
2015-01-30 15:10:47 +0800

    Local Server Setup
    Setting up the local server to work with 127.0.0.1 - Current packetver
    2014-10-22

--- src/common/mmo.h
+++ src/common/mmo.h
@@ -49,7 +49,7 @@
 // 20120307 - 2012-03-07aRagexeRE+ - 0x970
 
 #ifndef PACKETVER
-	#define PACKETVER 20141022
+	#define PACKETVER 20141022 //20140115
 #endif // PACKETVER
 
 //Uncomment the following line if your client is ragexeRE instead of ragexe (required because of conflicting packets in ragexe vs ragexeRE).
