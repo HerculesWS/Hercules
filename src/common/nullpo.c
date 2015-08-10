62a7e575b45d7955c5100961e3610975857ce81d
Haru <haru@dotalux.com>
2015-08-10 03:14:36 +0200

    Fixed a compile error in Cygwin
    - Follow-up to c5ff02958806ea5a672dcc9371602a6c090c6758
    - Cygwin doesn't support backtrace() nor it provides execinfo.h
    
    Signed-off-by: Haru <haru@dotalux.com>

--- src/common/nullpo.c
+++ src/common/nullpo.c
@@ -12,7 +12,7 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
-#ifdef __GNUC__
+#if defined(__GNUC__) && !defined(CYGWIN)
 #include <execinfo.h>
 #endif
 
@@ -28,7 +28,7 @@ struct nullpo_interface nullpo_s;
  * @param title      Message title to display (i.e. failed assertion or nullpo info)
  */
 void assert_report(const char *file, int line, const char *func, const char *targetname, const char *title) {
-#ifdef __GNUC__
+#if defined(__GNUC__) && !defined(CYGWIN)
 	void *array[10];
 	int size;
 	char **strings;
@@ -42,7 +42,7 @@ void assert_report(const char *file, int line, const char *func, const char *tar
 
 	ShowError("--- %s --------------------------------------------\n", title);
 	ShowError("%s:%d: '%s' in function `%s'\n", file, line, targetname, func);
-#ifdef __GNUC__
+#if defined(__GNUC__) && !defined(CYGWIN)
 	size = (int)backtrace(array, 10);
 	strings = backtrace_symbols(array, size);
 	for (i = 0; i < size; i++)
