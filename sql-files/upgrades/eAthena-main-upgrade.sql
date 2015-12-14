b9c2af357dba7a6281f32517603b49e2cb682c53
maqc1 <alex--223@hotmail.com>
2015-12-13 21:12:32 -0500

    Split eathena-upgrade in two files
    Split eathena-upgrade in two files, one for main and one for logs (as is it for rAthena-upgrade)
    
    Closes #946 as merged

--- sql-files/upgrades/eAthena-main-upgrade.sql
+++ sql-files/upgrades/eAthena-main-upgrade.sql
@@ -24,21 +24,12 @@
 
 ALTER TABLE `global_reg_value` MODIFY `type` TINYINT(1) UNSIGNED NOT NULL DEFAULT '3';
 
--- Adds 'I' and 'X' to `type` in `picklog` table
-ALTER TABLE `picklog` MODIFY `type` ENUM('M','P','L','T','V','S','N','C','A','R','G','E','B','O','I','X') NOT NULL DEFAULT 'P';
-
--- Adds 'D' and 'U' to `type` in `picklog` table
-ALTER TABLE `picklog` MODIFY `type` ENUM('M','P','L','T','V','S','N','C','A','R','G','E','B','O','I','X','D','U') NOT NULL DEFAULT 'P';
-
 -- `ExpPer` column removed from `mob_db` and `mob_db2` tables
 ALTER TABLE `mob_db` DROP COLUMN `ExpPer`;
 ALTER TABLE `mob_db2` DROP COLUMN `ExpPer`;
 
 -- Rename `level` column to `group_id` in `login` table
 ALTER TABLE `login` CHANGE COLUMN `level` `group_id` TINYINT(3) NOT NULL DEFAULT '0';
-
--- Adds 'I' to `type` in `zenylog`
-ALTER TABLE `zenylog` MODIFY `type` ENUM('M','T','V','S','N','A','E','B','I') NOT NULL DEFAULT 'S';
 
 ALTER TABLE `char` ADD COLUMN `elemental_id` INT(11) UNSIGNED NOT NULL DEFAULT '0';
 
@@ -61,9 +52,6 @@ CREATE TABLE IF NOT EXISTS `elemental` (
   PRIMARY KEY (`ele_id`)
 ) ENGINE=MyISAM;
 
--- Adds 'D' to `type` in `zenylog`
-ALTER TABLE `zenylog` MODIFY `type` ENUM('M','T','V','S','N','A','E','B','I','D') NOT NULL DEFAULT 'S';
-
 ALTER TABLE `char` ADD CONSTRAINT `name_key` UNIQUE (`name`);
 
 ALTER TABLE `inventory`  ADD COLUMN `favorite` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0' AFTER `expire_time`;
@@ -80,8 +68,6 @@ ALTER TABLE `item_db_re` MODIFY `defence` SMALLINT(5) DEFAULT NULL;
 
 ALTER TABLE `item_db` MODIFY `defence` SMALLINT(5) DEFAULT NULL;
 
-ALTER TABLE `zenylog` MODIFY `type` ENUM('T','V','P','M','S','N','D','C','A','E','I','B') NOT NULL DEFAULT 'S';
-
 ALTER TABLE `elemental` CHANGE COLUMN `str` `atk1` MEDIUMINT(6) UNSIGNED NOT NULL DEFAULT 0,
   CHANGE COLUMN `agi` `atk2` MEDIUMINT(6) UNSIGNED NOT NULL DEFAULT 0,
   CHANGE COLUMN `vit` `matk` MEDIUMINT(6) UNSIGNED NOT NULL DEFAULT 0,
@@ -91,8 +77,6 @@ ALTER TABLE `elemental` CHANGE COLUMN `str` `atk1` MEDIUMINT(6) UNSIGNED NOT NUL
   CHANGE COLUMN `life_time` `flee` SMALLINT(4) UNSIGNED NOT NULL DEFAULT 0,
   ADD COLUMN `hit` SMALLINT(4) UNSIGNED NOT NULL DEFAULT 0 AFTER `flee`,
   ADD COLUMN `life_time` INT(11) NOT NULL DEFAULT 0 AFTER `hit`;
-
-ALTER TABLE `picklog` ADD `nsiuid` BIGINT NOT NULL DEFAULT '0' AFTER `card3`;
 
 CREATE TABLE IF NOT EXISTS `interreg` (
   `varname` VARCHAR(11) NOT NULL,
@@ -114,8 +98,6 @@ ALTER TABLE `mail` ADD `nsiuid` BIGINT NOT NULL DEFAULT '0';
 
 ALTER TABLE `storage` ADD `nsiuid` BIGINT NOT NULL DEFAULT '0';
 
-ALTER TABLE `picklog` CHANGE `nsiuid` `unique_id` BIGINT( 20 ) NOT NULL DEFAULT '0';
-
 UPDATE `interreg` SET `varname` = 'unique_id' WHERE `interreg`.`varname` = 'nsiuid';
 
 ALTER TABLE `auction` CHANGE `nsiuid` `unique_id` BIGINT( 20 ) NOT NULL DEFAULT '0';
@@ -129,4 +111,3 @@ ALTER TABLE `inventory` CHANGE `nsiuid` `unique_id` BIGINT( 20 ) NOT NULL DEFAUL
 ALTER TABLE `mail` CHANGE `nsiuid` `unique_id` BIGINT( 20 ) NOT NULL DEFAULT '0';
 
 ALTER TABLE `storage` CHANGE `nsiuid` `unique_id` BIGINT( 20 ) NOT NULL DEFAULT '0';
-
