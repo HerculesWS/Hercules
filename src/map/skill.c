214f71bee4b238d0b6dade86596ca901033080a0
Dastgir <dastgirpojee@rocketmail.com>
2015-12-27 19:00:10 +0530

    SO_ARRULLO: Formula Fixed

--- src/map/skill.c
+++ src/map/skill.c
@@ -9375,7 +9375,7 @@ int skill_castend_nodamage_id(struct block_list *src, struct block_list *bl, uin
 			{
 				// [(15 + 5 * Skill Level) + ( Caster?s INT / 5 ) + ( Caster?s Job Level / 5 ) - ( Target?s INT / 6 ) - ( Target?s LUK / 10 )] %
 				int rate = (15 + 5 * skill_lv) + status_get_int(src)/5 + (sd? sd->status.job_level:0)/5;
-				rate -= status_get_int(bl)/6 - status_get_luk(bl)/10;
+				rate -= status_get_int(bl)/6 + status_get_luk(bl)/10;
 				clif->skill_nodamage(src, bl, skill_id, skill_lv, 1);
 				sc_start2(src,bl, type, rate, skill_lv, 1, skill->get_time(skill_id, skill_lv));
 			}
