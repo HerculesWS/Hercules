0463e4f9b06bc0949b40e1e78985b954a811b5b5
Haru <haru@dotalux.com>
2015-12-29 02:17:26 +0100

    Extended the numeric view ID deprecation to '-1' (FAKE_NPC)
    Signed-off-by: Haru <haru@dotalux.com>

--- src/map/npc.c
+++ src/map/npc.c
@@ -2564,8 +2564,7 @@ int npc_parseview(const char* w4, const char* start, const char* buffer, const c
 	} else {
 		// NPC has an ID specified for view id.
 		val = atoi(w4);
-		if (val != FAKE_NPC) // TODO: Add this to the constants table and replace -1 with FAKE_NPC in the scripts, then remove this check.
-			ShowWarning("npc_parseview: Use of numeric NPC view IDs is deprecated and may be removed in a future update. Please use NPC view constants instead. ID '%d' specified in file '%s', line '%d'.\n", val, filepath, strline(buffer, start-buffer));
+		ShowWarning("npc_parseview: Use of numeric NPC view IDs is deprecated and may be removed in a future update. Please use NPC view constants instead. ID '%d' specified in file '%s', line '%d'.\n", val, filepath, strline(buffer, start-buffer));
 	}
 
 	return val;
