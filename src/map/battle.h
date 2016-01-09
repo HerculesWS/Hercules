e8ab5029f9cf6450e8d60b441c826bd152718dc2
Jedzkie <jedzkie13@rocketmail.com>
2016-01-09 12:00:09 +0800

    Bow UnEquip Official Behavior - When unequip a bow if there's arrow equipped, it will also unequips the arrow. - Added battle configuration.

--- src/map/battle.h
+++ src/map/battle.h
@@ -539,6 +539,8 @@ struct Battle_Config {
 	// BodyStyle
 	int min_body_style, max_body_style;
 	int save_body_style;
+	
+	int bow_unequip_arrow;
 };
 
 /* criteria for battle_config.idletime_critera */
