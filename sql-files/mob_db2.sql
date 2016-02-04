0c5278ff6f1cc291dbd3832352ea66a9e04dfaa9
Hercules.ws <Hercules@efficiently.awesome>
2016-02-04 22:20:42 +0100

    ItemDB SQL Sync
    Signed-off-by: HerculesWSAPI <Hercules@efficiently.awesome>

--- sql-files/mob_db2.sql
+++ sql-files/mob_db2.sql
@@ -1,3 +1,26 @@
+-- This file is part of Hercules.
+-- http://herc.ws - http://github.com/HerculesWS/Hercules
+--
+-- Copyright (C) 2013-2016  Hercules Dev Team
+--
+-- Hercules is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-- NOTE: This file was auto-generated and should never be manually edited,
+--       as it will get overwritten. If you need to modify this file,
+--       please consider modifying the corresponding .conf file inside
+--       the db folder, and then re-run the db2sql plugin.
+
 --
 -- Table structure for table `mob_db2`
 --
@@ -63,3 +86,4 @@ CREATE TABLE `mob_db2` (
   `DropCardper` SMALLINT(9) UNSIGNED NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM;
+
