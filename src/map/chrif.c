177db883383ad71c1bbd4e65443ba422bd9d5c8f
Andrei Karas <akaras@inbox.ru>
2016-02-19 20:18:50 +0300

    Add noreturn attributes.
    Also add compiler flag for check for missing noreturn attributes.

--- src/map/chrif.c
+++ src/map/chrif.c
@@ -110,7 +110,9 @@ struct chrif_interface *chrif;
 #define chrif_check(a) do { if(!chrif->isconnected()) return a; } while(0)
 
 /// Resets all the data.
-void chrif_reset(void) {
+void chrif_reset(void) __attribute__ ((noreturn));
+void chrif_reset(void)
+{
 	// TODO kick everyone out and reset everything [FlavioJS]
 	exit(EXIT_FAILURE);
 }
