4e49441c6520b024ed5303064c38d619644df08c
Haru <haru@dotalux.com>
2016-02-28 02:20:40 +0100

    HPM Hooks Update
    Signed-off-by: Haru <haru@dotalux.com>

--- src/common/HPMSymbols.inc.h
+++ src/common/HPMSymbols.inc.h
@@ -2,7 +2,7 @@
  * This file is part of Hercules.
  * http://herc.ws - http://github.com/HerculesWS/Hercules
  *
- * Copyright (C) 2015-2016  Hercules Dev Team
+ * Copyright (C) 2013-2016  Hercules Dev Team
  *
  * Hercules is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
