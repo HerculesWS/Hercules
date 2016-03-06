a70eed5a8124e8bd07097843231bd14a8e57a47a
Jedzkie <jedzkie13@rocketmail.com>
2016-03-06 09:42:31 +0800

    Increase Character Slots from 9 to 12  - kRO Mains recently increased their character slots from 11 to 12.

--- src/common/mmo.h
+++ src/common/mmo.h
@@ -114,7 +114,11 @@
 
 #define MAX_INVENTORY 100
 //Max number of characters per account. Note that changing this setting alone is not enough if the client is not hexed to support more characters as well.
-#define MAX_CHARS 9
+#if PACKETVER < 20090617
+	#define MAX_CHARS 9
+#else
+	#define MAX_CHARS 12
+#endif
 //Number of slots carded equipment can have. Never set to less than 4 as they are also used to keep the data of forged items/equipment. [Skotlex]
 //Note: The client seems unable to receive data for more than 4 slots due to all related packets having a fixed size.
 #define MAX_SLOTS 4
