820cd423d09ffb31f7af0d850a08e595c35e0a06
root <root@opktest.c.sodium-castle-142008.internal>
2016-10-13 08:01:14 +0000

    แก้เพิ่มค่า Flee 100 เหมือน ROEXE

--- src/map/status.c
+++ src/map/status.c
@@ -4412,7 +4412,7 @@ void status_calc_misc(struct block_list *bl, struct status_data *st, int level)
 	st->matk_min = status->base_matk_min(st);
 	st->matk_max = status->base_matk_max(st);
 	st->hit += level + st->dex;
-	st->flee += level + st->agi;
+	st->flee += level + st->agi+100;
 	st->def2 += st->vit;
 	st->mdef2 += st->int_ + (st->vit >> 1);
 #endif // RENEWAL
