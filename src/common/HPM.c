HerculesWS/Hercules

commit e7c2f7d827ad286dc826e483391e64b8ffe2720b

Automatic HPM symbol sharing
Signed-off-by: Haru <haru@dotalux.com>

src/common/HPM.c
@@ -533,9 +533,6 @@ void hplugins_config_read(void) {
 	if (libconfig->read_file(&plugins_conf, config_filename))
 		return;
 
-	if( HPM->symbol_defaults_sub )
-		HPM->symbol_defaults_sub();
-
 	plist = libconfig->lookup(&plugins_conf, "plugins_list");
 	for (i = 0; i < HPM->cmdline_plugins_count; i++) {
 		config_setting_t *entry = libconfig->setting_add(plist, NULL, CONFIG_TYPE_STRING);
@@ -748,37 +745,6 @@ void HPM_datacheck_final(void) {
 	db_destroy(datacheck_db);
 }
 
-void hplugins_share_defaults(void) {
-	/* core */
-	HPM->share(core,"core");
-	HPM->share(HPMiMalloc, "iMalloc");
-	HPM->share(cmdline,"cmdline");
-	/* console */
-	HPM->share(console,"console");
-	/* db */
-	HPM->share(DB, "DB");
-	/* nullpo */
-	HPM->share(nullpo,"nullpo");
-	/* showmsg */
-	HPM->share(showmsg,"showmsg");
-	/* socket */
-	HPM->share(sockt,"sockt");
-	/* strlib */
-	HPM->share(strlib,"strlib");
-	HPM->share(sv,"sv");
-	HPM->share(StrBuf,"StrBuf");
-	/* sql */
-	HPM->share(SQL,"SQL");
-	/* timer */
-	HPM->share(timer,"timer");
-	/* libconfig */
-	HPM->share(libconfig,"libconfig");
-	/* sysinfo */
-	HPM->share(sysinfo,"sysinfo");
-	/* utils */
-	HPM->share(HCache,"HCache");
-}
-
 void hpm_init(void) {
 	unsigned int i;
 	datacheck_db = NULL;
@@ -809,8 +775,6 @@ void hpm_init(void) {
 		HPM->packets[i] = NULL;
 		HPM->packetsc[i] = 0;
 	}
-
-	HPM->symbol_defaults();
 
 #ifdef CONSOLE_INPUT
 	console->input->addCommand("plugins",CPCMD_A(plugins));
@@ -906,9 +870,7 @@ void hpm_defaults(void) {
 	HPM->iscompatible = hplugin_iscompatible;
 	HPM->import_symbol = hplugin_import_symbol;
 	HPM->share = hplugin_export_symbol;
-	HPM->symbol_defaults = hplugins_share_defaults;
 	HPM->config_read = hplugins_config_read;
-	HPM->symbol_defaults_sub = NULL;
 	HPM->pid2name = hplugins_id2name;
 	HPM->parse_packets = hplugins_parse_packets;
 	HPM->load_sub = NULL;

src/common/HPMi.h
@@ -218,7 +218,9 @@ struct HPMi_interface {
 
 	Sql *sql_handle;
 };
-#ifndef HERCULES_CORE
+#ifdef HERCULES_CORE
+#define HPM_SYMBOL(n, s) (HPM->share((s), (n)), true)
+#else // ! HERCULES_CORE
 HPExport struct HPMi_interface HPMi_s;
 HPExport struct HPMi_interface *HPMi;
 HPExport void *(*import_symbol) (char *name, unsigned int pID);

src/char/HPMchar.c
@@ -62,6 +62,7 @@ void HPM_char_plugin_load_sub(struct hplugin *plugin) {
 
 void HPM_char_do_init(void) {
 	HPM->datacheck_init(HPMDataCheck, HPMDataCheckLen, HPMDataCheckVer);
+	HPM_shared_symbols(SERVER_TYPE_CHAR);
 }
 
 void HPM_char_do_final(void) {

src/char/char.c
@@ -5806,26 +5806,6 @@ void do_shutdown(void)
 	}
 }
 
-void char_hp_symbols(void) {
-	HPM->share(mapindex,"mapindex");
-	HPM->share(chr, "chr");
-	HPM->share(geoip, "geoip");
-	HPM->share(inter_auction, "inter_auction");
-	HPM->share(inter_elemental, "inter_elemental");
-	HPM->share(inter_guild, "inter_guild");
-	HPM->share(inter_homunculus, "inter_homunculus");
-	HPM->share(inter_mail, "inter_mail");
-	HPM->share(inter_mercenary, "inter_mercenary");
-	HPM->share(inter_party, "inter_party");
-	HPM->share(inter_pet, "inter_pet");
-	HPM->share(inter_quest, "inter_quest");
-	HPM->share(inter_storage, "inter_storage");
-	HPM->share(inter, "inter");
-	HPM->share(loginif, "loginif");
-	HPM->share(mapif, "mapif");
-	HPM->share(pincode, "pincode");
-}
-
 /**
  * --char-config handler
  *
@@ -5887,7 +5867,6 @@ int do_init(int argc, char **argv) {
 		chr->server[i].map = NULL;
 
 	HPM_char_do_init();
-	HPM->symbol_defaults_sub = char_hp_symbols;
 	cmdline->exec(argc, argv, CMDLINE_OPT_PREINIT);
 	HPM->config_read();
 	HPM->event(HPET_PRE_INIT);

src/common/HPM.h
@@ -129,9 +129,7 @@ struct HPM_interface {
 	void (*event) (enum hp_event_types type);
 	void *(*import_symbol) (char *name, unsigned int pID);
 	void (*share) (void *, char *);
-	void (*symbol_defaults) (void);
 	void (*config_read) (void);
-	void (*symbol_defaults_sub) (void);//TODO drop
 	char *(*pid2name) (unsigned int pid);
 	unsigned char (*parse_packets) (int fd, enum HPluginPacketHookingPoints point);
 	void (*load_sub) (struct hplugin *plugin);

src/login/HPMlogin.c
@@ -47,6 +47,7 @@ void HPM_login_plugin_load_sub(struct hplugin *plugin) {
 
 void HPM_login_do_init(void) {
 	HPM->datacheck_init(HPMDataCheck, HPMDataCheckLen, HPMDataCheckVer);
+	HPM_shared_symbols(SERVER_TYPE_LOGIN);
 }
 
 void HPM_login_do_final(void) {

src/login/login.c
@@ -1918,11 +1918,6 @@ void do_shutdown_login(void)
 	}
 }
 
-void login_hp_symbols(void) {
-	HPM->share(account_db_sql_up(accounts),"sql_handle");
-	HPM->share(login,"login");
-}
-
 /**
  * --login-config handler
  *
@@ -1980,7 +1975,6 @@ int do_init(int argc, char** argv)
 	login->NET_CONF_NAME   = aStrdup("conf/network.conf");
 
 	HPM_login_do_init();
-	HPM->symbol_defaults_sub = login_hp_symbols;
 	cmdline->exec(argc, argv, CMDLINE_OPT_PREINIT);
 	HPM->config_read();
 	HPM->event(HPET_PRE_INIT);

src/map/HPMmap.c
@@ -186,6 +186,7 @@ void HPM_map_do_init(void) {
 	HPM->load_sub = HPM_map_plugin_load_sub;
 	HPM->grabHPDataSub = HPM_map_grabHPData;
 	HPM->datacheck_init(HPMDataCheck, HPMDataCheckLen, HPMDataCheckVer);
+	HPM_shared_symbols(SERVER_TYPE_MAP);
 }
 
 void HPM_map_do_final(void) {

commit b664a9fe58c26323699bb7f3798092491fd3bd12

Fixed a memory manager crash when a memory leak occurs in a plugin
- Plugins were accidentally using the core's memory manager instead of
  the HPM-safe wrappers.
- As a side-effect of this, plugins shall not be able to hook into the
  iMalloc interface.
- The issue was introduced in e7c2f7d827ad286dc826e483391e64b8ffe2720b

Signed-off-by: Haru <haru@dotalux.com>

src/common/HPM.c
@@ -567,6 +567,7 @@ struct hplugin *hplugin_load(const char* filename)
 	/* id */
 	plugin->hpi->pid                = plugin->idx;
 	/* core */
+	plugin->hpi->memmgr             = HPMiMalloc;
 #ifdef CONSOLE_INPUT
 	plugin->hpi->addCPCommand       = console->input->addCommand;
 #endif // CONSOLE_INPUT
@@ -1093,8 +1094,8 @@ void hpm_init(void)
 
 	HPM->off = false;
 
-	memcpy(&iMalloc_HPM, iMalloc, sizeof(struct malloc_interface));
 	HPMiMalloc = &iMalloc_HPM;
+	*HPMiMalloc = *iMalloc;
 	HPMiMalloc->malloc = HPM_mmalloc;
 	HPMiMalloc->calloc = HPM_calloc;
 	HPMiMalloc->realloc = HPM_realloc;

src/common/HPMi.h
@@ -231,6 +231,7 @@ struct HPMi_interface {
 
 	/* Hooking */
 	struct HPMHooking_interface *hooking;
+	struct malloc_interface *memmgr;
 };
 #ifdef HERCULES_CORE
 #define HPM_SYMBOL(n, s) (HPM->share((s), (n)), true)

src/common/memmgr.h
@@ -101,8 +101,10 @@ struct malloc_interface {
 void malloc_defaults(void);
 
 void memmgr_report(int extra);
-#endif // HERCULES_CORE
 
 HPShared struct malloc_interface *iMalloc;
+#else
+#define iMalloc HPMi->memmgr
+#endif // HERCULES_CORE
 
 #endif /* COMMON_MEMMGR_H */

tools/HPMHookGen/HPMHookGen.pl
@@ -295,6 +295,7 @@ foreach my $file (@files) { # Loop through the xml files
 	my $loc = $data->{compounddef}->{$filekey}->{location}->[0];
 	next unless $loc->{file} =~ /src\/(map|char|login|common)\//;
 	next if $loc->{file} =~ /\/HPM.*\.h/; # Don't allow hooking into the HPM itself
+	next if $loc->{file} =~ /\/memmgr\.h/; # Don't allow hooking into the memory manager
 	my $servertype = $1;
 	my $key = $data->{compounddef}->{$filekey}->{compoundname}->[0];
 	my $original = $key;
@@ -338,8 +339,6 @@ foreach my $file (@files) { # Loop through the xml files
 		$key = "chr";
 	} elsif ($key eq "db_interface") {
 		$key = "DB";
-	} elsif ($key eq "malloc_interface") {
-		$key = "iMalloc";
 	} elsif ($key eq "socket_interface") {
 		$key = "sockt";
 	} elsif ($key eq "sql_interface") {