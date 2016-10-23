HerculesWS/Hercules

commit b0006317e5cc4a48cd5b8a501b94813cd3b93670

Replaced hardcoded values with constants for status_change_start's flag
- Please use the values from enum scstart_flag when calling
  status_change_start or related functions.
- This also applies to the sc_start script commands.

Signed-off-by: Haru <haru@dotalux.com>

src/map/status.c
@@ -6311,9 +6311,12 @@ void status_change_init(struct block_list *bl) {
 	memset(sc, 0, sizeof (struct status_change));
 }
 
-//Applies SC defense to a given status change.
-//Returns the adjusted duration based on flag values.
-//the flag values are the same as in status->change_start.
+/**
+ * Applies SC defense to a given status change.
+ *
+ * @see status_change_start for the expected parameters.
+ * @return the adjusted duration based on flag values.
+ */
 int status_get_sc_def(struct block_list *src, struct block_list *bl, enum sc_type type, int rate, int tick, int flag) {
 	//Percentual resistance: 10000 = 100% Resist
 	//Example: 50% -> sc_def=5000 -> 25%; 5000ms -> tick_def=5000 -> 2500ms
@@ -6606,7 +6609,7 @@ int status_get_sc_def(struct block_list *src, struct block_list *bl, enum sc_typ
 		tick_def = sc_def;
 
 	//Natural resistance
-	if (!(flag&8)) {
+	if (!(flag&SCFLAG_FIXEDRATE)) {
 		rate -= rate*sc_def/10000;
 		rate -= sc_def2;
 
@@ -6640,7 +6643,7 @@ int status_get_sc_def(struct block_list *src, struct block_list *bl, enum sc_typ
 	if (tick < 1) return 1;
 
 	//Rate reduction
-	if (flag&2)
+	if (flag&SCFLAG_FIXEDTICK)
 		return tick;
 
 	tick -= tick*tick_def/10000;
@@ -6727,18 +6730,23 @@ void status_display_remove(struct map_session_data *sd, enum sc_type type) {
 		}
 	}
 }
-/*==========================================
-* Starts a status change.
-* 'type' = type, 'val1~4' depend on the type.
-* 'rate' = base success rate. 10000 = 100%
-* 'tick' is base duration
-* 'flag':
-* &1 : Cannot be avoided (it has to start)
-* &2 : Tick should not be reduced (by vit, luk, lv, etc)
-* &4 : sc_data loaded, no value has to be altered.
-* &8 : rate should not be reduced (not evaluated here, but in some calls to other functions)
-* &16: Status icon (SI) should not be send
-*------------------------------------------*/
+/**
+ * Starts a status change.
+ *
+ * @param src  Status change source bl.
+ * @param bl   Status change target bl.
+ * @param type Status change type.
+ * @param rate Base success rate. 1 means 0.01%, 10000 means 100%.
+ * @param val1 Additional value (meaning depends on type).
+ * @param val2 Additional value (meaning depends on type).
+ * @param val3 Additional value (meaning depends on type).
+ * @param val4 Additional value (meaning depends on type).
+ * @param tick Base duration (milliseconds).
+ * @param flag Special flags (@see enum scstart_flag).
+ *
+ * @retval 0 if no status change happened.
+ * @retval 1 if the status change was successfully applied.
+ */
 int status_change_start(struct block_list *src, struct block_list *bl, enum sc_type type, int rate, int val1, int val2, int val3, int val4, int tick, int flag) {
 	struct map_session_data *sd = NULL;
 	struct status_change* sc;
@@ -6841,7 +6849,7 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 	sd = BL_CAST(BL_PC, bl);
 
 	//Adjust tick according to status resistances
-	if( !(flag&(1|4)) ) {
+	if( !(flag&(SCFLAG_NOAVOID|SCFLAG_LOADED)) ) {
 		tick = status->get_sc_def(src, bl, type, rate, tick, flag);
 		if( !tick ) return 0;
 	}
@@ -6871,7 +6879,7 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 				return 0;
 		case SC_FREEZE:
 			//Undead are immune to Freeze/Stone
-			if (undead_flag && !(flag&1))
+			if (undead_flag && !(flag&SCFLAG_NOAVOID))
 				return 0;
 		case SC_SLEEP:
 		case SC_STUN:
@@ -6980,7 +6988,7 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 			break;
 			//Strip skills, need to divest something or it fails.
 		case SC_NOEQUIPWEAPON:
-			if (sd && !(flag&4)) { //apply sc anyway if loading saved sc_data
+			if (sd && !(flag&SCFLAG_LOADED)) { //apply sc anyway if loading saved sc_data
 				int i;
 				opt_flag = 0; //Reuse to check success condition.
 				if(sd->bonus.unstripable_equip&EQP_WEAPON)
@@ -6998,7 +7006,7 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 		case SC_NOEQUIPSHIELD:
 			if( val2 == 1 ) val2 = 0; //GX effect. Do not take shield off..
 			else
-				if (sd && !(flag&4)) {
+				if (sd && !(flag&SCFLAG_LOADED)) {
 					int i;
 					if(sd->bonus.unstripable_equip&EQP_SHIELD)
 						return 0;
@@ -7010,7 +7018,7 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 				if (tick == 1) return 1; //Minimal duration: Only strip without causing the SC
 				break;
 		case SC_NOEQUIPARMOR:
-			if (sd && !(flag&4)) {
+			if (sd && !(flag&SCFLAG_LOADED)) {
 				int i;
 				if(sd->bonus.unstripable_equip&EQP_ARMOR)
 					return 0;
@@ -7022,7 +7030,7 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 			if (tick == 1) return 1; //Minimal duration: Only strip without causing the SC
 			break;
 		case SC_NOEQUIPHELM:
-			if (sd && !(flag&4)) {
+			if (sd && !(flag&SCFLAG_LOADED)) {
 				int i;
 				if(sd->bonus.unstripable_equip&EQP_HELM)
 					return 0;
@@ -7135,7 +7143,7 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 	}
 
 	//Check for BOSS resistances
-	if(st->mode&MD_BOSS && !(flag&1)) {
+	if(st->mode&MD_BOSS && !(flag&SCFLAG_NOAVOID)) {
 		if (type>=SC_COMMON_MIN && type <= SC_COMMON_MAX)
 			return 0;
 		switch (type) {
@@ -7533,7 +7541,7 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 
 	vd = status->get_viewdata(bl);
 	calc_flag = status->ChangeFlagTable[type];
-	if(!(flag&4)) { //&4 - Do not parse val settings when loading SCs
+	if(!(flag&SCFLAG_LOADED)) { // Do not parse val settings when loading SCs
 		switch(type) {
 			case SC_ADORAMUS:
 				sc_start(src,bl,SC_BLIND,100,val1,skill->get_time(status->sc2skill(type),val1));
@@ -7544,16 +7552,16 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 				break;
 			case SC_ENDURE:
 				val2 = 7; // Hit-count [Celest]
-				if( !(flag&1) && (bl->type&(BL_PC|BL_MER)) && !map_flag_gvg(bl->m) && !map->list[bl->m].flag.battleground && !val4 ) {
+				if( !(flag&SCFLAG_NOAVOID) && (bl->type&(BL_PC|BL_MER)) && !map_flag_gvg(bl->m) && !map->list[bl->m].flag.battleground && !val4 ) {
 					struct map_session_data *tsd;
 					if( sd ) {
 						int i;
 						for( i = 0; i < 5; i++ ) {
 							if( sd->devotion[i] && (tsd = map->id2sd(sd->devotion[i])) )
-								status->change_start(bl, &tsd->bl, type, 10000, val1, val2, val3, val4, tick, 17);
+								status->change_start(bl, &tsd->bl, type, 10000, val1, val2, val3, val4, tick, SCFLAG_ALL);
 						}
 					} else if( bl->type == BL_MER && ((TBL_MER*)bl)->devotion_flag && (tsd = ((TBL_MER*)bl)->master) )
-						status->change_start(bl, &tsd->bl, type, 10000, val1, val2, val3, val4, tick, 17);
+						status->change_start(bl, &tsd->bl, type, 10000, val1, val2, val3, val4, tick, SCFLAG_ALL);
 				}
 				//val4 signals infinite endure (if val4 == 2 it is infinite endure from Berserk)
 				if( val4 )
@@ -7641,16 +7649,16 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 				break;
 			case SC_REFLECTSHIELD:
 				val2=10+val1*3; // %Dmg reflected
-				if( !(flag&1) && (bl->type&(BL_PC|BL_MER)) ) {
+				if( !(flag&SCFLAG_NOAVOID) && (bl->type&(BL_PC|BL_MER)) ) {
 					struct map_session_data *tsd;
 					if( sd ) {
 						int i;
 						for( i = 0; i < 5; i++ ) {
 							if( sd->devotion[i] && (tsd = map->id2sd(sd->devotion[i])) )
-								status->change_start(bl, &tsd->bl, type, 10000, val1, val2, 0, 0, tick, 17);
+								status->change_start(bl, &tsd->bl, type, 10000, val1, val2, 0, 0, tick, SCFLAG_ALL);
 						}
 					} else if( bl->type == BL_MER && ((TBL_MER*)bl)->devotion_flag && (tsd = ((TBL_MER*)bl)->master) )
-						status->change_start(bl, &tsd->bl, type, 10000, val1, val2, 0, 0, tick, 17);
+						status->change_start(bl, &tsd->bl, type, 10000, val1, val2, 0, 0, tick, SCFLAG_ALL);
 				}
 				break;
 			case SC_NOEQUIPWEAPON:
@@ -7893,7 +7901,7 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 				break;
 
 			case SC_AUTOGUARD:
-				if( !(flag&1) ) {
+				if( !(flag&SCFLAG_NOAVOID) ) {
 					struct map_session_data *tsd;
 					int i,t;
 					for( i = val2 = 0; i < val1; i++) {
@@ -7905,17 +7913,17 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 						if( sd ) {
 							for( i = 0; i < 5; i++ ) {
 								if( sd->devotion[i] && (tsd = map->id2sd(sd->devotion[i])) )
-									status->change_start(bl, &tsd->bl, type, 10000, val1, val2, 0, 0, tick, 17);
+									status->change_start(bl, &tsd->bl, type, 10000, val1, val2, 0, 0, tick, SCFLAG_ALL);
 							}
 						}
 						else if( bl->type == BL_MER && ((TBL_MER*)bl)->devotion_flag && (tsd = ((TBL_MER*)bl)->master) )
-							status->change_start(bl, &tsd->bl, type, 10000, val1, val2, 0, 0, tick, 17);
+							status->change_start(bl, &tsd->bl, type, 10000, val1, val2, 0, 0, tick, SCFLAG_ALL);
 					}
 				}
 				break;
 
 			case SC_DEFENDER:
-				if (!(flag&1)) {
+				if (!(flag&SCFLAG_NOAVOID)) {
 					val2 = 5 + 15*val1; //Damage reduction
 					val3 = 0; // unused, previously speed adjustment
 					val4 = 250 - 50*val1; //Aspd adjustment
@@ -7926,7 +7934,7 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 						for (i = 0; i < 5; i++) {
 							//See if there are devoted characters, and pass the status to them. [Skotlex]
 							if (sd->devotion[i] && (tsd = map->id2sd(sd->devotion[i])))
-								status->change_start(bl, &tsd->bl,type,10000,val1,5+val1*5,val3,val4,tick,1);
+								status->change_start(bl, &tsd->bl,type,10000,val1,5+val1*5,val3,val4,tick,SCFLAG_NOAVOID);
 						}
 					}
 				}
@@ -8053,7 +8061,9 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 					while( i >= 0 ) {
 						type2 = types[i];
 						if( d_sc->data[type2] )
-							status->change_start(bl, bl, type2, 10000, d_sc->data[type2]->val1, 0, 0, 0, skill->get_time(status->sc2skill(type2),d_sc->data[type2]->val1), (type2 != SC_DEFENDER) ? 16 : 0);
+							status->change_start(bl, bl, type2, 10000, d_sc->data[type2]->val1, 0, 0, 0,
+							                     skill->get_time(status->sc2skill(type2),d_sc->data[type2]->val1),
+							                     (type2 != SC_DEFENDER) ? SCFLAG_NOICON : SCFLAG_NONE);
 						i--;
 					}
 				}
@@ -8452,7 +8462,7 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 				tick_time = 4000; // [GodLesZ] tick time
 				break;
 			case SC_PYREXIA:
-				status->change_start(src, bl,SC_BLIND,10000,val1,0,0,0,30000,11); // Blind status that last for 30 seconds
+				status->change_start(src, bl,SC_BLIND,10000,val1,0,0,0,30000,SCFLAG_NOAVOID|SCFLAG_FIXEDTICK|SCFLAG_FIXEDRATE); // Blind status that last for 30 seconds
 				val4 = tick / 3000;
 				tick_time = 3000; // [GodLesZ] tick time
 				break;
@@ -9050,7 +9060,7 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 		}
 	}
 
-	/* values that must be set regardless of flag&4 e.g. val_flag */
+	/* values that must be set regardless of SCFLAG_LOADED e.g. val_flag */
 	switch(type) {
 		case SC_FIGHTINGSPIRIT:
 			val_flag |= 1|2;
@@ -9462,7 +9472,7 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 		calc_flag&=~SCB_DYE;
 	}
 
-	if(!(flag&16) && !(flag&4 && status->DisplayType[type]))
+	if(!(flag&SCFLAG_NOICON) && !(flag&SCFLAG_LOADED && status->DisplayType[type]))
 		clif->status_change(bl,status->IconChangeTable[type],1,tick,(val_flag&1)?val1:1,(val_flag&2)?val2:0,(val_flag&4)?val3:0);
 
 	/**
@@ -11655,7 +11665,7 @@ int status_change_spread( struct block_list *src, struct block_list *bl ) {
 			data.val2 = sc->data[i]->val2;
 			data.val3 = sc->data[i]->val3;
 			data.val4 = sc->data[i]->val4;
-			status->change_start(src,bl,(sc_type)i,10000,data.val1,data.val2,data.val3,data.val4,data.tick,1|2|8);
+			status->change_start(src,bl,(sc_type)i,10000,data.val1,data.val2,data.val3,data.val4,data.tick,SCFLAG_NOAVOID|SCFLAG_FIXEDTICK|SCFLAG_FIXEDRATE);
 			flag = 1;
 		}
 	}

db/pre-re/item_db.conf
@@ -415,7 +415,7 @@ item_db: (
 	BuyingStore: true
 	Script: <"
 		itemheal rand(105,145),0;
-		sc_start SC_FREEZE,10000,0,2500,0;
+		sc_start SC_FREEZE,10000,0,2500,SCFLAG_NONE;
 	">
 },
 {
@@ -545,7 +545,7 @@ item_db: (
 	BuyingStore: true
 	Script: <"
 		itemheal rand(50,100),0;
-		sc_start SC_STUN,3000,0,1500,0;
+		sc_start SC_STUN,3000,0,1500,SCFLAG_NONE;
 	">
 },
 {
@@ -597,7 +597,7 @@ item_db: (
 	Script: <"
 		itemheal rand(105,145),0;
 		sc_start SC_STUN,3000,0;
-		sc_start SC_BLIND,2000,0,1500,0;
+		sc_start SC_BLIND,2000,0,1500,SCFLAG_NONE;
 	">
 },
 {
@@ -1858,7 +1858,7 @@ item_db: (
 	Weight: 300
 	BuyingStore: true
 	Script: <"
-		sc_start SC_DPOISON,10000,0,1000,0;
+		sc_start SC_DPOISON,10000,0,1000,SCFLAG_NONE;
 		sc_start SC_POISON,50000,0;
 	">
 },
@@ -61832,7 +61832,7 @@ item_db: (
 	}
 	Script: <"
 		percentheal 9,0;
-		sc_start SC_CURSE,30000,0,3000,0;
+		sc_start SC_CURSE,30000,0,3000,SCFLAG_NONE;
 	">
 },
 {
@@ -61926,7 +61926,7 @@ item_db: (
 	}
 	Script: <"
 		percentheal 0,9;
-		sc_start SC_SILENCE,30000,0,3000,0;
+		sc_start SC_SILENCE,30000,0,3000,SCFLAG_NONE;
 	">
 },
 {
@@ -63140,7 +63140,7 @@ item_db: (
 	Weight: 30
 	Script: <"
 		percentheal 50,50;
-		sc_start SC_CONFUSION,10000,0,1000,0;
+		sc_start SC_CONFUSION,10000,0,1000,SCFLAG_NONE;
 	">
 },
 {
@@ -64657,7 +64657,7 @@ item_db: (
 	Buy: 20
 	Weight: 100
 	Script: <"
-		sc_start SC_DPOISON,10000,0,1000,0;
+		sc_start SC_DPOISON,10000,0,1000,SCFLAG_NONE;
 		sc_start SC_POISON,50000,0;
 	">
 },
@@ -64669,7 +64669,7 @@ item_db: (
 	Buy: 20
 	Weight: 100
 	Script: <"
-		sc_start SC_DPOISON,10000,0,1000,0;
+		sc_start SC_DPOISON,10000,0,1000,SCFLAG_NONE;
 		sc_start SC_POISON,50000,0;
 	">
 },
@@ -69085,7 +69085,7 @@ item_db: (
 	Buy: 2
 	Weight: 100
 	BuyingStore: true
-	Script: <" sc_start SC_PARALYSE,300000,0,10,2; ">
+	Script: <" sc_start SC_PARALYSE,300000,0,10,SCFLAG_FIXEDTICK; ">
 },
 {
 	Id: 12718
@@ -69095,7 +69095,7 @@ item_db: (
 	Buy: 2
 	Weight: 100
 	BuyingStore: true
-	Script: <" sc_start SC_LEECHESEND,300000,0,10,2; ">
+	Script: <" sc_start SC_LEECHESEND,300000,0,10,SCFLAG_FIXEDTICK; ">
 },
 {
 	Id: 12719
@@ -69105,7 +69105,7 @@ item_db: (
 	Buy: 2
 	Weight: 100
 	BuyingStore: true
-	Script: <" sc_start SC_OBLIVIONCURSE,300000,0,10,2; ">
+	Script: <" sc_start SC_OBLIVIONCURSE,300000,0,10,SCFLAG_FIXEDTICK; ">
 },
 {
 	Id: 12720
@@ -69115,7 +69115,7 @@ item_db: (
 	Buy: 2
 	Weight: 100
 	BuyingStore: true
-	Script: <" sc_start SC_DEATHHURT,300000,0,10,2; ">
+	Script: <" sc_start SC_DEATHHURT,300000,0,10,SCFLAG_FIXEDTICK; ">
 },
 {
 	Id: 12721
@@ -69125,7 +69125,7 @@ item_db: (
 	Buy: 2
 	Weight: 100
 	BuyingStore: true
-	Script: <" sc_start SC_TOXIN,300000,0,10,2; ">
+	Script: <" sc_start SC_TOXIN,300000,0,10,SCFLAG_FIXEDTICK; ">
 },
 {
 	Id: 12722
@@ -69135,7 +69135,7 @@ item_db: (
 	Buy: 2
 	Weight: 100
 	BuyingStore: true
-	Script: <" sc_start SC_PYREXIA,300000,0,10,2; ">
+	Script: <" sc_start SC_PYREXIA,300000,0,10,SCFLAG_FIXEDTICK; ">
 },
 {
 	Id: 12723
@@ -69145,7 +69145,7 @@ item_db: (
 	Buy: 2
 	Weight: 100
 	BuyingStore: true
-	Script: <" sc_start SC_MAGICMUSHROOM,300000,0,10,2; ">
+	Script: <" sc_start SC_MAGICMUSHROOM,300000,0,10,SCFLAG_FIXEDTICK; ">
 },
 {
 	Id: 12724
@@ -69155,7 +69155,7 @@ item_db: (
 	Buy: 2
 	Weight: 100
 	BuyingStore: true
-	Script: <" sc_start SC_VENOMBLEED,15000,0,10,2; ">
+	Script: <" sc_start SC_VENOMBLEED,15000,0,10,SCFLAG_FIXEDTICK; ">
 },
 //  Rune Knight's Rune Stones
 {

db/re/item_db.conf
@@ -449,7 +449,7 @@ item_db: (
 	BuyingStore: true
 	Script: <"
 		itemheal rand(105,145),0;
-		sc_start SC_FREEZE,10000,0,2500,0;
+		sc_start SC_FREEZE,10000,0,2500,SCFLAG_NONE;
 	">
 },
 {
@@ -588,7 +588,7 @@ item_db: (
 	BuyingStore: true
 	Script: <"
 		itemheal rand(50,100),0;
-		sc_start SC_STUN,3000,0,1500,0;
+		sc_start SC_STUN,3000,0,1500,SCFLAG_NONE;
 	">
 },
 {
@@ -645,7 +645,7 @@ item_db: (
 	Script: <"
 		itemheal rand(105,145),0;
 		sc_start SC_STUN,3000,0;
-		sc_start SC_BLIND,2000,0,1500,0;
+		sc_start SC_BLIND,2000,0,1500,SCFLAG_NONE;
 	">
 },
 {
@@ -2020,7 +2020,7 @@ item_db: (
 	Upper: 63
 	BuyingStore: true
 	Script: <"
-		sc_start SC_DPOISON,10000,0,1000,0;
+		sc_start SC_DPOISON,10000,0,1000,SCFLAG_NONE;
 		sc_start SC_POISON,50000,0;
 	">
 },
@@ -74670,7 +74670,7 @@ item_db: (
 	}
 	Script: <"
 		percentheal 9,0;
-		sc_start SC_CURSE,30000,0,3000,0;
+		sc_start SC_CURSE,30000,0,3000,SCFLAG_NONE;
 	">
 },
 {
@@ -74771,7 +74771,7 @@ item_db: (
 	}
 	Script: <"
 		percentheal 0,9;
-		sc_start SC_SILENCE,30000,0,3000,0;
+		sc_start SC_SILENCE,30000,0,3000,SCFLAG_NONE;
 	">
 },
 {
@@ -76085,7 +76085,7 @@ item_db: (
 	Upper: 63
 	Script: <"
 		percentheal 50,50;
-		sc_start SC_CONFUSION,10000,0,1000,0;
+		sc_start SC_CONFUSION,10000,0,1000,SCFLAG_NONE;
 	">
 },
 {
@@ -77703,7 +77703,7 @@ item_db: (
 	Weight: 100
 	Upper: 63
 	Script: <"
-		sc_start SC_DPOISON,10000,0,1000,0;
+		sc_start SC_DPOISON,10000,0,1000,SCFLAG_NONE;
 		sc_start SC_POISON,50000,0;
 	">
 },
@@ -77716,7 +77716,7 @@ item_db: (
 	Weight: 100
 	Upper: 63
 	Script: <"
-		sc_start SC_DPOISON,10000,0,1000,0;
+		sc_start SC_DPOISON,10000,0,1000,SCFLAG_NONE;
 		sc_start SC_POISON,50000,0;
 	">
 },
@@ -83575,7 +83575,7 @@ item_db: (
 	Weight: 20
 	Upper: 63
 	BuyingStore: true
-	Script: <" sc_start SC_PARALYSE,300000,0,10,2; ">
+	Script: <" sc_start SC_PARALYSE,300000,0,10,SCFLAG_FIXEDTICK; ">
 },
 {
 	Id: 12718
@@ -83586,7 +83586,7 @@ item_db: (
 	Weight: 20
 	Upper: 63
 	BuyingStore: true
-	Script: <" sc_start SC_LEECHESEND,300000,0,10,2; ">
+	Script: <" sc_start SC_LEECHESEND,300000,0,10,SCFLAG_FIXEDTICK; ">
 },
 {
 	Id: 12719
@@ -83597,7 +83597,7 @@ item_db: (
 	Weight: 20
 	Upper: 63
 	BuyingStore: true
-	Script: <" sc_start SC_OBLIVIONCURSE,300000,0,10,2; ">
+	Script: <" sc_start SC_OBLIVIONCURSE,300000,0,10,SCFLAG_FIXEDTICK; ">
 },
 {
 	Id: 12720
@@ -83608,7 +83608,7 @@ item_db: (
 	Weight: 20
 	Upper: 63
 	BuyingStore: true
-	Script: <" sc_start SC_DEATHHURT,300000,0,10,2; ">
+	Script: <" sc_start SC_DEATHHURT,300000,0,10,SCFLAG_FIXEDTICK; ">
 },
 {
 	Id: 12721
@@ -83619,7 +83619,7 @@ item_db: (
 	Weight: 20
 	Upper: 63
 	BuyingStore: true
-	Script: <" sc_start SC_TOXIN,300000,0,10,2; ">
+	Script: <" sc_start SC_TOXIN,300000,0,10,SCFLAG_FIXEDTICK; ">
 },
 {
 	Id: 12722
@@ -83630,7 +83630,7 @@ item_db: (
 	Weight: 20
 	Upper: 63
 	BuyingStore: true
-	Script: <" sc_start SC_PYREXIA,300000,0,10,2; ">
+	Script: <" sc_start SC_PYREXIA,300000,0,10,SCFLAG_FIXEDTICK; ">
 },
 {
 	Id: 12723
@@ -83641,7 +83641,7 @@ item_db: (
 	Weight: 20
 	Upper: 63
 	BuyingStore: true
-	Script: <" sc_start SC_MAGICMUSHROOM,300000,0,10,2; ">
+	Script: <" sc_start SC_MAGICMUSHROOM,300000,0,10,SCFLAG_FIXEDTICK; ">
 },
 {
 	Id: 12724
@@ -83652,7 +83652,7 @@ item_db: (
 	Weight: 20
 	Upper: 63
 	BuyingStore: true
-	Script: <" sc_start SC_VENOMBLEED,15000,0,10,2; ">
+	Script: <" sc_start SC_VENOMBLEED,15000,0,10,SCFLAG_FIXEDTICK; ">
 },
 //  Rune Knight's Rune Stones
 {

src/map/atcommand.c
@@ -5469,7 +5469,8 @@ ACMD(autotrade) {
 	sd->state.autotrade = 1;
 	if( battle_config.at_timeout ) {
 		int timeout = atoi(message);
-		status->change_start(NULL,&sd->bl, SC_AUTOTRADE, 10000, 0, 0, 0, 0, ((timeout > 0) ? min(timeout,battle_config.at_timeout) : battle_config.at_timeout) * 60000, 0);
+		status->change_start(NULL,&sd->bl, SC_AUTOTRADE, 10000, 0, 0, 0, 0,
+		                     ((timeout > 0) ? min(timeout,battle_config.at_timeout) : battle_config.at_timeout) * 60000, SCFLAG_NONE);
 	}
 
 	/* currently standalone is not supporting buyingstores, so we rely on the previous method */

src/map/chrif.c
@@ -1178,7 +1178,8 @@ bool chrif_load_scdata(int fd) {
 	
 	for (i = 0; i < count; i++) {
 		data = (struct status_change_data*)RFIFOP(fd,14 + i*sizeof(struct status_change_data));
-		status->change_start(NULL, &sd->bl, (sc_type)data->type, 10000, data->val1, data->val2, data->val3, data->val4, data->tick, 15);
+		status->change_start(NULL, &sd->bl, (sc_type)data->type, 10000, data->val1, data->val2, data->val3, data->val4,
+		                     data->tick, SCFLAG_NOAVOID|SCFLAG_FIXEDTICK|SCFLAG_LOADED|SCFLAG_FIXEDRATE);
 	}
 	
 	pc->scdata_received(sd);

src/map/mercenary.c
@@ -362,7 +362,7 @@ int mercenary_killbonus(struct mercenary_data *md)
 	const enum sc_type scs[] = { SC_MER_FLEE, SC_MER_ATK, SC_MER_HP, SC_MER_SP, SC_MER_HIT };
 	int index = rnd() % ARRAYLENGTH(scs);
 
-    sc_start(NULL,&md->bl, scs[index], 100, rnd() % 5, 600000);
+	sc_start(NULL,&md->bl, scs[index], 100, rnd() % 5, 600000);
 	return 0;
 }
 

src/map/mob.c
@@ -418,7 +418,8 @@ bool mob_ksprotected(struct block_list *src, struct block_list *target) {
 		return true;
 	} while(0);
 
-	status->change_start(NULL, target, SC_KSPROTECTED, 10000, sd->bl.id, sd->state.noks, sd->status.party_id, sd->status.guild_id, battle_config.ksprotection, 0);
+	status->change_start(NULL, target, SC_KSPROTECTED, 10000, sd->bl.id, sd->state.noks,
+	                     sd->status.party_id, sd->status.guild_id, battle_config.ksprotection, SCFLAG_NONE);
 
 	return false;
 }

src/map/script.c
@@ -10087,14 +10087,9 @@ BUILDIN(hideonnpc)
  * sc_start  <effect_id>,<duration>,<val1>{,<rate>,<flag>,{<unit_id>}};
  * sc_start2 <effect_id>,<duration>,<val1>,<val2>{,<rate,<flag>,{<unit_id>}};
  * sc_start4 <effect_id>,<duration>,<val1>,<val2>,<val3>,<val4>{,<rate,<flag>,{<unit_id>}};
- * <flag>
- * 	&1: Cannot be avoided (it has to start)
- * 	&2: Tick should not be reduced (by vit, luk, lv, etc)
- * 	&4: sc_data loaded, no value has to be altered.
- * 	&8: rate should not be reduced
+ * <flag>: @see enum scstart_flag
  */
-BUILDIN(sc_start)
-{
+BUILDIN(sc_start) {
 	TBL_NPC * nd = map->id2nd(st->oid);
 	struct block_list* bl;
 	enum sc_type type;
@@ -10113,11 +10108,11 @@ BUILDIN(sc_start)
 	tick = script_getnum(st,3);
 	val1 = script_getnum(st,4);
 
-	//If from NPC we make default flag 1 to be unavoidable
+	//If from NPC we make default flag SCFLAG_NOAVOID to be unavoidable
 	if(nd && nd->bl.id == npc->fake_nd->bl.id)
-		flag = script_hasdata(st,5+start_type)?script_getnum(st,5+start_type):2;
+		flag = script_hasdata(st,5+start_type) ? script_getnum(st,5+start_type) : SCFLAG_FIXEDTICK;
 	else
-		flag = script_hasdata(st,5+start_type)?script_getnum(st,5+start_type):1;
+		flag = script_hasdata(st,5+start_type) ? script_getnum(st,5+start_type) : SCFLAG_NOAVOID;
 
 	rate = script_hasdata(st,4+start_type)?min(script_getnum(st,4+start_type),10000):10000;
 
@@ -10220,7 +10215,7 @@ BUILDIN(getscrate) {
 		bl = map->id2bl(st->rid);
 
 	if (bl)
-		rate = status->get_sc_def(bl, bl, (sc_type)type, 10000, 10000, 0);
+		rate = status->get_sc_def(bl, bl, (sc_type)type, 10000, 10000, SCFLAG_NONE);
 
 	script_pushint(st,rate);
 	return true;
@@ -16063,7 +16058,7 @@ BUILDIN(mercenary_sc_start) {
 	tick = script_getnum(st,3);
 	val1 = script_getnum(st,4);
 
-	status->change_start(NULL, &sd->md->bl, type, 10000, val1, 0, 0, 0, tick, 2);
+	status->change_start(NULL, &sd->md->bl, type, 10000, val1, 0, 0, 0, tick, SCFLAG_FIXEDTICK);
 	return true;
 }
 

commit 96b70caec928b635797fea4ec48b82a445d5524d

Fixed some incorrect SCFLAG values introduced in b0006317e5cc4a48cd5b8a501b94813cd3b93670
- Their original value was `17` (0x11), which is equivalent to
  `SCFLAG_NOAVOID|SCFLAG_NOICON`, rather than `SCFLAG_ALL` (0x1f)

Signed-off-by: Haru <haru@dotalux.com>

src/map/status.c
@@ -8243,12 +8243,12 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 						int i;
 						for( i = 0; i < MAX_PC_DEVOTION; i++ ) {
 							if (sd->devotion[i] && (tsd = map->id2sd(sd->devotion[i])) != NULL)
-								status->change_start(bl, &tsd->bl, type, 10000, val1, val2, val3, val4, tick, SCFLAG_ALL);
+								status->change_start(bl, &tsd->bl, type, 10000, val1, val2, val3, val4, tick, SCFLAG_NOAVOID|SCFLAG_NOICON);
 						}
 					} else if (bl->type == BL_MER) {
 						struct mercenary_data *mc = BL_UCAST(BL_MER, bl);
 						if (mc->devotion_flag && (tsd = mc->master) != NULL) {
-							status->change_start(bl, &tsd->bl, type, 10000, val1, val2, val3, val4, tick, SCFLAG_ALL);
+							status->change_start(bl, &tsd->bl, type, 10000, val1, val2, val3, val4, tick, SCFLAG_NOAVOID|SCFLAG_NOICON);
 						}
 					}
 				}
@@ -8347,12 +8347,12 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 						int i;
 						for( i = 0; i < MAX_PC_DEVOTION; i++ ) {
 							if (sd->devotion[i] && (tsd = map->id2sd(sd->devotion[i])) != NULL)
-								status->change_start(bl, &tsd->bl, type, 10000, val1, val2, 0, 0, tick, SCFLAG_ALL);
+								status->change_start(bl, &tsd->bl, type, 10000, val1, val2, 0, 0, tick, SCFLAG_NOAVOID|SCFLAG_NOICON);
 						}
 					} else if (bl->type == BL_MER) {
 						struct mercenary_data *mc = BL_UCAST(BL_MER, bl);
 						if (mc->devotion_flag && (tsd = mc->master) != NULL) {
-							status->change_start(bl, &tsd->bl, type, 10000, val1, val2, 0, 0, tick, SCFLAG_ALL);
+							status->change_start(bl, &tsd->bl, type, 10000, val1, val2, 0, 0, tick, SCFLAG_NOAVOID|SCFLAG_NOICON);
 						}
 					}
 				}
@@ -8611,12 +8611,12 @@ int status_change_start(struct block_list *src, struct block_list *bl, enum sc_t
 						if( sd ) {
 							for( i = 0; i < MAX_PC_DEVOTION; i++ ) {
 								if (sd->devotion[i] && (tsd = map->id2sd(sd->devotion[i])) != NULL)
-									status->change_start(bl, &tsd->bl, type, 10000, val1, val2, 0, 0, tick, SCFLAG_ALL);
+									status->change_start(bl, &tsd->bl, type, 10000, val1, val2, 0, 0, tick, SCFLAG_NOAVOID|SCFLAG_NOICON);
 							}
 						} else if (bl->type == BL_MER) {
 							struct mercenary_data *mc = BL_UCAST(BL_MER, bl);
 							if (mc->devotion_flag && (tsd = mc->master) != NULL) {
-								status->change_start(bl, &tsd->bl, type, 10000, val1, val2, 0, 0, tick, SCFLAG_ALL);
+								status->change_start(bl, &tsd->bl, type, 10000, val1, val2, 0, 0, tick, SCFLAG_NOAVOID|SCFLAG_NOICON);
 							}
 						}
 					}