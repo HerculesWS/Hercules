HerculesWS/Hercules

commit f4e8189545642ee0bf87f3da546bddc918682935

Implemented SU_BUNCHOFSHRIMP Skill. Lasts for (30+30*SkillLv) Seconds. Consumes 1 Shrimp. Increases ATK and MATK by 10% for limited time.

src/map/skill.c
@@ -9553,6 +9553,16 @@ int skill_castend_nodamage_id(struct block_list *src, struct block_list *bl, uin
 			clif->skill_nodamage(src,bl,skill_id,skill_lv,1);
 			sc_start(src,bl,type,100,skill_lv,skill->get_time(skill_id,skill_lv));
 			break;
+			break;
+
+		case SU_BUNCHOFSHRIMP:
+			if (sd == NULL || sd->status.party_id == 0 || flag&1) {
+				clif->skill_nodamage(bl, bl, skill_id, skill_lv, sc_start(src, bl, type, 100, skill_lv, skill->get_time(skill_id, skill_lv)));
+			} else if (sd != NULL) {
+				party->foreachsamemap(skill->area_sub, sd, skill->get_splash(skill_id, skill_lv), src, skill_id, skill_lv, tick, flag|BCT_PARTY|1, skill->castend_nodamage_id);
+			}
+			break;
+
 		case GM_SANDMAN:
 			if( tsc ) {
 				if( tsc->opt1 == OPT1_SLEEP )

src/map/status.c
@@ -735,6 +735,7 @@ void initChangeTables(void)
 	status->set_sc(SU_ARCLOUSEDASH, SC_ARCLOUSEDASH, SI_ARCLOUSEDASH, SCB_AGI | SCB_SPEED);
 	add_sc(SU_LUNATICCARROTBEAT, SC_STUN);
 	status->set_sc(SU_TUNAPARTY, SC_TUNAPARTY, SI_TUNAPARTY, SCB_NONE);
+	status->set_sc(SU_BUNCHOFSHRIMP, SC_SHRIMP, SI_SHRIMP, SCB_BATK | SCB_MATK);
 
 	// Elemental Spirit summoner's 'side' status changes.
 	status->set_sc( EL_CIRCLE_OF_FIRE  , SC_CIRCLE_OF_FIRE_OPTION, SI_CIRCLE_OF_FIRE_OPTION, SCB_NONE );

commit 2c60e01501cdce2fb18eba0fa07f6a350ae96d5e

Corrected an issue causing WZ_HEAVENDRIVE to be ineffective
The issue was caused by an evident patch mishap in commit
5e04f472e1ef618d9201fd22e382dca829992b3b. The misplaced piece of code
has now been moved to the most likely place it was supposed to be in.

This commit also removes an extra 'break' introduced by accident in
commit f4e8189545642ee0bf87f3da546bddc918682935 (which caused no harm).

Related: http://herc.ws/board/topic/13672-heaven-driver/

Signed-off-by: Haru <haru@dotalux.com>

src/map/skill.c
@@ -913,6 +913,10 @@ int skill_additional_effect(struct block_list* src, struct block_list *bl, uint1
 			break;
 	#endif
 
+		case WZ_HEAVENDRIVE:
+			status_change_end(bl, SC_SV_ROOTTWIST, INVALID_TIMER);
+			break;
+
 		case WZ_STORMGUST:
 		/**
 		 * Storm Gust counter was dropped in renewal
@@ -9558,7 +9562,6 @@ int skill_castend_nodamage_id(struct block_list *src, struct block_list *bl, uin
 			clif->skill_nodamage(src, bl, skill_id, skill_lv, 1);
 			sc_start(src, bl, type, 100, skill_lv, skill->get_time(skill_id, skill_lv));
 			break;
-			break;
 
 		case SU_BUNCHOFSHRIMP:
 			if (sd == NULL || sd->status.party_id == 0 || flag&1) {
@@ -12321,9 +12324,6 @@ int skill_unit_onplace_timer(struct skill_unit *src, struct block_list *bl, int6
 					map->freeblock_unlock();
 				}
 				break;
-		case WZ_HEAVENDRIVE:
-			status_change_end(bl, SC_SV_ROOTTWIST, INVALID_TIMER);
-			break;
 		/**
 		 * The storm gust counter was dropped in renewal
 		 **/