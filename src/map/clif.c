0c324a9c89d1f75c203f9c55e369f88a89bb79cd
Daciano Benini <dacianobenini@gmail.com>
2017-02-05 22:32:39 -0200

    No quest request for showing saoHp

--- src/map/clif.c
+++ src/map/clif.c
@@ -17644,7 +17644,7 @@ void clif_sao_hp_bar( struct mob_data* md, struct map_session_data *sd ){
 		fd = sd->fd;
 		// m = sd->status.show_equip;
 
-        for (i = 0; i < sd->avail_quests; i++) {
+      //  for (i = 0; i < sd->avail_quests; i++) {
 		//if(sd->quest_log[i].quest_id == 1020){
 				if( PER_HP <= 0 )
 					clif->specialeffect_single(&md->bl, 638, sd->fd);
@@ -17666,8 +17666,8 @@ void clif_sao_hp_bar( struct mob_data* md, struct map_session_data *sd ){
 					clif->specialeffect_single(&md->bl, 742, sd->fd);
 				else if( PER_HP < 100 )
 					clif->specialeffect_single(&md->bl, 743, sd->fd);
-		//}
-		 }
+	//	}
+		// }
 		 
 }
 
