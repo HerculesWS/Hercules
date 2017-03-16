af6d0a9e9a6fe7bbc4a5ae235988af16890e1461
Flipp <mysteriousragnarok@hotmail.com>
2017-03-16 15:31:46 -0400

    Fixed coding style to NPC_LICK

--- src/map/skill.c
+++ src/map/skill.c
@@ -7470,8 +7470,8 @@ int skill_castend_nodamage_id(struct block_list *src, struct block_list *bl, uin
 
 		case NPC_LICK:
 			status_zap(bl, 0, 100);
-			clif->skill_nodamage(src,bl,skill_id,skill_lv,
-				sc_start(src,bl,type,(skill_lv*20),skill_lv,skill->get_time2(skill_id,skill_lv)));
+			clif->skill_nodamage(src, bl, skill_id, skill_lv,
+				sc_start(src, bl, type, (skill_lv * 20), skill_lv, skill->get_time2(skill_id, skill_lv)));
 			break;
 
 		case NPC_SUICIDE:
