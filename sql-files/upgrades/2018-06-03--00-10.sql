f412e41b742fb76f9a6a517e1b11743dbdc66635
smokexyz <sagunkho@hotmail.com>
2017-06-03 00:28:08 +0800

    Add SQL Upgrades for Achievements
    Co-authored-by: "Dastgir" <dastgirp@gmail.com>

---
+++ sql-files/upgrades/2018-06-03--00-10.sql
@@ -0,0 +1,40 @@
+#1527964800
+
+-- This file is part of Hercules.
+-- http://herc.ws - http://github.com/HerculesWS/Hercules
+--
+-- Copyright (C) 2018 Hercules Dev Team
+-- Copyright (C) Smokexyz
+--
+-- Hercules is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+CREATE TABLE `char_achievements` (
+	`char_id` INT(11) UNSIGNED NOT NULL,
+	`ach_id` INT(11) UNSIGNED NOT NULL,
+	`completed_at` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+	`rewarded_at` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+	`obj_0` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+	`obj_1` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+	`obj_2` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+	`obj_3` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+	`obj_4` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+	`obj_5` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+	`obj_6` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+	`obj_7` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+	`obj_8` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+	`obj_9` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+	PRIMARY KEY (`char_id`, `ach_id`)
+) ENGINE=MyISAM;
+
+INSERT INTO `sql_updates` (`timestamp`, `ignored`) VALUES (1527964800, 'No');
