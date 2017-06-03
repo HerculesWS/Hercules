9913f13573d737b4c54c7cad23b77c6a940fe8f4
gumi <mekolat@users.noreply.github.com>
2017-06-03 12:52:53 -0400

    flag misceffect() as deprecated

--- src/map/script.c
+++ src/map/script.c
@@ -23580,7 +23580,7 @@ void script_parse_builtin(void) {
 		BUILDIN_DEF(getskilllist,""),
 		BUILDIN_DEF(clearitem,""),
 		BUILDIN_DEF(classchange,"ii?"),
-		BUILDIN_DEF(misceffect,"i"),
+		BUILDIN_DEF_DEPRECATED(misceffect,"i"),
 		BUILDIN_DEF(playbgm,"s"),
 		BUILDIN_DEF(playbgmall,"s?????"),
 		BUILDIN_DEF(soundeffect,"si"),
