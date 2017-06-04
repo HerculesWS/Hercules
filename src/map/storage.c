0892d558a94fdd165ed1f9667e612fcc769d2bfe
smokexyz <sagunkho@hotmail.com>
2017-06-04 23:58:18 +0800

    Add storage access modes.
    Added support for storage modes:
    STORAGE_ACCESS_VIEW
    STORAGE_ACCESS_GET
    STORAGE_ACCESS_PUT
    STORAGE_ACCESS_ALL

--- src/map/storage.c
+++ src/map/storage.c
@@ -353,6 +353,12 @@ int storage_add_from_inventory(struct map_session_data* sd, struct storage_data 
 
 	Assert_ret(stor->received == true);
 
+	if ((sd->storage.access & STORAGE_ACCESS_PUT) == 0) {
+		clif->delitem(sd, index, amount, DELITEM_NORMAL);
+		clif->additem(sd, index, amount, 0);
+		return 0;
+	}
+
 	if (stor->aggregate >= stst->capacity)
 		return 0; // storage full
 
@@ -390,6 +396,9 @@ int storage_add_to_inventory(struct map_session_data* sd, struct storage_data *s
 
 	Assert_ret(stor->received == true);
 
+	if ((sd->storage.access & STORAGE_ACCESS_GET) == 0)
+		return 0;
+
 	if (index < 0 || index >= VECTOR_LENGTH(stor->item))
 		return 0;
 
@@ -426,6 +435,13 @@ int storage_storageaddfromcart(struct map_session_data* sd, struct storage_data 
 
 	Assert_ret(stor->received == true);
 
+
+	if ((sd->storage.access & STORAGE_ACCESS_PUT) == 0) {
+		clif->delitem(sd, index, amount, DELITEM_NORMAL);
+		clif->additem(sd, index, amount, 0);
+		return 0;
+	}
+
 	if (stor->aggregate >= stst->capacity)
 		return 0; // storage full / storage closed
 
@@ -459,6 +475,9 @@ int storage_storagegettocart(struct map_session_data* sd, struct storage_data *s
 	nullpo_ret(sd);
 
 	Assert_ret(stor->received == true);
+
+	if ((sd->storage.access & STORAGE_ACCESS_GET) == 0)
+		return 0;
 
 	if (index < 0 || index >= VECTOR_LENGTH(stor->item))
 		return 0;
