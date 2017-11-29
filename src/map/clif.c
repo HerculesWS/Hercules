5f8f132fb165bd30b2519aa1e7ce581ca0889557
Andrei Karas <akaras@inbox.ru>
2017-11-29 21:00:00 +0300

    Add missing cutin packet value comment.

--- src/map/clif.c
+++ src/map/clif.c
@@ -2300,6 +2300,7 @@ void clif_viewpoint(struct map_session_data *sd, int npc_id, int type, int x, in
 ///     2 = bottom right corner
 ///     3 = middle of screen, inside a movable window
 ///     4 = middle of screen, movable with a close button, chrome-less
+///   255 = hide
 void clif_cutin(struct map_session_data* sd, const char* image, int type)
 {
 	int fd;
