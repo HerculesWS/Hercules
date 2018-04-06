21430f9a04179268344f90d6482898d64f716b1a
Andrei Karas <akaras@inbox.ru>
2018-04-06 04:50:04 +0300

    Add new packets in packets.h for 20180404 client.

--- src/map/packets.h
+++ src/map/packets.h
@@ -4067,4 +4067,24 @@ packet(0x96e,-1,clif->ackmergeitems);
 #endif
 #endif  // PACKETVER_ZERO
 
+
+#ifndef PACKETVER_ZERO
+// 2018-04-04bRagexe, 2018-04-04cRagexeRE
+#if PACKETVER >= 20180404
+// new packets
+	packet(0x0af9,6,clif->pDull/*,XXX*/);
+	packet(0x0afa,54,clif->pDull/*,XXX*/);
+// changed packet sizes
+	packet(0x0ae7,38,clif->pDull/*,XXX*/);
+#endif
+#endif  // PACKETVER_ZERO
+
+#ifdef PACKETVER_RE
+// 2018-04-04cRagexeRE
+#if PACKETVER >= 20180404
+// changed packet sizes
+	packet(0x0821,102,clif->pDull/*,XXX*/); // AC_OTP_USER
+#endif
+#endif
+
 #endif /* MAP_PACKETS_H */
