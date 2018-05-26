cb07f4d630b9577180057ff4566e4f4d74a7ed33
Asheraf <acheraf1998@gmail.com>
2018-05-26 23:42:02 +0000

    add support for send target in unittalk

--- src/map/atcommand.c
+++ src/map/atcommand.c
@@ -6227,7 +6227,7 @@ ACMD(npctalk)
 	snprintf(temp, sizeof(temp), "%s : %s", name, mes);
 
 	if(ifcolor) clif->messagecolor(&nd->bl,color,temp);
-	else clif->disp_overhead(&nd->bl, temp);
+	else clif->disp_overhead(&nd->bl, temp, AREA_CHAT_WOC, NULL);
 
 	return true;
 }
@@ -6287,7 +6287,7 @@ ACMD(pettalk)
 	}
 
 	snprintf(temp, sizeof temp ,"%s : %s", pd->pet.name, mes);
-	clif->disp_overhead(&pd->bl, temp);
+	clif->disp_overhead(&pd->bl, temp, AREA_CHAT_WOC, NULL);
 
 	return true;
 }
@@ -7062,7 +7062,7 @@ ACMD(homtalk)
 	}
 
 	snprintf(temp, sizeof temp ,"%s : %s", sd->hd->homunculus.name, mes);
-	clif->disp_overhead(&sd->hd->bl, temp);
+	clif->disp_overhead(&sd->hd->bl, temp, AREA_CHAT_WOC, NULL);
 
 	return true;
 }
@@ -7422,7 +7422,7 @@ ACMD(me)
 	}
 
 	safesnprintf(atcmd_output, sizeof(atcmd_output), msg_fd(fd,270), sd->status.name, tempmes); // *%s %s*
-	clif->disp_overhead(&sd->bl, atcmd_output);
+	clif->disp_overhead(&sd->bl, atcmd_output, AREA_CHAT_WOC, NULL);
 
 	return true;
 }
