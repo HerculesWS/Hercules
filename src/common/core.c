HerculesWS/Hercules

commit 22bd368e5d4d8d61a7189d03f52c3afd90c0729e

Added core HPM interface
Signed-off-by: Haru <haru@dotalux.com>

src/common/core.c
@@ -41,11 +41,8 @@
 /// Called when a terminate signal is received.
 void (*shutdown_callback)(void) = NULL;
 
-int runflag = CORE_ST_RUN;
-int arg_c = 0;
-char **arg_v = NULL;
-
-char *SERVER_NAME = NULL;
+struct core_interface core_s;
+struct core_interface *core = &core_s;
 
 #ifndef MINICORE // minimalist Core
 // Added by Gabuzomeu
@@ -90,7 +87,7 @@ static BOOL WINAPI console_handler(DWORD c_event) {
 			if( shutdown_callback != NULL )
 				shutdown_callback();
 			else
-				runflag = CORE_ST_STOP;// auto-shutdown
+				core->runflag = CORE_ST_STOP;// auto-shutdown
 			break;
 		default:
 			return FALSE;
@@ -118,7 +115,7 @@ static void sig_proc(int sn) {
 			if( shutdown_callback != NULL )
 				shutdown_callback();
 			else
-				runflag = CORE_ST_STOP;// auto-shutdown
+				core->runflag = CORE_ST_STOP;// auto-shutdown
 			break;
 		case SIGSEGV:
 		case SIGFPE:
@@ -389,8 +386,9 @@ int main (int argc, char **argv) {
 			SERVER_NAME = ++p1;
 			p2 = p1;
 		}
-		arg_c = argc;
-		arg_v = argv;
+		core->arg_c = argc;
+		core->arg_v = argv;
+		core->runflag = CORE_ST_RUN;
 	}
 	core_defaults();
 
@@ -442,7 +440,7 @@ int main (int argc, char **argv) {
 	do_init(argc,argv);
 
 	// Main runtime cycle
-	while (runflag != CORE_ST_STOP) {
+	while (core->runflag != CORE_ST_STOP) {
 		int next = timer->perform(timer->gettick_nocache());
 		sockt->perform(next);
 	}

src/char/char.c
@@ -3242,7 +3242,7 @@ void char_parse_frommap_char_select_req(int fd)
 	int32 group_id = RFIFOL(fd, 18);
 	RFIFOSKIP(fd,22);
 
-	if( runflag != CHARSERVER_ST_RUNNING )
+	if( core->runflag != CHARSERVER_ST_RUNNING )
 	{
 		chr->select_ack(fd, account_id, 0);
 	}
@@ -3297,7 +3297,7 @@ void char_parse_frommap_change_map_server(int fd)
 		char_data = (struct mmo_charstatus*)uidb_get(chr->char_db_,RFIFOL(fd,14));
 	}
 
-	if (runflag == CHARSERVER_ST_RUNNING && sockt->session_is_active(map_fd) && char_data) {
+	if (core->runflag == CHARSERVER_ST_RUNNING && sockt->session_is_active(map_fd) && char_data) {
 		//Send the map server the auth of this player.
 		struct online_char_data* data;
 		struct char_auth_node* node;
@@ -3778,7 +3778,7 @@ void char_parse_frommap_auth_request(int fd, int id)
 		cd = (struct mmo_charstatus*)uidb_get(chr->char_db_,char_id);
 	}
 
-	if( runflag == CHARSERVER_ST_RUNNING && cd && standalone ) {
+	if( core->runflag == CHARSERVER_ST_RUNNING && cd && standalone ) {
 		cd->sex = sex;
 
 		chr->map_auth_ok(fd, account_id, NULL, cd);
@@ -3786,7 +3786,7 @@ void char_parse_frommap_auth_request(int fd, int id)
 		return;
 	}
 
-	if( runflag == CHARSERVER_ST_RUNNING &&
+	if( core->runflag == CHARSERVER_ST_RUNNING &&
 		cd != NULL &&
 		node != NULL &&
 		node->account_id == account_id &&
@@ -4454,7 +4454,7 @@ void char_parse_char_connect(int fd, struct char_session_data* sd, uint32 ipl)
 	// send back account_id
 	chr->send_account_id(fd, account_id);
 
-	if( runflag != CHARSERVER_ST_RUNNING ) {
+	if( core->runflag != CHARSERVER_ST_RUNNING ) {
 		chr->auth_error(fd, 0);
 		return;
 	}
@@ -4962,7 +4962,7 @@ void char_parse_char_login_map_server(int fd, uint32 ipl)
 	l_pass[23] = '\0';
 
 	ARR_FIND( 0, ARRAYLENGTH(chr->server), i, chr->server[i].fd <= 0 );
-	if (runflag != CHARSERVER_ST_RUNNING ||
+	if (core->runflag != CHARSERVER_ST_RUNNING ||
 		i == ARRAYLENGTH(chr->server) ||
 		strcmp(l_user, chr->userid) != 0 ||
 		strcmp(l_pass, chr->passwd) != 0 ||
@@ -5792,17 +5792,17 @@ void set_server_type(void) {
 /// Called when a terminate signal is received.
 void do_shutdown(void)
 {
-	if( runflag != CHARSERVER_ST_SHUTDOWN )
+	if( core->runflag != CHARSERVER_ST_SHUTDOWN )
 	{
 		int id;
-		runflag = CHARSERVER_ST_SHUTDOWN;
+		core->runflag = CHARSERVER_ST_SHUTDOWN;
 		ShowStatus("Shutting down...\n");
 		// TODO proper shutdown procedure; wait for acks?, kick all characters, ... [FlavoJS]
 		for( id = 0; id < ARRAYLENGTH(chr->server); ++id )
 			mapif->server_reset(id);
 		loginif->check_shutdown();
 		sockt->flush_fifos();
-		runflag = CORE_ST_STOP;
+		core->runflag = CORE_ST_STOP;
 	}
 }
 
@@ -5976,10 +5976,10 @@ int do_init(int argc, char **argv) {
 #endif
 	ShowStatus("The char-server is "CL_GREEN"ready"CL_RESET" (Server is listening on the port %d).\n\n", chr->port);
 
-	if( runflag != CORE_ST_STOP )
+	if( core->runflag != CORE_ST_STOP )
 	{
-		shutdown_callback = do_shutdown;
-		runflag = CHARSERVER_ST_RUNNING;
+		core->shutdown_callback = do_shutdown;
+		core->runflag = CHARSERVER_ST_RUNNING;
 	}
 
 	HPM->event(HPET_READY);

src/char/loginif.c
@@ -39,9 +39,9 @@ void loginif_reset(void)
 /// If all the conditions are met, it stops the core loop.
 void loginif_check_shutdown(void)
 {
-	if( runflag != CHARSERVER_ST_SHUTDOWN )
+	if( core->runflag != CHARSERVER_ST_SHUTDOWN )
 		return;
-	runflag = CORE_ST_STOP;
+	core->runflag = CORE_ST_STOP;
 }
 
 

src/common/HPM.c
@@ -752,11 +752,7 @@ void hplugins_share_defaults(void) {
 	HPM->share(hpm_add_arg,"addArg");
 	HPM->share(hplugins_addconf,"addConf");
 	/* core */
-	HPM->share(&runflag,"runflag");
-	HPM->share(arg_v,"arg_v");
-	HPM->share(&arg_c,"arg_c");
-	HPM->share(SERVER_NAME,"SERVER_NAME");
-	HPM->share(&SERVER_TYPE,"SERVER_TYPE");
+	HPM->share(core,"core");
 	HPM->share(HPMiMalloc, "iMalloc");
 	HPM->share(cmdline,"cmdline");
 	/* console */

src/common/console.c
@@ -102,7 +102,7 @@ int console_parse_key_pressed(void)
  * Stops server
  **/
 CPCMD_C(exit,server) {
-	runflag = 0;
+	core->runflag = 0;
 }
 
 /**

src/common/core.h
@@ -57,27 +57,29 @@ struct cmdline_interface {
 	const char *(*arg_source) (struct CmdlineArgData *arg);
 };
 
+struct core_interface {
+	int arg_c;
+	char **arg_v;
+	/// @see E_CORE_ST
+	int runflag;
+	char *server_name;
+	enum server_types server_type;
+
+	/// Called when a terminate signal is received. (Ctrl+C pressed)
+	/// If NULL, runflag is set to CORE_ST_STOP instead.
+	void (*shutdown_callback)(void);
+};
+
 #define CMDLINEARG(x) bool cmdline_arg_ ## x (const char *name, const char *params)
+#define SERVER_NAME (core->server_name)
+#define SERVER_TYPE (core->server_type)
 
 #ifdef HERCULES_CORE
-extern int arg_c;
-extern char **arg_v;
-
-/// @see E_CORE_ST
-extern int runflag;
-extern char *SERVER_NAME;
-
-enum server_types SERVER_TYPE;
-
 extern void cmdline_args_init_local(void);
 extern int do_init(int,char**);
 extern void set_server_type(void);
 extern void do_abort(void);
 extern int do_final(void);
-
-/// Called when a terminate signal is received. (Ctrl+C pressed)
-/// If NULL, runflag is set to CORE_ST_STOP instead.
-extern void (*shutdown_callback)(void);
 
 /// Special plugin ID assigned to the Hercules core
 #define HPM_PID_CORE ((unsigned int)-1)
@@ -88,6 +90,7 @@ extern void (*shutdown_callback)(void);
 void cmdline_defaults(void);
 #endif // HERCULES_CORE
 
+HPShared struct core_interface *core;
 HPShared struct cmdline_interface *cmdline;
 
 #endif /* COMMON_CORE_H */

src/login/login.c
@@ -294,7 +294,7 @@ void login_fromchar_parse_auth(int fd, int id, const char *const ip)
 	RFIFOSKIP(fd,23);
 
 	node = (struct login_auth_node*)idb_get(login->auth_db, account_id);
-	if( runflag == LOGINSERVER_ST_RUNNING &&
+	if( core->runflag == LOGINSERVER_ST_RUNNING &&
 		node != NULL &&
 		node->account_id == account_id &&
 		node->login_id1  == login_id1 &&
@@ -1192,7 +1192,7 @@ void login_auth_ok(struct login_session_data* sd)
 	nullpo_retv(sd);
 	fd = sd->fd;
 	ip = sockt->session[fd]->client_addr;
-	if( runflag != LOGINSERVER_ST_RUNNING )
+	if( core->runflag != LOGINSERVER_ST_RUNNING )
 	{
 		// players can only login while running
 		login->connection_problem(fd, 1); // 01 = server closed
@@ -1537,7 +1537,7 @@ void login_parse_request_connection(int fd, struct login_session_data* sd, const
 	login_log(sockt->session[fd]->client_addr, sd->userid, 100, message);
 
 	result = login->mmo_auth(sd, true);
-	if (runflag == LOGINSERVER_ST_RUNNING &&
+	if (core->runflag == LOGINSERVER_ST_RUNNING &&
 		result == -1 &&
 		sd->sex == 'S' &&
 		sd->account_id >= 0 &&
@@ -1905,16 +1905,16 @@ void set_server_type(void) {
 /// Called when a terminate signal is received.
 void do_shutdown_login(void)
 {
-	if( runflag != LOGINSERVER_ST_SHUTDOWN )
+	if( core->runflag != LOGINSERVER_ST_SHUTDOWN )
 	{
 		int id;
-		runflag = LOGINSERVER_ST_SHUTDOWN;
+		core->runflag = LOGINSERVER_ST_SHUTDOWN;
 		ShowStatus("Shutting down...\n");
 		// TODO proper shutdown procedure; kick all characters, wait for acks, ...  [FlavioJS]
 		for( id = 0; id < ARRAYLENGTH(server); ++id )
 			chrif_server_reset(id);
 		sockt->flush_fifos();
-		runflag = CORE_ST_STOP;
+		core->runflag = CORE_ST_STOP;
 	}
 }
 
@@ -2033,9 +2033,9 @@ int do_init(int argc, char** argv)
 		exit(EXIT_FAILURE);
 	}
 
-	if( runflag != CORE_ST_STOP ) {
-		shutdown_callback = do_shutdown_login;
-		runflag = LOGINSERVER_ST_RUNNING;
+	if( core->runflag != CORE_ST_STOP ) {
+		core->shutdown_callback = do_shutdown_login;
+		core->runflag = LOGINSERVER_ST_RUNNING;
 	}
 
 	ShowStatus("The login-server is "CL_GREEN"ready"CL_RESET" (Server is listening on the port %u).\n\n", login_config.login_port);

src/map/atcommand.c
@@ -10200,7 +10200,7 @@ bool atcommand_can_use2(struct map_session_data *sd, const char *command, AtComm
 bool atcommand_hp_add(char *name, AtCommandFunc func) {
 	/* if commands are added after group permissions are thrown in, they end up with no permissions */
 	/* so we restrict commands to be linked in during boot */
-	if( runflag == MAPSERVER_ST_RUNNING ) {
+	if( core->runflag == MAPSERVER_ST_RUNNING ) {
 		ShowDebug("atcommand_hp_add: Commands can't be added after server is ready, skipping '%s'...\n",name);
 		return false;
 	}
@@ -10232,7 +10232,7 @@ void atcommand_db_clear(void) {
 }
 
 void atcommand_doload(void) {
-	if( runflag >= MAPSERVER_ST_RUNNING )
+	if( core->runflag >= MAPSERVER_ST_RUNNING )
 		atcommand->cmd_db_clear();
 	if( atcommand->db == NULL )
 		atcommand->db = stridb_alloc(DB_OPT_DUP_KEY|DB_OPT_RELEASE_DATA, ATCOMMAND_LENGTH);

commit 97243c6c325f980fcc90d7c814207070262efc7e

Use the correct shutdown callback when the server receives a termination signal
This fixes a regression introduced in 22bd368e5d4d8d61a7189d03f52c3afd90c0729e

Signed-off-by: Haru <haru@dotalux.com>

src/common/core.c
@@ -80,9 +80,6 @@
 // And don't complain to us if the XYZ plugin you installed wiped your hard disk, or worse.
 // Note: This feature is deprecated, and should not be used.
 
-/// Called when a terminate signal is received.
-void (*shutdown_callback)(void) = NULL;
-
 struct core_interface core_s;
 struct core_interface *core = &core_s;
 
@@ -128,8 +125,8 @@ static BOOL WINAPI console_handler(DWORD c_event)
 		case CTRL_CLOSE_EVENT:
 		case CTRL_LOGOFF_EVENT:
 		case CTRL_SHUTDOWN_EVENT:
-			if( shutdown_callback != NULL )
-				shutdown_callback();
+			if (core->shutdown_callback != NULL)
+				core->shutdown_callback();
 			else
 				core->runflag = CORE_ST_STOP;// auto-shutdown
 			break;
@@ -158,8 +155,8 @@ static void sig_proc(int sn)
 		case SIGTERM:
 			if (++is_called > 3)
 				exit(EXIT_SUCCESS);
-			if( shutdown_callback != NULL )
-				shutdown_callback();
+			if (core->shutdown_callback != NULL)
+				core->shutdown_callback();
 			else
 				core->runflag = CORE_ST_STOP;// auto-shutdown
 			break;