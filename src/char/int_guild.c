HerculesWS/Hercules

commit 735e2e8340c9522ee5a9afdcad29d191da29c8d4

Move some non-mapif functionality from the mapif interface to the inter_homunculus interface
Signed-off-by: Haru <haru@dotalux.com>

src/char/mapif.c
@@ -885,11 +885,6 @@ void mapif_homunculus_deleted(int fd, int flag);
 void mapif_homunculus_loaded(int fd, int account_id, struct s_homunculus *hd);
 void mapif_homunculus_saved(int fd, int account_id, bool flag);
 void mapif_homunculus_renamed(int fd, int account_id, int char_id, unsigned char flag, const char *name);
-bool mapif_homunculus_create(struct s_homunculus *hd);
-bool mapif_homunculus_save(const struct s_homunculus *hd);
-bool mapif_homunculus_load(int homun_id, struct s_homunculus* hd);
-bool mapif_homunculus_delete(int homun_id);
-bool mapif_homunculus_rename(const char *name);
 void mapif_parse_homunculus_create(int fd, int len, int account_id, const struct s_homunculus *phd);
 void mapif_parse_homunculus_delete(int fd, int homun_id);
 void mapif_parse_homunculus_load(int fd, int account_id, int homun_id);
@@ -1098,11 +1093,6 @@ void mapif_defaults(void) {
 	mapif->homunculus_loaded = mapif_homunculus_loaded;
 	mapif->homunculus_saved = mapif_homunculus_saved;
 	mapif->homunculus_renamed = mapif_homunculus_renamed;
-	mapif->homunculus_create = mapif_homunculus_create;
-	mapif->homunculus_save = mapif_homunculus_save;
-	mapif->homunculus_load = mapif_homunculus_load;
-	mapif->homunculus_delete = mapif_homunculus_delete;
-	mapif->homunculus_rename = mapif_homunculus_rename;
 	mapif->parse_homunculus_create = mapif_parse_homunculus_create;
 	mapif->parse_homunculus_delete = mapif_parse_homunculus_delete;
 	mapif->parse_homunculus_load = mapif_parse_homunculus_load;

src/char/char.c
@@ -1811,7 +1811,7 @@ int char_delete_char_sql(int char_id)
 
 	/* remove homunculus */
 	if( hom_id )
-		mapif->homunculus_delete(hom_id);
+		inter_homunculus->delete(hom_id);
 
 	/* remove elemental */
 	if (elemental_id)

src/char/int_homun.c
@@ -119,7 +119,7 @@ void mapif_homunculus_renamed(int fd, int account_id, int char_id, unsigned char
  * @param[in,out] hd The new homunculus' data.
  * @retval false in case of errors.
  */
-bool mapif_homunculus_create(struct s_homunculus *hd)
+bool inter_homunculus_create(struct s_homunculus *hd)
 {
 	char esc_name[NAME_LENGTH*2+1];
 
@@ -146,7 +146,7 @@ bool mapif_homunculus_create(struct s_homunculus *hd)
  * @param hd The homunculus' data.
  * @retval false in case of errors.
  */
-bool mapif_homunculus_save(const struct s_homunculus *hd)
+bool inter_homunculus_save(const struct s_homunculus *hd)
 {
 	bool flag = true;
 	char esc_name[NAME_LENGTH*2+1];
@@ -188,7 +188,7 @@ bool mapif_homunculus_save(const struct s_homunculus *hd)
 }
 
 // Load an homunculus
-bool mapif_homunculus_load(int homun_id, struct s_homunculus* hd)
+bool inter_homunculus_load(int homun_id, struct s_homunculus* hd)
 {
 	char* data;
 	size_t len;
@@ -269,7 +269,7 @@ bool mapif_homunculus_load(int homun_id, struct s_homunculus* hd)
 	return true;
 }
 
-bool mapif_homunculus_delete(int homun_id)
+bool inter_homunculus_delete(int homun_id)
 {
 	if (SQL_ERROR == SQL->Query(inter->sql_handle, "DELETE FROM `%s` WHERE `homun_id` = '%d'", homunculus_db, homun_id)
 	 || SQL_ERROR == SQL->Query(inter->sql_handle, "DELETE FROM `%s` WHERE `homun_id` = '%d'", skill_homunculus_db, homun_id)
@@ -280,7 +280,7 @@ bool mapif_homunculus_delete(int homun_id)
 	return true;
 }
 
-bool mapif_homunculus_rename(const char *name)
+bool inter_homunculus_rename(const char *name)
 {
 	int i;
 
@@ -310,32 +310,32 @@ void mapif_parse_homunculus_create(int fd, int len, int account_id, const struct
 
 	memcpy(&shd, phd, sizeof(shd));
 
-	result = mapif->homunculus_create(&shd);
+	result = inter_homunculus->create(&shd);
 	mapif->homunculus_created(fd, account_id, &shd, result);
 }
 
 void mapif_parse_homunculus_delete(int fd, int homun_id)
 {
-	bool result = mapif->homunculus_delete(homun_id);
+	bool result = inter_homunculus->delete(homun_id);
 	mapif->homunculus_deleted(fd, result);
 }
 
 void mapif_parse_homunculus_load(int fd, int account_id, int homun_id)
 {
 	struct s_homunculus hd;
-	bool result = mapif->homunculus_load(homun_id, &hd);
+	bool result = inter_homunculus->load(homun_id, &hd);
 	mapif->homunculus_loaded(fd, account_id, ( result ? &hd : NULL ));
 }
 
 void mapif_parse_homunculus_save(int fd, int len, int account_id, const struct s_homunculus *phd)
 {
-	bool result = mapif->homunculus_save(phd);
+	bool result = inter_homunculus->save(phd);
 	mapif->homunculus_saved(fd, account_id, result);
 }
 
 void mapif_parse_homunculus_rename(int fd, int account_id, int char_id, const char *name)
 {
-	bool result = mapif->homunculus_rename(name);
+	bool result = inter_homunculus->rename(name);
 	mapif->homunculus_renamed(fd, account_id, char_id, result, name);
 }
 
@@ -365,4 +365,10 @@ void inter_homunculus_defaults(void)
 	inter_homunculus->sql_init = inter_homunculus_sql_init;
 	inter_homunculus->sql_final = inter_homunculus_sql_final;
 	inter_homunculus->parse_frommap = inter_homunculus_parse_frommap;
+
+	inter_homunculus->create = inter_homunculus_create;
+	inter_homunculus->save = inter_homunculus_save;
+	inter_homunculus->load = inter_homunculus_load;
+	inter_homunculus->delete = inter_homunculus_delete;
+	inter_homunculus->rename = inter_homunculus_rename;
 }

src/char/int_homun.h
@@ -22,6 +22,7 @@
 #define CHAR_INT_HOMUN_H
 
 #include "common/hercules.h"
+#include "common/mmo.h"
 
 /**
  * inter_homunculus interface
@@ -30,6 +31,12 @@ struct inter_homunculus_interface {
 	int (*sql_init) (void);
 	void (*sql_final) (void);
 	int (*parse_frommap) (int fd);
+
+	bool (*create) (struct s_homunculus *hd);
+	bool (*save) (const struct s_homunculus *hd);
+	bool (*load) (int homun_id, struct s_homunculus* hd);
+	bool (*delete) (int homun_id);
+	bool (*rename) (const char *name);
 };
 
 #ifdef HERCULES_CORE

src/char/mapif.h
@@ -97,11 +97,6 @@ struct mapif_interface {
 	void (*homunculus_loaded) (int fd, int account_id, struct s_homunculus *hd);
 	void (*homunculus_saved) (int fd, int account_id, bool flag);
 	void (*homunculus_renamed) (int fd, int account_id, int char_id, unsigned char flag, const char *name);
-	bool (*homunculus_create) (struct s_homunculus *hd);
-	bool (*homunculus_save) (const struct s_homunculus *hd);
-	bool (*homunculus_load) (int homun_id, struct s_homunculus* hd);
-	bool (*homunculus_delete) (int homun_id);
-	bool (*homunculus_rename) (const char *name);
 	void (*parse_homunculus_create) (int fd, int len, int account_id, const struct s_homunculus *phd);
 	void (*parse_homunculus_delete) (int fd, int homun_id);
 	void (*parse_homunculus_load) (int fd, int account_id, int homun_id);

commit 6c99adb0b3de4677fdcc28d636aeaa618ce04bb1

Fix a regression in inter_guild_add_member
Fixes #2122
Regression introduced in 735e2e8340c9522ee5a9afdcad29d191da29c8d4, #2108

Signed-off-by: Haru <haru@dotalux.com>

src/char/int_guild.c
@@ -969,21 +969,24 @@ static struct guild *inter_guild_create(const char *name, const struct guild_mem
 }
 
 // Add member to guild
-static bool inter_guild_add_member(int guild_id, const struct guild_member *member)
+static bool inter_guild_add_member(int guild_id, const struct guild_member *member, int map_fd)
 {
 	struct guild * g;
 	int i;
 	nullpo_ret(member);
 
 	g = inter_guild->fromsql(guild_id);
-	if (g == NULL)
+	if (g == NULL) {
+		mapif->guild_memberadded(map_fd, guild_id, member->account_id, member->char_id, 1); // 1: Failed to add
 		return false;
+	}
 
 	// Find an empty slot
 	for (i = 0; i < g->max_member; i++) {
 		if (g->member[i].account_id == 0) {
 			g->member[i] = *member;
 			g->member[i].modified = (GS_MEMBER_NEW | GS_MEMBER_MODIFIED);
+			mapif->guild_memberadded(map_fd, guild_id, member->account_id, member->char_id, 0); // 0: success
 			if (!inter_guild->calcinfo(g)) //Send members if it was not invoked.
 				mapif->guild_info(-1, g);
 
@@ -993,6 +996,8 @@ static bool inter_guild_add_member(int guild_id, const struct guild_member *memb
 			return true;
 		}
 	}
+
+	mapif->guild_memberadded(map_fd, guild_id, member->account_id, member->char_id, 1); // 1: Failed to add
 	return false;
 }
 

src/char/int_guild.h
@@ -70,7 +70,7 @@ struct inter_guild_interface {
 	int (*parse_frommap) (int fd);
 	int (*broken) (int guild_id);
 	struct guild *(*create) (const char *name, const struct guild_member *master);
-	bool (*add_member) (int guild_id, const struct guild_member *member);
+	bool (*add_member) (int guild_id, const struct guild_member *member, int map_fd);
 	bool (*leave) (int guild_id, int account_id, int char_id, int flag, const char *mes, int map_fd);
 	bool (*update_member_info_short) (int guild_id, int account_id, int char_id, int online, int lv, int16 class);
 	bool (*update_member_info) (int guild_id, int account_id, int char_id, int type, const char *data, int len);

src/char/mapif.c
@@ -784,11 +784,7 @@ static int mapif_parse_GuildAddMember(int fd, int guild_id, const struct guild_m
 {
 	nullpo_ret(m);
 
-	if (!inter_guild->add_member(guild_id, m)) {
-		mapif->guild_memberadded(fd, guild_id, m->account_id, m->char_id, 1); // 1: Failed to add
-	} else {
-		mapif->guild_memberadded(fd, guild_id, m->account_id, m->char_id, 0); // 0: success
-	}
+	inter_guild->add_member(guild_id, m, fd);
 	return 0;
 }
 