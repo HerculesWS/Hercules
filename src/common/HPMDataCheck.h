0c6a7a0539fa56c722d820fae9ed486c36f9ab78
Hercules.ws <dev@herc.ws>
2018-07-24 05:23:22 +0200

    HPM Hooks Update
    Signed-off-by: HerculesWSAPI <dev@herc.ws>

--- src/common/HPMDataCheck.h
+++ src/common/HPMDataCheck.h
@@ -541,6 +541,7 @@ HPExport const struct s_HPMDataCheck HPMDataCheck[] = {
 		{ "mapflag_skill_adjust", sizeof(struct mapflag_skill_adjust), SERVER_TYPE_MAP },
 		{ "mapit_interface", sizeof(struct mapit_interface), SERVER_TYPE_MAP },
 		{ "questinfo", sizeof(struct questinfo), SERVER_TYPE_MAP },
+		{ "questinfo_qreq", sizeof(struct questinfo_qreq), SERVER_TYPE_MAP },
 		{ "spawn_data", sizeof(struct spawn_data), SERVER_TYPE_MAP },
 	#else
 		#define MAP_MAP_H
