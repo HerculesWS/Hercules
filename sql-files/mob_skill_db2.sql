addaee6a7e33de6d8e909fbba0d7ccc1ab4c56f9
Hercules.ws <dev@herc.ws>
2018-09-23 16:18:43 +0200

    MobDB SQL Sync
    Signed-off-by: HerculesWSAPI <dev@herc.ws>

--- sql-files/mob_skill_db2.sql
+++ sql-files/mob_skill_db2.sql
@@ -1,9 +1,34 @@
+-- This file is part of Hercules.
+-- http://herc.ws - http://github.com/HerculesWS/Hercules
+--
+-- Copyright (C) 2013-2018  Hercules Dev Team
+--
+-- Hercules is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-- NOTE: This file was auto-generated and should never be manually edited,
+--       as it will get overwritten. If you need to modify this file,
+--       please consider modifying the corresponding .conf file inside
+--       the db folder, and then re-run the db2sql plugin.
+
+-- GENERATED FILE DO NOT EDIT --
+
 --
 -- Table structure for table `mob_skill_db2`
 --
 
 DROP TABLE IF EXISTS `mob_skill_db2`;
-CREATE TABLE IF NOT EXISTS `mob_skill_db2` (
+CREATE TABLE `mob_skill_db2` (
   `MOB_ID` SMALLINT(6) NOT NULL,
   `INFO` TEXT NOT NULL,
   `STATE` TEXT NOT NULL,
@@ -16,11 +41,12 @@ CREATE TABLE IF NOT EXISTS `mob_skill_db2` (
   `TARGET` TEXT NOT NULL,
   `CONDITION` TEXT NOT NULL,
   `CONDITION_VALUE` TEXT,
-  `VAL1` MEDIUMINT(9) DEFAULT NULL,
-  `VAL2` MEDIUMINT(9) DEFAULT NULL,
-  `VAL3` MEDIUMINT(9) DEFAULT NULL,
-  `VAL4` MEDIUMINT(9) DEFAULT NULL,
-  `VAL5` MEDIUMINT(9) DEFAULT NULL,
+  `VAL1` INT(11) DEFAULT NULL,
+  `VAL2` INT(11) DEFAULT NULL,
+  `VAL3` INT(11) DEFAULT NULL,
+  `VAL4` INT(11) DEFAULT NULL,
+  `VAL5` INT(11) DEFAULT NULL,
   `EMOTION` TEXT,
   `CHAT` TEXT
 ) ENGINE=MyISAM;
+
