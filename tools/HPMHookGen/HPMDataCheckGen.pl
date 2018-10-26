579cba117fedaada2cf3213318e696e1794981f2
jrcole2884 <43038957+jrcole2884@users.noreply.github.com>
2018-10-26 09:12:52 -0400

    Update HPMDataCheckGen.pl

--- tools/HPMHookGen/HPMDataCheckGen.pl
+++ tools/HPMHookGen/HPMDataCheckGen.pl
@@ -10,6 +10,8 @@
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
+# We encourage any and all contributers to the project.
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
