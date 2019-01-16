1e28acbaab22d5a92e96a6a19a6e59a92b3f7a46
Andrei Karas <akaras@inbox.ru>
2019-01-16 06:25:45 +0300

    Update shuffle packets up to 2019-01-30

--- src/map/packets_shuffle_zero.h
+++ src/map/packets_shuffle_zero.h
@@ -742,13 +742,16 @@
 	packet(0x0968,clif->pStoragePassword,0);  // CZ_ACK_STORE_PASSWORD // 36
 #endif
 
-// 2018-11-14aRagexe_zero, 2018-11-20aRagexe_zero, 2018-11-28aRagexe_zero, 2018-12-12aRagexe_zero, 2018-12-19aRagexe_zero, 2018-12-26_2aRagexe_zero
+// 2018-11-14aRagexe_zero, 2018-11-20aRagexe_zero, 2018-11-28aRagexe_zero, 2018-12-12aRagexe_zero, 2018-12-19aRagexe_zero, 2018-12-26_2aRagexe_zero, 2019-01-16_2aRagexe_zero, 2019-01-17_1aRagexe_zero, 2019-01-30_2aRagexe_zero
 #if PACKETVER == 20181114 || \
     PACKETVER == 20181120 || \
     PACKETVER == 20181128 || \
     PACKETVER == 20181212 || \
     PACKETVER == 20181219 || \
-    PACKETVER >= 20181226
+    PACKETVER == 20181226 || \
+    PACKETVER == 20190116 || \
+    PACKETVER == 20190117 || \
+    PACKETVER >= 20190130
 	packet(0x0202,clif->pFriendsListAdd,2);  // CZ_ADD_FRIENDS // 26
 	packet(0x022d,clif->pHomMenu,2,4);  // CZ_COMMAND_MER // 5
 	packet(0x023b,clif->pStoragePassword,0);  // CZ_ACK_STORE_PASSWORD // 36
