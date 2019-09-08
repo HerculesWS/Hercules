504d30428dc8d17c8521fd3b4b725963a49b045a
Haru <haru@dotalux.com>
2019-09-08 20:36:29 +0200

    Add support for enum market_buy_result and enum unit_dir to the HPMHookGen
    Signed-off-by: Haru <haru@dotalux.com>

--- tools/HPMHookGen/HPMHookGen.pl
+++ tools/HPMHookGen/HPMHookGen.pl
@@ -246,6 +246,8 @@ sub parse($$) {
 			$rtinit = ' = THREADPRIO_NORMAL';
 		} elsif ($x =~ /^enum\s+market_buy_result$/) { # Known enum market_buy_result
 			$rtinit = ' = MARKET_BUY_RESULT_ERROR';
+		} elsif ($x =~ /^enum\s+unit_dir$/) { # Known enum unit_dir
+			$rtinit = ' = UNIT_DIR_UNDEFINED';
 		} elsif ($x eq 'DBComparator' or $x eq 'DBHasher' or $x eq 'DBReleaser') { # DB function pointers
 			$rtinit = ' = NULL';
 		} elsif ($x =~ /^(?:struct|union)\s+.*$/) { # Structs and unions
