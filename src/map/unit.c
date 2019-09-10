e2045d33de5cae2d749a9355b2f028290fe37bbf
skyleo <skyleo@skyleo.de>
2019-09-10 22:00:14 +0200

    Replace nullpointer-checks with nullpo_retr macros in unit->delay_walktoxy_timer

--- src/map/unit.c
+++ src/map/unit.c
@@ -491,9 +491,8 @@ static int unit_walktoxy_timer(int tid, int64 tick, int id, intptr_t data)
 static int unit_delay_walktoxy_timer(int tid, int64 tick, int id, intptr_t data)
 {
 	struct block_list *bl = map->id2bl(id);
-
-	if (!bl || bl->prev == NULL)
-		return 0;
+	nullpo_retr(0, bl);
+	nullpo_retr(0, bl->prev);
 	unit->walktoxy(bl, (short)((data>>16)&0xffff), (short)(data&0xffff), 0);
 	return 1;
 }
