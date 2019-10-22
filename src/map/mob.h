HerculesWS/Hercules

commit ed72a947a6c97804c1eef5b80bfa49d99f7d7586

Item DB overhaul
http://hercules.ws/board/topic/2954-item-db-file-structure-overhaul

- Item db was changed to libconfig format.
- This new format is larger than the original format, but it's less
  subject to conflicts when some items are edited and the file gets
  updated.
- It is no longer necessary to specify fields with no value, and only
  the actually used fields are specified.
- Items scripts (especially the long ones) are made more readable by
  splitting them into multiple lines, with proper indentation.
- A converter perl script is provided in the tools directory (and a link
  to the web-based version of the same script canbe found in the above
  forum topic)
- All of this was made possible thanks to Ind, Yommy.

Signed-off-by: Haru <haru@dotalux.com>

src/map/pc.c
@@ -983,10 +983,10 @@ int pc_isequip(struct map_session_data *sd,int n)
 		return 0;
 	//Not usable by upper class. [Inkfish]
 	while( 1 ) {
-		if( item->class_upper&1 && !(sd->class_&(JOBL_UPPER|JOBL_THIRD|JOBL_BABY)) ) break;
-		if( item->class_upper&2 && sd->class_&(JOBL_UPPER|JOBL_THIRD) ) break;
-		if( item->class_upper&4 && sd->class_&JOBL_BABY ) break;
-		if( item->class_upper&8 && sd->class_&JOBL_THIRD ) break;
+		if( item->class_upper&ITEMUPPER_NORMAL && !(sd->class_&(JOBL_UPPER|JOBL_THIRD|JOBL_BABY)) ) break;
+		if( item->class_upper&ITEMUPPER_UPPER  &&   sd->class_&(JOBL_UPPER|JOBL_THIRD)            ) break;
+		if( item->class_upper&ITEMUPPER_BABY   &&   sd->class_&JOBL_BABY                          ) break;
+		if( item->class_upper&ITEMUPPER_THIRD  &&   sd->class_&JOBL_THIRD                         ) break;
 		return 0;
 	}
 
@@ -4338,22 +4338,22 @@ int pc_isUseitem(struct map_session_data *sd,int n)
 	//Not usable by upper class. [Haru]
 	while( 1 ) {
 		// Normal classes (no upper, no baby, no third classes)
-		if( item->class_upper&0x01 && !(sd->class_&(JOBL_UPPER|JOBL_THIRD|JOBL_BABY)) ) break;
+		if( item->class_upper&ITEMUPPER_NORMAL && !(sd->class_&(JOBL_UPPER|JOBL_THIRD|JOBL_BABY)) ) break;
 #ifdef RENEWAL
 		// Upper classes (no third classes)
-		if( item->class_upper&0x02 && sd->class_&JOBL_UPPER && !(sd->class_&JOBL_THIRD) ) break;
+		if( item->class_upper&ITEMUPPER_UPPER && sd->class_&JOBL_UPPER && !(sd->class_&JOBL_THIRD) ) break;
 #else
 		//pre-re has no use for the extra, so we maintain the previous for backwards compatibility
-		if( item->class_upper&0x02 && sd->class_&(JOBL_UPPER|JOBL_THIRD) ) break;
+		if( item->class_upper&ITEMUPPER_UPPER && sd->class_&(JOBL_UPPER|JOBL_THIRD) ) break;
 #endif
 		// Baby classes (no third classes)
-		if( item->class_upper&0x04 && sd->class_&JOBL_BABY && !(sd->class_&JOBL_THIRD) ) break;
+		if( item->class_upper&ITEMUPPER_BABY && sd->class_&JOBL_BABY && !(sd->class_&JOBL_THIRD) ) break;
 		// Third classes (no upper, no baby classes)
-		if( item->class_upper&0x08 && sd->class_&JOBL_THIRD && !(sd->class_&(JOBL_UPPER|JOBL_BABY)) ) break;
+		if( item->class_upper&ITEMUPPER_THIRD && sd->class_&JOBL_THIRD && !(sd->class_&(JOBL_UPPER|JOBL_BABY)) ) break;
 		// Upper third classes
-		if( item->class_upper&0x10 && sd->class_&JOBL_THIRD && sd->class_&JOBL_UPPER ) break;
+		if( item->class_upper&ITEMUPPER_THURDUPPER && sd->class_&JOBL_THIRD && sd->class_&JOBL_UPPER ) break;
 		// Baby third classes
-		if( item->class_upper&0x20 && sd->class_&JOBL_THIRD && sd->class_&JOBL_BABY ) break;
+		if( item->class_upper&ITEMUPPER_THIRDBABY && sd->class_&JOBL_THIRD && sd->class_&JOBL_BABY ) break;
 		return 0;
 	}
 

db/item_db2.conf
@@ -1,0 +1,554 @@
+item_db: (
+//  Items Additional Database
+//
+/******************************************************************************
+ ************* Entry structure ************************************************
+ ******************************************************************************
+{
+	// =================== Mandatory fields ===============================
+	Id: ID                        (int)
+	AegisName: "Aegis_Name"       (string, optional if Inherit: true)
+	Name: "Item Name"             (string, optional if Inherit: true)
+	// =================== Optional fields ================================
+	Type: Item Type               (int, defaults to 3 = etc item)
+	Buy: Buy Price                (int, defaults to Sell * 2)
+	Sell: Sell Price              (int, defaults to Buy / 2)
+	Weight: Item Weight           (int, defaults to 0)
+	Atk: Attack                   (int, defaults to 0)
+	Matk: Magical Attack          (int, defaults to 0, ignored in pre-re)
+	Def: Defense                  (int, defaults to 0)
+	Range: Attack Range           (int, defaults to 0)
+	Slots: Slots                  (int, defaults to 0)
+	Job: Job mask                 (int, defaults to all jobs = 0xFFFFFFFF)
+	Upper: Upper mask             (int, defaults to any = 0x3f)
+	Gender: Gender                (int, defaults to both = 2)
+	Loc: Equip location           (int, required value for equipment)
+	WeaponLv: Weapon Level        (int, defaults to 0)
+	EquipLv: Equip required level (int, defaults to 0)
+	EquipLv: [min, max]           (alternative syntax with min / max level)
+	Refine: Refineable            (boolean, defaults to true)
+	View: View ID                 (int, defaults to 0)
+	Script: <"
+		Script
+		(it can be multi-line)
+	">
+	OnEquipScript: <" OnEquip Script (can also be multi-line) ">
+	OnUnequipScript: <" OnUnequip Script (can also be multi-line) ">
+	// =================== Optional fields (item_db2 only) ================
+	Inherit: true/false           (boolean, if true, inherit the values
+	                              that weren't specified, from item_db.conf,
+	                              else override it and use default values)
+},
+******************************************************************************/
+
+//  THQ Quest Items
+// =============================================================
+/*
+{
+	Id: 7950
+	AegisName: "THG_Membership"
+	Name: "THG Membership"
+	Type: 3
+	Sell: 10
+	Weight: 10
+},
+*/
+/*
+{
+	Id: 7951
+	AegisName: "Token_Bag"
+	Name: "Token Bag"
+	Type: 3
+	Sell: 10
+	Weight: 10
+},
+*/
+/*
+{
+	Id: 1998
+	AegisName: "Jeramiah's_Jur"
+	Name: "Jeramiah's Jur"
+	Type: 3
+	Sell: 10
+	Weight: 10
+},
+*/
+/*
+{
+	Id: 1999
+	AegisName: "Zed's_Staff"
+	Name: "Zed's Staff"
+	Type: 3
+	Sell: 10
+	Weight: 10
+},
+*/
+//  Official Event Items that had their Effects removed after the event was completed
+/*
+{
+	Id: 585
+	AegisName: "Wurst"
+	Name: "Brusti"
+	Type: 11
+	Buy: 2
+	Weight: 40
+	Script: <"
+		itemheal rand(15,20),0;
+		itemskill PR_MAGNIFICAT,3;
+	">
+},
+*/
+/*
+{
+	Id: 679
+	AegisName: "Gold_Pill"
+	Name: "Pilule"
+	Type: 0
+	Buy: 5000
+	Weight: 300
+	Script: <" percentheal 50,50; ">
+},
+*/
+/*
+{
+	Id: 2681
+	AegisName: "Republic_Ring"
+	Name: "Republic Anniversary Ring"
+	Type: 5
+	Buy: 20
+	Weight: 100
+	Loc: 136
+	Refine: false
+	Script: <" bonus bAllStats,3; ">
+},
+*/
+/*
+{
+	Id: 5134
+	AegisName: "Pumpkin_Hat"
+	Name: "Pumpkin-Head"
+	Type: 5
+	Buy: 20
+	Weight: 200
+	Def: 2
+	Loc: 256
+	View: 206
+	Script: <" bonus2 bSubRace,RC_Demon,5; ">
+},
+*/
+/*
+{
+	Id: 5136
+	AegisName: "Santa's_Hat_"
+	Name: "Louise's Santa Hat"
+	Type: 5
+	Buy: 20
+	Weight: 100
+	Def: 3
+	Loc: 256
+	View: 20
+	Script: <"
+		bonus bMdef,1;
+		bonus bLuk,1;
+		bonus3 bAutoSpellWhenHit,AL_HEAL,3,50;
+		bonus3 bAutoSpellWhenHit,AL_BLESSING,10,50;
+	">
+},
+*/
+/*
+{
+	Id: 5145
+	AegisName: "Carnival_Joker_Jester"
+	Name: "Carnival Jester"
+	Type: 5
+	Buy: 10
+	Weight: 100
+	Loc: 256
+	View: 89
+	Script: <" bonus bAllStats,3; ">
+},
+*/
+/*
+{
+	Id: 5147
+	AegisName: "Baseball_Cap"
+	Name: "Baseball Cap"
+	Type: 5
+	Buy: 0
+	Weight: 200
+	Def: 3
+	Loc: 256
+	View: 216
+	Script: <"
+		bonus2 bExpAddRace,RC_Boss,50;
+		bonus2 bExpAddRace,RC_NonBoss,50;
+	">
+},
+*/
+/*
+{
+	Id: 5201
+	AegisName: "Party_Hat_B"
+	Name: "2nd Anniversary Party Hat"
+	Type: 5
+	Buy: 20
+	Weight: 300
+	Def: 3
+	Loc: 256
+	View: 144
+	Script: <" bonus bAllStats,3; ">
+},
+*/
+/*
+{
+	Id: 5202
+	AegisName: "Pumpkin_Hat_"
+	Name: "Pumpkin Hat"
+	Type: 5
+	Buy: 20
+	Weight: 200
+	Def: 2
+	Loc: 256
+	View: 206
+	Script: <"
+		bonus bAllStats,2;
+		bonus2 bSubRace,RC_Demon,5;
+		bonus3 bAddMonsterDropItem,529,RC_DemiHuman,1500;
+	">
+},
+*/
+/*
+{
+	Id: 5204
+	AegisName: "Event_Pierrot_Nose"
+	Name: "Rudolf's Red Nose"
+	Type: 5
+	Buy: 20
+	Weight: 100
+	Loc: 1
+	Refine: false
+	View: 49
+	Script: <"
+		bonus2 bResEff,Eff_Blind,3000;
+		bonus2 bAddMonsterDropItem,12130,30;
+	">
+},
+*/
+/*
+{
+	Id: 5264
+	AegisName: "Aussie_Flag_Hat"
+	Name: "Australian Flag Hat"
+	Type: 5
+	Buy: 20
+	Weight: 500
+	Def: 4
+	Loc: 256
+	View: 304
+	Script: <" bonus bAllStats,2; ">
+},
+*/
+/*
+{
+	Id: 5356
+	AegisName: "Pumpkin_Hat_H"
+	Name: "Pumpkin Hat"
+	Type: 5
+	Buy: 20
+	Weight: 200
+	Def: 2
+	Loc: 256
+	View: 206
+	Script: <"
+		bonus bAllStats,2;
+		bonus2 bSubRace,RC_Demon,5;
+		bonus2 bMagicAddRace,RC_Demon,5;
+	">
+},
+*/
+/*
+{
+	Id: 5811
+	AegisName: "Santa_Beard"
+	Name: "Santa Beard"
+	Type: 5
+	Buy: 20
+	Weight: 100
+	Def: 5
+	Loc: 1
+	Refine: false
+	View: 25
+	Script: <" bonus2 bSubRace,RC_Brute,5; ">
+},
+*/
+/*
+{
+	Id: 11702
+	AegisName: "Moon_Cookie"
+	Name: "Moon Cookie"
+	Type: 11
+	Buy: 0
+	Weight: 10
+	Script: <"
+		sc_end SC_POISON;
+		sc_end SC_SILENCE;
+		sc_end SC_BLIND;
+		sc_end SC_CONFUSION;
+		sc_end SC_CURSE;
+		sc_end SC_ILLUSION;
+		itemskill AL_BLESSING,7;
+	">
+},
+*/
+/*
+{
+	Id: 12131
+	AegisName: "Lucky_Potion"
+	Name: "Lucky Potion"
+	Type: 0
+	Buy: 2
+	Weight: 100
+	Script: <" sc_start SC_FOOD_LUK,180000,15; ">
+},
+*/
+/*
+{
+	Id: 12143
+	AegisName: "Red_Can"
+	Name: "Red Can"
+	Type: 2
+	Buy: 50000
+	Weight: 300
+	Script: <" percentheal 25,25; ">
+},
+*/
+// Event effect: Summon monster? Probably Rice_Cake. x_x
+/*
+{
+	Id: 12199
+	AegisName: "Rice_Scroll"
+	Name: "Rice Scroll"
+	Type: 2
+	Buy: 0
+},
+*/
+/*
+{
+	Id: 12200
+	AegisName: "Event_Cake"
+	Name: "Event Cake"
+	Type: 2
+	Buy: 20
+	Weight: 50
+	Script: <" itemskill PR_MAGNIFICAT,3; ">
+},
+*/
+/*
+{
+	Id: 12238
+	AegisName: "New_Year_Rice_Cake_1"
+	Name: "New Year Rice Cake"
+	Type: 0
+	Buy: 20
+	Weight: 100
+	Script: <"
+		percentheal 20,15;
+		sc_start SC_FOOD_STR,1200000,3;
+		sc_start SC_FOOD_INT,1200000,3;
+		sc_start SC_FOOD_LUK,1200000,3;
+		sc_start SC_MOVHASTE_INFINITY,5000,0;
+	">
+},
+*/
+/*
+{
+	Id: 12239
+	AegisName: "New_Year_Rice_Cake_2"
+	Name: "New Year Rice Cake"
+	Type: 0
+	Buy: 20
+	Weight: 100
+	Script: <"
+		percentheal 20,15;
+		sc_start SC_FOOD_DEX,1200000,3;
+		sc_start SC_FOOD_AGI,1200000,3;
+		sc_start SC_FOOD_VIT,1200000,3;
+		sc_start SC_MOVHASTE_INFINITY,5000,0;
+	">
+},
+*/
+//  iRO St. Patrick's Day Event 2008
+// =============================================================
+/*
+{
+	Id: 12715
+	AegisName: "Black_Treasure_Chest"
+	Name: "Black Treasure Chest"
+	Type: 2
+	Buy: 0
+	Weight: 200
+	Script: <" callfunc "F_08stpattyseventbox"; ">
+},
+*/
+//  iRO Valentine's Day Event 2009
+// =============================================================
+/*
+{
+	Id: 12742
+	AegisName: "Valentine_Gift_Box_M"
+	Name: "Valentine Gift Box"
+	Type: 2
+	Buy: 10
+	Script: <" getitem 7946,1; ">
+},
+*/
+/*
+{
+	Id: 12743
+	AegisName: "Valentine_Gift_Box_F"
+	Name: "Valentine Gift Box"
+	Type: 2
+	Buy: 10
+	Script: <" getitem 7947,1; ">
+},
+*/
+/*
+{
+	Id: 12744
+	AegisName: "Chocolate_Box"
+	Name: "Chocolate Box"
+	Type: 2
+	Buy: 10
+	Script: <" getitem 558,1; ">
+},
+*/
+/*
+{
+	Id: 14466
+	AegisName: "Valentine's_Emblem_Box"
+	Name: "Valentine's Emblem Box"
+	Type: 2
+	Buy: 10
+	Script: <" getitem 5817,1; ">
+},
+*/
+/*
+{
+	Id: 7946
+	AegisName: "Gold_Ring_Of_Valentine"
+	Name: "Gold Ring Of Valentine"
+	Type: 3
+	Buy: 10
+},
+*/
+/*
+{
+	Id: 7947
+	AegisName: "Silver_Ring_Of_Valentine"
+	Name: "Silver Ring Of Valentine"
+	Type: 3
+	Buy: 10
+},
+*/
+/*
+{
+	Id: 7948
+	AegisName: "Box"
+	Name: "Box"
+	Type: 3
+	Buy: 10
+	Weight: 10
+},
+*/
+/*
+{
+	Id: 5817
+	AegisName: "Valentine's_Emblem"
+	Name: "Valentine's Emblem"
+	Type: 5
+	Buy: 10
+	Def: 3
+	Loc: 136
+	Refine: false
+	Script: <"
+		bonus bAtkRate,3;
+		bonus bMatkRate,3;
+		bonus bAllStats,2;
+		bonus bFlee,10;
+		bonus bAspd,1;
+		bonus bMdef,3;
+		bonus2 bSkillAtk,"AL_HEAL",10;
+		bonus2 bSkillHeal,AL_HEAL,10;
+		bonus2 bSkillHeal,AM_POTIONPITCHER,10;
+		bonus2 bAddItemHealRate,IG_Potion,10;
+	">
+},
+*/
+//  iRO Halloween Event 2009
+// =============================================================
+/*
+{
+	Id: 5668
+	AegisName: "Weird_Pumpkin_Hat"
+	Name: "Weird Pumpkin Hat"
+	Type: 5
+	Buy: 20
+	Def: 5
+	Loc: 256
+	View: 206
+	Script: <"
+		bonus bMdef,5;
+		bonus2 bAddMonsterDropItem,12192,2500;
+	">
+},
+*/
+/*
+{
+	Id: 6298
+	AegisName: "Crushed_Pumpkin"
+	Name: "Crushed Pumpkin"
+	Type: 3
+	Buy: 0
+},
+*/
+/*
+{
+	Id: 6299
+	AegisName: "Worn_Fabric"
+	Name: "Worn Fabric"
+	Type: 3
+	Buy: 0
+},
+*/
+//  Old Tuxedo and Wedding Dress, will display the outfit when worn.
+// ==================================================================
+/*
+{
+	Id: 2338
+	AegisName: "Wedding_Dress"
+	Name: "Wedding Dress"
+	Type: 5
+	Buy: 43000
+	Weight: 500
+	Job: 0xFFFFFFFE
+	Loc: 16
+	OnEquipScript: <" sc_start SC_WEDDING,-1,0; ">
+	OnUnequipScript: <" sc_end SC_WEDDING; ">
+},
+*/
+/*
+{
+	Id: 7170
+	AegisName: "Tuxedo"
+	Name: "Tuxedo"
+	Type: 5
+	Buy: 43000
+	Weight: 10
+	Job: 0xFFFFFFFE
+	Gender: 1
+	Loc: 16
+	OnEquipScript: <" sc_start SC_WEDDING,-1,0; ">
+	OnUnequipScript: <" sc_end SC_WEDDING; ">
+},
+*/
+)

src/map/itemdb.h
@@ -105,6 +105,17 @@ enum e_chain_cache {
 	ECC_MAX,
 };
 
+enum item_class_upper {
+	ITEMUPPER_NONE       = 0x00,
+	ITEMUPPER_NORMAL     = 0x01,
+	ITEMUPPER_UPPER      = 0x02,
+	ITEMUPPER_BABY       = 0x04,
+	ITEMUPPER_THIRD      = 0x08,
+	ITEMUPPER_THURDUPPER = 0x10,
+	ITEMUPPER_THIRDBABY  = 0x20,
+	ITEMUPPER_ALL        = 0x3f, // Sum of all the above
+};
+
 struct item_data {
 	uint16 nameid;
 	char name[ITEM_NAME_LENGTH],jname[ITEM_NAME_LENGTH];
@@ -125,10 +136,8 @@ struct item_data {
 	int elv;
 	int wlv;
 	int view_id;
-#ifdef RENEWAL
 	int matk;
 	int elvmax;/* maximum level for this item */
-#endif
 
 	int delay;
 //Lupus: I rearranged order of these fields due to compatibility with ITEMINFO script command
@@ -300,7 +309,6 @@ struct itemdb_interface {
 	int (*search_name_array) (struct item_data** data, int size, const char *str, int flag);
 	struct item_data* (*load)(int nameid);
 	struct item_data* (*search)(int nameid);
-	int (*parse_dbrow) (char** str, const char* source, int line, int scriptopt);
 	struct item_data* (*exists) (int nameid);
 	bool (*in_group) (struct item_group *group, int nameid);
 	int (*group_item) (struct item_group *group);
@@ -338,7 +346,9 @@ struct itemdb_interface {
 	int (*combo_split_atoi) (char *str, int *val);
 	void (*read_combos) ();
 	int (*gendercheck) (struct item_data *id);
-	void (*re_split_atoi) (char *str, int *atk, int *matk);
+	int (*validate_entry) (struct item_data *entry, int n, const char *source);
+	int (*readdb_sql_sub) (Sql *handle, int n, const char *source);
+	int (*readdb_libconfig_sub) (config_setting_t *it, int n, const char *source);
 	int (*readdb) (void);
 	int (*read_sqldb) (void);
 	uint64 (*unique_id) (int8 flag, int64 value);

tools/itemdbconverter.pl
@@ -1,0 +1,194 @@
+#!/usr/bin/perl
+#
+# Copyright (c) Hercules Dev Team, licensed under GNU GPL.
+# See the LICENSE file
+# Base Author: Haru @ http://hercules.ws
+#
+# This script converts an item_db(2).txt to the new item_db(2).conf format.
+# usage example: perl tools/itemdbconverter.pl < db/item_db2.txt > db/item_db2.conf
+#
+use strict;
+use warnings;
+
+sub prettifyscript ($) {
+	my ($orig) = @_;
+	$orig =~ s/^[\s\t]*//; $orig =~ s/[\s\t]*$//;
+	return '' unless $orig =~ /[^\s\t]/;
+	my ($p, $script) = ($orig, '');
+	my ($curly, $lines, $comment) = (2, 0, 0);
+	my ($linebreak, $needindent) = (0, 0);
+	while ($p =~ /[^\s\t]/) {
+		$linebreak = 0;
+		if ($comment && $p =~ s|^\s*\*/\s*||) {
+			$comment = 0;
+			next;
+		} elsif ($p =~ s/^\s*({)\s*//) {
+			$curly++ unless $comment;
+			$comment++ if $comment;
+			$script .= " ";
+			$linebreak = 1;
+			$lines++;
+		} elsif ($p =~ s/^\s*(})\s*//) {
+			$curly-- unless $comment;
+			$comment-- if $comment - 1 > 0;
+			$linebreak = 1;
+			$lines++;
+		} elsif ($p =~ s/^\s*(;)\s*//) {
+			if ($p && (!$comment || $p !~ m|^[\s\t]*(?:\*/)[\s\t]*$|)) {
+				$linebreak = 1;
+				$lines++
+			}
+		} elsif ($p =~ s/^("[^"]*")//) {
+		} elsif ($p =~ s|^\s*/\*\s*||) {
+			$comment = 1;
+			next;
+		} elsif ($p !~ s/^(.)//) {
+			last;
+		}
+		$script .= "\t" x $curly if $needindent;
+		$script .= "//" . ("\t" x ($comment-1)) if ($comment && ($needindent || $script eq ''));
+		$script .= "$1";
+		if ($linebreak) {
+			$script .= "\n";
+			$needindent = 1;
+		} else {
+			$needindent = 0;
+		}
+	}
+	if ($curly != 2) {
+		printf STDERR "Parse error, curly braces count ". ($curly-2) .". returning unmodified script:\n$orig\n\n";
+		return $orig;
+	}
+	if ($lines) {
+		$script = "\n\t\t$script\n\t";
+	} else {
+		$script = " $script ";
+	}
+	return $script;
+}
+
+sub parsedb (@) {
+	my @input = @_;
+	foreach (@input) {
+		chomp $_;
+#		ID,AegisName,Name,Type,Buy,Sell,Weight,ATK,DEF,Range,Slots,Job,Upper,Gender,Loc,wLV,eLV,Refineable,View,{ Script },{ OnEquip_Script },{ OnUnequip_Script }
+		if( $_ =~ qr/^
+			(?<prefix>(?:\/\/[^0-9]*)?)
+			(?<ID>[0-9]+),
+			(?<AegisName>[^,]+),
+			(?<Name>[^,]+),
+			(?<Type>[0-9]+),
+			(?<Buy>[0-9]*),
+			(?<Sell>[0-9]*),
+			(?<Weight>[0-9]*),
+			(?<ATK>[0-9-]*)(?<hasmatk>:(?<MATK>[0-9-]*))?,
+			(?<DEF>[0-9-]*),
+			(?<Range>[0-9]*),
+			(?<Slots>[0-9]*),
+			(?<Job>[x0-9A-F]*),
+			(?<Upper>[0-9]*),
+			(?<Gender>[0-9]*),
+			(?<Loc>[0-9]*),
+			(?<wLV>[0-9]*),
+			(?<eLV>[0-9]*)(?<hasmaxlv>:(?<eLVmax>[0-9]*))?,
+			(?<Refineable>[0-9]*),
+			(?<View>[0-9]*),
+			{(?<Script>.*)},
+			{(?<OnEquip>.*)},
+			{(?<OnUnequip>.*)}
+		/x ) {
+			my %cols = map { $_ => $+{$_} } keys %+;
+			print "/*\n" if $cols{prefix};
+			print "$cols{prefix}\n" if $cols{prefix} and $cols{prefix} !~ m|^//[\s\t]*$|;
+			print "{\n";
+			print "\tId: $cols{ID}\n";
+			print "\tAegisName: \"$cols{AegisName}\"\n";
+			print "\tName: \"$cols{Name}\"\n";
+			print "\tType: $cols{Type}\n";
+			print "\tBuy: $cols{Buy}\n" if $cols{Buy} || $cols{Buy} eq '0';
+			print "\tSell: $cols{Sell}\n" if $cols{Sell} || $cols{Sell} eq '0';
+			print "\tWeight: $cols{Weight}\n" if $cols{Weight};
+			print "\tAtk: $cols{ATK}\n" if $cols{ATK};
+			print "\tMatk: $cols{MATK}\n" if $cols{MATK};
+			print "\tDef: $cols{DEF}\n" if $cols{DEF};
+			print "\tRange: $cols{Range}\n" if $cols{Range};
+			print "\tSlots: $cols{Slots}\n" if $cols{Slots};
+			$cols{Job} = '0xFFFFFFFF' unless $cols{Job};
+			print "\tJob: $cols{Job}\n" unless $cols{Job} =~ /0xFFFFFFFF/i;
+			print "\tUpper: $cols{Upper}\n" if $cols{Upper} && (($cols{hasmatk} && $cols{Upper} != 0x3f) || (!$cols{hasmatk} && $cols{Upper} != 7));
+			$cols{Gender} = '2' unless $cols{Gender};
+			print "\tGender: $cols{Gender}\n" unless $cols{Gender} eq '2';
+			print "\tLoc: $cols{Loc}\n" if $cols{Loc};
+			print "\tWeaponLv: $cols{wLV}\n" if $cols{wLV};
+			if ($cols{hasmaxlv} and $cols{eLVmax}) {
+				$cols{eLV} = 0 unless $cols{eLV};
+				print "\tEquipLv: [$cols{eLV}, $cols{eLVmax}]\n";
+			} else {
+				print "\tEquipLv: $cols{eLV}\n" if $cols{eLV};
+			}
+			print "\tRefine: false\n" if !$cols{Refineable} and ($cols{Type} == 4 or $cols{Type} == 5);
+			print "\tView: $cols{View}\n" if $cols{View};
+			$cols{Script} = prettifyscript($cols{Script});
+			print "\tScript: <\"$cols{Script}\">\n" if $cols{Script};
+			$cols{OnEquip} = prettifyscript($cols{OnEquip});
+			print "\tOnEquipScript: <\"$cols{OnEquip}\">\n" if $cols{OnEquip};
+			$cols{OnUnequip} = prettifyscript($cols{OnUnequip});
+			print "\tOnUnequipScript: <\"$cols{OnUnequip}\">\n" if $cols{OnUnequip};
+			print "},\n";
+			print "*/\n" if $cols{prefix};
+		} elsif( $_ =~ /^\/\/(.*)$/ ) {
+			my $s = $1;
+			print "// $s\n" unless $s =~ /^[\s\t]*$/;
+		} elsif( $_ !~ /^\s*$/ ) {
+			print "// Error parsing: $_\n";
+		}
+
+	}
+}
+print <<"EOF";
+item_db: (
+/******************************************************************************
+ ************* Entry structure ************************************************
+ ******************************************************************************
+{
+	// =================== Mandatory fields ===============================
+	Id: ID                        (int)
+	AegisName: "Aegis_Name"       (string, optional if Inherit: true)
+	Name: "Item Name"             (string, optional if Inherit: true)
+	// =================== Optional fields ================================
+	Type: Item Type               (int, defaults to 3 = etc item)
+	Buy: Buy Price                (int, defaults to Sell * 2)
+	Sell: Sell Price              (int, defaults to Buy / 2)
+	Weight: Item Weight           (int, defaults to 0)
+	Atk: Attack                   (int, defaults to 0)
+	Matk: Magical Attack          (int, defaults to 0, ignored in pre-re)
+	Def: Defense                  (int, defaults to 0)
+	Range: Attack Range           (int, defaults to 0)
+	Slots: Slots                  (int, defaults to 0)
+	Job: Job mask                 (int, defaults to all jobs = 0xFFFFFFFF)
+	Upper: Upper mask             (int, defaults to any = 0x3f)
+	Gender: Gender                (int, defaults to both = 2)
+	Loc: Equip location           (int, required value for equipment)
+	WeaponLv: Weapon Level        (int, defaults to 0)
+	EquipLv: Equip required level (int, defaults to 0)
+	EquipLv: [min, max]           (alternative syntax with min / max level)
+	Refine: Refineable            (boolean, defaults to true)
+	View: View ID                 (int, defaults to 0)
+	Script: <"
+		Script
+		(it can be multi-line)
+	">
+	OnEquipScript: <" OnEquip Script (can also be multi-line) ">
+	OnUnequipScript: <" OnUnequip Script (can also be multi-line) ">
+	// =================== Optional fields (item_db2 only) ================
+	Inherit: true/false           (boolean, if true, inherit the values
+	                              that weren't specified, from item_db.conf,
+	                              else override it and use default values)
+},
+******************************************************************************/
+
+EOF
+
+parsedb(<>);
+
+print ")\n";

commit 22b946b2db0374164697ba57b64abb11551b0ff3

Fix Steal accidentally being able to steal cards from certain mobs
Cards are now blocked by item type rather than item drop slot position,
since cards no longer have a fixed position in the drop list (regression
in ed72a947a6c97804c1eef5b80bfa49d99f7d7586)

Signed-off-by: Haru <haru@dotalux.com>

src/map/mob.h
@@ -40,7 +40,6 @@ struct hplugin_data_store;
 //The number of drops all mobs have and the max drop-slot that the steal skill will attempt to steal from.
 #define MAX_MOB_DROP 10
 #define MAX_MVP_DROP 3
-#define MAX_STEAL_DROP 7
 
 //Min time between AI executions
 #define MIN_MOBTHINKTIME 100

src/map/pc.c
@@ -5583,15 +5583,17 @@ static int pc_steal_item(struct map_session_data *sd, struct block_list *bl, uin
 
 	// Try dropping one item, in the order from first to last possible slot.
 	// Droprate is affected by the skill success rate.
-	for (i = 0; i < MAX_STEAL_DROP; i++) {
+	for (i = 0; i < MAX_MOB_DROP; i++) {
 		if (md->db->dropitem[i].nameid == 0)
 			continue;
 		if ((data = itemdb->exists(md->db->dropitem[i].nameid)) == NULL)
 			continue;
+		if (data->type == IT_CARD)
+			continue;
 		if (rnd() % 10000 < apply_percentrate(md->db->dropitem[i].p, rate, 100))
 			break;
 	}
-	if (i == MAX_STEAL_DROP)
+	if (i == MAX_MOB_DROP)
 		return 0;
 
 	itemid = md->db->dropitem[i].nameid;