755edf380613992cc05e38afcd2e9ae5eff6fd6d
skyleo <skyleo@skyleo.de>
2020-01-16 19:14:29 +0100

    Update src/map/battle.c
    Co-Authored-By: Kenpachi2k13 <Kenpachi.Developer@gmx.de>

--- src/map/battle.c
+++ src/map/battle.c
@@ -6608,7 +6608,7 @@ static int battle_check_target(struct block_list *src, struct block_list *target
 	if( (s_bl = battle->get_master(src)) == NULL )
 		s_bl = src;
 
-	if ((flag & BCT_ENEMY) != 0 && (status_get_mode(s_bl) & MD_BOSS) == 0 && (map->getcell(m, src, src->x, src->y, CELL_CHKBASILICA)
+	if ((flag & BCT_ENEMY) != 0 && (status_get_mode(s_bl) & MD_BOSS) == 0 && (map->getcell(m, src, src->x, src->y, CELL_CHKBASILICA) != 0
 	    || map->getcell(m, src, target->x, target->y, CELL_CHKBASILICA))) {
 		return -1;
 	}
