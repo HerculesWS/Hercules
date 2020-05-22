8e66eddfd8a1abfb1c827ce4a9f33d3e4c5cee87
Andrei Karas <akaras@inbox.ru>
2020-05-22 04:26:56 +0300

    Add workaorund for authors generation

--- tools/authors.sh
+++ tools/authors.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-git log --format=format:"%aN <%aE>"|grep -v "54d463be-8e91-2dee-dedb-b68131a5f0ec"|sort|uniq
+# grep -v "kenpachi2k11" as workaround for avoid .mailmap issue
+git log --format=format:"%aN <%aE>"|grep -v "54d463be-8e91-2dee-dedb-b68131a5f0ec"|grep -v "kenpachi2k11"|sort|uniq
