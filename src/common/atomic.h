40eecd7dee4626f829c3e5e05a1681090d561dbe
Andrei Karas <akaras@inbox.ru>
2020-06-05 08:33:26 +0300

    Disable error in atomic.h

--- src/common/atomic.h
+++ src/common/atomic.h
@@ -104,7 +104,7 @@ forceinline volatile int64 InterlockedExchange64(volatile int64 *target, int64 v
 // The __sync functions are available on x86 or ARMv6+
 #if !defined(__x86_64__) && !defined(__i386__) \
 	&& ( !defined(__ARM_ARCH_VERSION__) || __ARM_ARCH_VERSION__ < 6 )
-#error Your Target Platfrom is not supported
+//#error Your Target Platfrom is not supported
 #endif
 
 static forceinline int64 InterlockedExchangeAdd64(volatile int64 *addend, int64 increment){
