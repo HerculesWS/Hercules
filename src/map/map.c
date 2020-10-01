95a7f69642f438ce447c450337f46e81576ffdef
skyleo <skyleo@skyleo.de>
2020-10-01 17:19:59 +0200

    Remove unnecessary casts in map->random_dir

--- src/map/map.c
+++ src/map/map.c
@@ -2968,14 +2968,14 @@ static int map_random_dir(struct block_list *bl, int16 *x, int16 *y)
 		yi = 1;
 	}
 	int dist2 = xi * xi + yi * yi;
-	short dist = (short)sqrt((float)dist2);
+	short dist = (short)sqrt(dist2);
 
 	short i = 0;
 	do {
 		enum unit_dir dir = unit_get_rnd_diagonal_dir();
 		short segment = 1+(rnd()%dist); //Pick a random interval from the whole vector in that direction
 		xi = bl->x + segment * dirx[dir];
-		segment = (short)sqrt((float)(dist2 - segment*segment)); //The complement of the previously picked segment
+		segment = (short)sqrt(dist2 - segment * segment); // The complement of the previously picked segment
 		yi = bl->y + segment * diry[dir];
 	} while ((map->getcell(bl->m, bl, xi, yi, CELL_CHKNOPASS) || !path->search(NULL, bl, bl->m, bl->x, bl->y, xi, yi, 1, CELL_CHKNOREACH))
 	       && (++i)<100);
