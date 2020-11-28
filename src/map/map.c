97c852e08c20c950cbde13fd89d01aeab1e49eb3
skyleo <skyleo@skyleo.de>
2020-11-28 21:58:29 +0100

    Reduce indentation in map->search_freecell

--- src/map/map.c
+++ src/map/map.c
@@ -1673,20 +1673,20 @@ static int map_search_freecell(struct block_list *src, int16 m, int16 *x, int16 
 		if (*x == center_x && *y == center_y)
 			continue; //Avoid picking the same target tile.
 
-		if (map->getcell(m, src, *x, *y, CELL_CHKREACH)) {
-			if ((flag & SFC_REACHABLE) != 0 && !unit->can_reach_pos(src, *x, *y, 1))
+		if (map->getcell(m, src, *x, *y, CELL_CHKREACH) == 0)
+			continue;
+
+		if ((flag & SFC_REACHABLE) != 0 && !unit->can_reach_pos(src, *x, *y, 1))
+			continue;
+		if ((flag & SFC_AVOIDPLAYER) != 0) {
+			if (avoidplayer_retries >= 100)
+				return 0; // Limit of retries reached.
+			if (avoidplayer_retries++ < battle_config.no_spawn_on_player
+			    && map->foreachinarea(map->count_sub, m, *x - AREA_SIZE, *y - AREA_SIZE,
+			                          *x + AREA_SIZE, *y + AREA_SIZE, BL_PC) != 0)
 				continue;
-			if ((flag & SFC_AVOIDPLAYER) != 0) {
-				if (avoidplayer_retries >= 100)
-					return 0; //Limit of retries reached.
-				if (avoidplayer_retries++ < battle_config.no_spawn_on_player
-				 && map->foreachinarea(map->count_sub, m, *x-AREA_SIZE, *y-AREA_SIZE,
-				                                         *x+AREA_SIZE, *y+AREA_SIZE, BL_PC)
-				)
-					continue;
-			}
-			return 1;
 		}
+		return 1;
 	}
 	*x = center_x;
 	*y = center_y;
