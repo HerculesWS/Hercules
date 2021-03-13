000cd1d98e45d102f560b58be1e9ff3d64b22b17
Andrei Karas <akaras@inbox.ru>
2021-03-13 23:43:54 +0300

    Move job enum into separate files
    Also use ENUM_DEFINE macro for each job enum member.

---
+++ src/common/class_hidden.h
@@ -0,0 +1,52 @@
+/**
+ * This file is part of Hercules.
+ * http://herc.ws - http://github.com/HerculesWS/Hercules
+ *
+ * Copyright (C) 2012-2021 Hercules Dev Team
+ * Copyright (C) Athena Dev Teams
+ *
+ * Hercules is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+ENUM_VALUE(KNIGHT2, 13)
+ENUM_VALUE(CRUSADER2, 21)
+ENUM_VALUE(LORD_KNIGHT2, 4014)
+ENUM_VALUE(PALADIN2, 4022)
+ENUM_VALUE(BABY_KNIGHT2, 4036)
+ENUM_VALUE(BABY_CRUSADER2, 4044)
+ENUM_VALUE(STAR_GLADIATOR2, 4048)
+//ENUM_VALUE(THIRDJOB_BEGIN, 4053)
+ENUM_VALUE(RUNE_KNIGHT2, 4080)
+ENUM_VALUE(RUNE_KNIGHT_T2, 4081)
+ENUM_VALUE(ROYAL_GUARD2, 4082)
+ENUM_VALUE(ROYAL_GUARD_T2, 4083)
+ENUM_VALUE(RANGER2, 4084)
+ENUM_VALUE(RANGER_T2, 4085)
+ENUM_VALUE(MECHANIC2, 4086)
+ENUM_VALUE(MECHANIC_T2, 4087)
+ENUM_VALUE(RUNE_KNIGHT3, 4088)
+ENUM_VALUE(RUNE_KNIGHT4, 4089)
+ENUM_VALUE(RUNE_KNIGHT5, 4090)
+ENUM_VALUE(RUNE_KNIGHT6, 4091)
+ENUM_VALUE(RUNE_KNIGHT7, 4092)
+ENUM_VALUE(RUNE_KNIGHT8, 4093)
+ENUM_VALUE(RUNE_KNIGHT9, 4094)
+ENUM_VALUE(RUNE_KNIGHT10, 4095)
+ENUM_VALUE(BABY_RUNE2, 4109)
+ENUM_VALUE(BABY_GUARD2, 4110)
+ENUM_VALUE(BABY_RANGER2, 4111)
+ENUM_VALUE(BABY_MECHANIC2, 4112)
+ENUM_VALUE(THIRD_LAST, 4113)
+ENUM_VALUE(BABY_STAR_GLADIATOR2, 4238)
+//ENUM_VALUE(2004_JOB_LAST, 4249)
