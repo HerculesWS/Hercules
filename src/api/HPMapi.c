e63c3e7446886a0f4227a09753e432237d8a304b
Andrei Karas <akaras@inbox.ru>
2021-04-05 23:47:15 +0300

    Fix compilation warning after add -Wswitch-enum compiler flag

--- src/api/HPMapi.c
+++ src/api/HPMapi.c
@@ -70,11 +70,6 @@
  */
 bool HPM_api_data_store_validate(enum HPluginDataTypes type, struct hplugin_data_store **storeptr, bool initialize)
 {
-	switch (type) {
-		// No supported types at the moment.
-		default:
-			break;
-	}
 	return false;
 }
 
