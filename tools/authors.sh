8966b6333be7865a81a56a14b69e0e8dcbda8896
Haru <haru@dotalux.com>
2021-04-07 00:40:56 +0200

    Add some missing mailmap entries and remove an exception from the AUTHORS generation script
    Signed-off-by: Haru <haru@dotalux.com>

--- tools/authors.sh
+++ tools/authors.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
 
-# grep -v "kenpachi2k11" as workaround for avoid .mailmap issue
-git log --format=format:"%aN <%aE>"|grep -v "54d463be-8e91-2dee-dedb-b68131a5f0ec"|grep -v "kenpachi2k11"|sort|uniq
+git log --format=format:"%aN <%aE>" | grep -v "54d463be-8e91-2dee-dedb-b68131a5f0ec" | sort | uniq
