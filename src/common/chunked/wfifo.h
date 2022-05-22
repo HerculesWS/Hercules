82445464bb31607e196c48a6184eadbc6b5be57c
Andrei Karas <akaras@inbox.ru>
2022-05-23 00:55:32 +0300

    Add support for send chunked packets from server to client

--- src/common/chunked/wfifo.h
+++ src/common/chunked/wfifo.h
@@ -26,7 +26,7 @@
 #endif  // WFIFO_CHUNK_SIZE
 
 #ifndef WFIFO_CLIENT_CHUNK_SIZE
-#define WFIFO_CLIENT_CHUNK_SIZE 30000
+#define WFIFO_CLIENT_CHUNK_SIZE 32000
 #endif  // WFIFO_CLIENT_CHUNK_SIZE
 
 // base WFIFO_CHUNKED defines
@@ -42,43 +42,70 @@
 	const int p ## _full_chunks_count = p ## data_len / (chunk_size); \
 	for (int p ## _cnt = 0; p ## _cnt < p ## _full_chunks_count; p ## _cnt ++, p ## _offset += (chunk_size))
 
-#define WFIFO_CHUNKED_BLOCK_START_RAW(p, chunk_size) \
+#define WFIFO_CHUNKED_BLOCK_START_RAW1(p, _dataField, chunk_size) \
 		p ## _len = p ## _fixed_len + (chunk_size); \
 		WFIFOHEAD(p ## _fd, p ## _len); \
 		p = WFIFOP(p ## _fd, 0); \
 		WFIFOW(p ## _fd, 0) = p ## _header_id; \
 		WFIFOW(p ## _fd, 2) = p ## _len; \
+		memcpy((p)->_dataField, p ## data + p ## _offset, (chunk_size))
+#define WFIFO_CHUNKED_BLOCK_START_RAW_FLAG(p) \
 		if (p ## _cnt == 0) \
 			(p)->flag = 0; \
 		else \
-			(p)->flag = 1; \
-		memcpy((p)->data, p ## data + p ## _offset, (chunk_size))
+			(p)->flag = 1
 
-#define WFIFO_CHUNKED_FINAL_START_RAW(p, chunk_size) \
+#define WFIFO_CHUNKED_BLOCK_END_RAW() \
+		WFIFOSET(p ## _fd, p ## _len)
+
+#define WFIFO_CHUNKED_FINAL_START_RAW1(p, _dataField, chunk_size) \
 	const uint32 p ## _left_size = p ## data_len - p ## _full_chunks_count * (chunk_size); \
 	p ## _len = p ## _fixed_len + p ## _left_size; \
 	WFIFOHEAD(p ## _fd, p ## _len); \
 	p = WFIFOP(p ## _fd, 0); \
 	WFIFOW(p ## _fd, 0) = p ## _header_id; \
 	WFIFOW(p ## _fd, 2) = p ## _len; \
-	(p)->flag = 2; \
 	if (p ## _left_size > 0) \
-		memcpy((p)->data, p ## data + p ## _offset, p ## _left_size);
+		memcpy((p)->_dataField, p ## data + p ## _offset, p ## _left_size)
+
+#define WFIFO_CHUNKED_FINAL_START_RAW_FLAG(p) \
+	(p)->flag = 2
+
+#define WFIFO_CHUNKED_FINAL_END_RAW() \
+		WFIFOSET(p ## _fd, p ## _len)
 
 // inter server WFIFO_CHUNKED defines
 #define WFIFO_CHUNKED_INIT(p, fd, header, pname, pdata, pdata_len) \
 	WFIFO_CHUNKED_INIT_RAW(p, fd, header, pname, pdata, pdata_len, WFIFO_CHUNK_SIZE)
 
 #define WFIFO_CHUNKED_BLOCK_START(p) \
-	WFIFO_CHUNKED_BLOCK_START_RAW(p, WFIFO_CHUNK_SIZE)
+	WFIFO_CHUNKED_BLOCK_START_RAW1(p, data, WFIFO_CHUNK_SIZE); \
+	WFIFO_CHUNKED_BLOCK_START_RAW_FLAG(p)
 
 #define WFIFO_CHUNKED_BLOCK_END() \
-		WFIFOSET(p ## _fd, p ## _len)
+	WFIFO_CHUNKED_BLOCK_END_RAW()
 
 #define WFIFO_CHUNKED_FINAL_START(p) \
-	WFIFO_CHUNKED_FINAL_START_RAW(p, WFIFO_CHUNK_SIZE)
+	WFIFO_CHUNKED_FINAL_START_RAW1(p, data, WFIFO_CHUNK_SIZE); \
+	WFIFO_CHUNKED_FINAL_START_RAW_FLAG(p)
 
 #define WFIFO_CHUNKED_FINAL_END() \
-		WFIFOSET(p ## _fd, p ## _len)
+	WFIFO_CHUNKED_FINAL_END_RAW()
+
+// server to client WFIFO_CHUNKED defines
+#define WFIFO_CLIENT_CHUNKED_INIT(p, fd, header, pname, pdata, pdata_len) \
+	WFIFO_CHUNKED_INIT_RAW(p, fd, header, pname, pdata, pdata_len, WFIFO_CLIENT_CHUNK_SIZE)
+
+#define WFIFO_CLIENT_CHUNKED_BLOCK_START(p, _dataField) \
+	WFIFO_CHUNKED_BLOCK_START_RAW1(p, _dataField, WFIFO_CLIENT_CHUNK_SIZE)
+
+#define WFIFO_CLIENT_CHUNKED_BLOCK_END() \
+	WFIFO_CHUNKED_BLOCK_END_RAW()
+
+#define WFIFO_CLIENT_CHUNKED_FINAL_START(p, _dataField) \
+	WFIFO_CHUNKED_FINAL_START_RAW1(p, _dataField, WFIFO_CLIENT_CHUNK_SIZE)
+
+#define WFIFO_CLIENT_CHUNKED_FINAL_END() \
+	WFIFO_CHUNKED_FINAL_END_RAW()
 
 #endif /* COMMON_CHUNKED_WFIFO_H */
