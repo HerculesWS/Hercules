821e9da86dc2622aaa1bb0899ee8b1612e1e80a1
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2023-07-08 11:41:20 -0300

    Rebalance of KN_TWOHANDQUICKEN (Two-Hand Quicken)
    - Gives additional 10% of ASPD (as equipment ASPD)
    - Now gives HIT +(2 * SkillLevel)
    - Now gives CRIT +(2 + SkillLevel)
    
    From massive skills rebalance (1st/2nd/transclass) (2018.10.31)

--- src/map/status.c
+++ src/map/status.c
@@ -4669,6 +4669,8 @@ static int status_calc_critical(struct block_list *bl, struct status_change *sc,
 #ifdef RENEWAL
 	if (sc->data[SC_SPEARQUICKEN])
 		critical += 3*sc->data[SC_SPEARQUICKEN]->val1 * 10;
+	if (sc->data[SC_TWOHANDQUICKEN] != NULL)
+		critical += 20 + sc->data[SC_TWOHANDQUICKEN]->val1 * 10;
 #endif
 
 	if (sc->data[SC__INVISIBILITY])
@@ -4743,6 +4745,8 @@ static int status_calc_hit(struct block_list *bl, struct status_change *sc, int 
 #ifdef RENEWAL
 	if (sc->data[SC_BLESSING] != NULL)
 		hit += sc->data[SC_BLESSING]->val3;
+	if (sc->data[SC_TWOHANDQUICKEN] != NULL)
+		hit += 2 * sc->data[SC_TWOHANDQUICKEN]->val1;
 #endif
 
 	return cap_value(hit, battle_config.hit_min, battle_config.hit_max);
@@ -5490,6 +5494,8 @@ static short status_calc_aspd(struct block_list *bl, struct status_change *sc, s
 #ifdef RENEWAL
 		if (sc->data[SC_INC_AGI] != NULL)
 			bonus += sc->data[SC_INC_AGI]->val1; // + SkillLevel%
+		if (sc->data[SC_TWOHANDQUICKEN] != NULL)
+			bonus += 10;
 #endif
 	}
 
