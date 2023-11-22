7fa862c6f4b5fa7c1ceb65904994fcacaeb6f133
csnv <jorgecsnova@gmail.com>
2023-11-23 00:47:04 +0100

    Fix sc counters not being reset when a mob unit dies

--- src/map/status.c
+++ src/map/status.c
@@ -10596,7 +10596,19 @@ static int status_change_clear(struct block_list *bl, int type)
 
 	sc = status->get_sc(bl);
 
-	if (!sc || !sc->count)
+	if (sc == NULL)
+		return 0;
+
+	sc->opt1 = 0;
+	sc->opt2 = 0;
+	sc->opt3 = 0;
+	sc->bs_counter = 0;
+	sc->fv_counter = 0;
+#ifndef RENEWAL
+	sc->sg_counter = 0;
+#endif
+
+	if (sc->count == 0)
 		return 0;
 
 	for(i = 0; i < SC_MAX; i++) {
@@ -10629,15 +10641,6 @@ static int status_change_clear(struct block_list *bl, int type)
 			sc->data[i] = NULL;
 		}
 	}
-
-	sc->opt1 = 0;
-	sc->opt2 = 0;
-	sc->opt3 = 0;
-	sc->bs_counter = 0;
-	sc->fv_counter = 0;
-#ifndef RENEWAL
-	sc->sg_counter = 0;
-#endif
 
 	if( type == 0 || type == 2 )
 		clif->changeoption(bl);
