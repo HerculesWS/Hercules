484691a0a2ad6abaa3618b2155f98b1e1bb801c0
Haru <haru@dotalux.com>
2024-10-31 23:34:09 +0000

    Release v2024.10
    Signed-off-by: Haru <haru@dotalux.com>

--- src/config/core.h
+++ src/config/core.h
@@ -22,7 +22,7 @@
 #define CONFIG_CORE_H
 
 /// Hercules version. From tag vYYYY.MM(+PPP) -> YYYYMMPPP
-#define HERCULES_VERSION 202409000
+#define HERCULES_VERSION 202410000
 
 /// Max number of items on @autolootid list
 #define AUTOLOOTITEM_SIZE 10
