007a858cad3b9c3256a1fed9178f99e15f629118
jsn <jasonch35@gmail.com>
2024-12-01 21:54:15 +0800

    Removed unnecessary cleaning of VECTOR

--- src/map/unit.c
+++ src/map/unit.c
@@ -3004,11 +3004,6 @@ static int unit_free(struct block_list *bl, enum clr_type clrtype)
 			VECTOR_CLEAR(sd->hatEffectId);
 			VECTOR_CLEAR(sd->title_ids); // Title [Dastgir/Hercules]
 			VECTOR_CLEAR(sd->agency_requests);
-
-			for (int i = 0; i < VECTOR_LENGTH(sd->storage.list); i++) { // Storages
-				VECTOR_CLEAR(VECTOR_INDEX(sd->storage.list, i).item);
-				VECTOR_INDEX(sd->storage.list, i).received = false;
-			}
 			VECTOR_CLEAR(sd->storage.list);
 			
 			if( sd->quest_log != NULL ) {
