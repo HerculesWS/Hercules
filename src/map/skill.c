ecc5316f6bb7dcd7f46beaf667643ef72c1ecf6d
Vaans <max.castillov93@gmail.com>
2025-02-09 22:33:44 -0300

    NPC_COMET
    NPC_COMET
    
    Missing colon
    
    SkillInfo

--- src/map/skill.c
+++ src/map/skill.c
@@ -2023,6 +2023,7 @@ static int skill_additional_effect(struct block_list *src, struct block_list *bl
 			sc_start(src, bl, SC_STUN, 40, skill_lv, skill->get_time(skill_id, skill_lv), skill_id);
 			break;
 		case WL_COMET:
+		case NPC_COMET:
 			sc_start4(src, bl, SC_BURNING, 100, skill_lv, 0, src->id, 0, skill->get_time2(skill_id, skill_lv), skill_id);
 			break;
 		case WL_EARTHSTRAIN:
@@ -3506,6 +3507,7 @@ static int skill_attack(int attack_type, struct block_list *src, struct block_li
 			FALLTHROUGH
 		case WL_SOULEXPANSION:
 		case WL_COMET:
+		case NPC_COMET:
 		case NJ_HUUMA:
 			dmg.dmotion = clif->skill_damage(src,bl,tick,dmg.amotion,dmg.dmotion,damage,dmg.div_,skill_id,skill_lv,BDT_MULTIHIT);
 			break;
@@ -5109,6 +5111,7 @@ static int skill_castend_damage_id(struct block_list *src, struct block_list *bl
 		case WL_SOULEXPANSION:
 		case WL_CRIMSONROCK:
 		case WL_COMET:
+		case NPC_COMET:
 		case WL_JACKFROST:
 		case RA_ARROWSTORM:
 		case RA_WUGDASH:
@@ -12263,6 +12266,7 @@ static int skill_castend_pos2(struct block_list *src, int x, int y, uint16 skill
 		case NPC_EARTHQUAKE:
 		case NPC_EVILLAND:
 		case WL_COMET:
+		case NPC_COMET:
 		case RA_ELECTRICSHOCKER:
 		case RA_CLUSTERBOMB:
 		case RA_MAGENTATRAP:
@@ -13368,6 +13372,7 @@ static struct skill_unit_group *skill_unitsetting(struct block_list *src, uint16
 			limit = 1000000;//it doesn't matter
 			break;
 		case WL_COMET:
+		case NPC_COMET:
 			if( sc ) {
 				sc->comet_x = x;
 				sc->comet_y = y;
