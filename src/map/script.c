d1fcefca844c0e37fc21e2e10a89b83219a9ca86
maqc1 <alex--223@hotmail.com>
2025-03-29 13:11:13 -0400

    Add files via upload

--- src/map/script.c
+++ src/map/script.c
@@ -18220,20 +18220,21 @@ static BUILDIN(getsavepoint)
 	return true;
 }
 
-/*==========================================
- *	bitcount(<number>)
- *	Return number of set bits in an integer [maqc]
- *------------------------------------------*/
+/**
+ * Returns the number of set bits in integer <number>
+ *
+ * bitcount(<number>)
+ */
 static BUILDIN(bitcount)
 {
 	unsigned int count = 0;
-	unsigned int value = script_getnum(st,2);
-	while(value > 0) {
-		if((value & 1) == 1)
+	unsigned int value = script_getnum(st, 2);
+	while (value > 0) {
+		if ((value & 1) == 1)
 			count++;
 		value >>= 1;
 	}
-	script_pushint(st,count);
+	script_pushint(st, count);
 	return true;
 }
 
@@ -29318,7 +29319,7 @@ static void script_parse_builtin(void)
 		BUILDIN_DEF(mobcount,"ss"),
 		BUILDIN_DEF(getlook,"i"),
 		BUILDIN_DEF(getsavepoint,"i"),
-		BUILDIN_DEF(bitcount,"i"), // [maqc]
+		BUILDIN_DEF(bitcount, "i"), // [maqc]
 		BUILDIN_DEF(npcspeed,"i"), // [Valaris]
 		BUILDIN_DEF(npcwalkto,"ii"), // [Valaris]
 		BUILDIN_DEF(npcstop,""), // [Valaris]
