072edfbdc24d581c2fafa1da0abe525fbb16b815
Ibrahim Zidan <brahem@aotsw.com>
2026-07-12 21:47:56 +0300

    Change SSL library used for mariadb to default to schannel on windows and openssl on other platforms
    Signed-off-by: Ibrahim Zidan <brahem@aotsw.com>

--- conanfile.py
+++ conanfile.py
@@ -15,10 +15,14 @@ class HerculesRecipe(ConanFile):
     }
 
     def requirements(self):
+        ssl_lib = "openssl"
+        if self.settings.os == "Windows":
+            ssl_lib = "schannel"
+
         if self.options.with_libbacktrace:
             self.requires("libbacktrace/cci.20240730")
         self.requires("cjson/1.7.19")
-        self.requires("mariadb-connector-c/3.4.8", options={"shared": True})
+        self.requires("mariadb-connector-c/3.4.8", options={"shared": True, "with_ssl": ssl_lib})
         self.requires("pcre/8.45")
         self.requires("zlib/1.3.2")
         self.requires("giflib/5.2.2")
