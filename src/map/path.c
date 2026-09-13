3dc5c3749e121a768f2b382bf110dbc53f444ab5
Haru <haru@dotalux.com>
2026-09-13 17:45:55 +0200

    Replace swap_ptr calls with HSWAP_PTR

--- src/map/path.c
+++ src/map/path.c
@@ -192,7 +192,7 @@ static bool path_search_long(struct shootpath_data *spd, struct block_list *bl, 
 static void heap_push_node(struct node_heap *heap, struct path_node *node)
 {
 	BHEAP_ENSURE(*heap, 1, 256);
-	BHEAP_PUSH2(*heap, node, NODE_MINTOPCMP, swap_ptr);
+	BHEAP_PUSH2(*heap, node, NODE_MINTOPCMP, HSWAP_PTR);
 }
 
 /// Updates path_node in the binary node_heap.
@@ -204,7 +204,7 @@ static int heap_update_node(struct node_heap *heap, struct path_node *node)
 		ShowError("heap_update_node: node not found\n");
 		return 1;
 	}
-	BHEAP_UPDATE(*heap, i, NODE_MINTOPCMP, swap_ptr);
+	BHEAP_UPDATE(*heap, i, NODE_MINTOPCMP, HSWAP_PTR);
 	return 0;
 }
 
@@ -367,7 +367,7 @@ static bool path_search(struct walkpath_data *wpd, struct block_list *bl, int16 
 		}
 
 		current = BHEAP_PEEK(open_set); // Look for the lowest f_cost node in the 'open' set
-		BHEAP_POP2(open_set, NODE_MINTOPCMP, swap_ptr); // Remove it from 'open' set
+		BHEAP_POP2(open_set, NODE_MINTOPCMP, HSWAP_PTR); // Remove it from 'open' set
 
 		x      = current->x;
 		y      = current->y;
