c8119c09cf22dae3fe79abcaeec4ba8bee8c7f56
Haru <haru@dotalux.com>
2026-09-16 01:42:10 +0200

    Add the HPM_DECLARE_PLUGIN() macro replacing the manual definition of the pinfo struct
    - The macro allows to automatically populate the .req_version field and
      to provide additional declarations.
    - The HPMi_s, HPMi, import_symbol variables are now declared only when
      the HPM_DECLARE_PLUGIN() macro is used, instead of automatically by
      the HPMi.h header (this makes the code more ODR-compliant in the C++
      migration, ensuring that a plugin with multiple .c files won't declare
      those symbols multiple times)
    - The HPM_VERSION is increased to 1.3
    - Plugins will need to be udpated to the new syntax
    
    Signed-off-by: Haru <haru@dotalux.com>

--- src/common/HPMi.h
+++ src/common/HPMi.h
@@ -36,7 +36,7 @@ struct hplugin_data_store;
 
 enum server_types CXX_ENUM_TYPE(unsigned int);
 
-#define HPM_VERSION "1.2"
+#define HPM_VERSION "1.3"
 
 // Maximum length of the configuration path for configs added with add*Conf
 #define HPM_ADDCONF_LENGTH 40
@@ -258,9 +258,24 @@ struct HPMi_interface {
 #ifdef HERCULES_CORE
 #define HPM_SYMBOL(n, s) (HPM->share((s), (n)), true)
 #else // ! HERCULES_CORE
-HPExport struct HPMi_interface HPMi_s;
-HPExport struct HPMi_interface *HPMi;
-HPExport void *(*import_symbol) (const char *name, unsigned int pID);
+extern struct HPMi_interface HPMi_s;
+extern struct HPMi_interface *HPMi;
+extern void *(*import_symbol) (const char *name, unsigned int pID);
+extern struct hplugin_info pinfo;
+
+#define HPM_PLUGIN_DEFS                                                        \
+  HPExport struct HPMi_interface HPMi_s;                                       \
+  HPExport struct HPMi_interface *HPMi;                                        \
+  HPExport void *(*import_symbol)(const char *name, unsigned int pID);
+
+#define HPM_DECLARE_PLUGIN(plugin_name, plugin_type, plugin_version)           \
+  HPM_PLUGIN_DEFS                                                              \
+  struct hplugin_info pinfo = {                                                \
+      (plugin_name),                                                           \
+      (plugin_type),                                                           \
+      (plugin_version),                                                        \
+      HPM_VERSION,                                                             \
+  };
 
 #define HPM_SYMBOL(n, s) ((s) = import_symbol((n),HPMi->pid))
 #endif // !HERCULES_CORE
